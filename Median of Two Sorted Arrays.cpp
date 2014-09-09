4de38497c40d5bb3837b485484bb2c17baabb56e
Zhao Wei <pkuzw@126.com>
2014-09-09 16:34:53 +0800

    完成源程序头部注释信息

---
+++ Median of Two Sorted Arrays.cpp
@@ -0,0 +1,26 @@
+﻿///@file 在O(lgn)的时间复杂度内找到两个已经排好序的数组的中位数
+/*
+There are two sorted arrays A and B of size m and n respectively. Find the median of the two sorted arrays. 
+The overall run time complexity should be O(log (m+n)).
+*/
+///@author zhaowei
+///@date 2014.09.09
+///@version 1.0
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution 
+{
+public:
+	double findMedianSortedArrays(int A[], int m, int B[], int n)
+	{
+
+	}
+};
+
+int main()
+{
+	return 0;
+}
