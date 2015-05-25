3106f5137a2aff87d2cb937580f5e72a8b1b45f0
Zhao Wei <pkuzw@126.com>
2015-05-25 22:10:03 +0800

    最长回文字符串-扩展法
    分两种基本情况向外扩展，找出最长子串。时间复杂度O(n^2)，空间复杂度O(n)

--- Longest Palindromic Substring.cpp
+++ Longest Palindromic Substring.cpp
@@ -75,6 +75,8 @@ public:
 	///@brief	动态规划法
 	///@param	s	待处理字符串
 	///@return	返回最长回文子串
+	///@author	zhaowei
+	///@date	2015.05.25
 	///@note	时间复杂度O(n^2)，空间复杂度O(n^2)
 	///			考虑到"a"和"aa"就是最简单的回文子串，如果存在子串P[i, j]是回文子串且S[i-1] = S[j+1]，则P[i-1, j+1]也是回文子串
 	string longestPalindrome_DP(string s)
@@ -114,6 +116,53 @@ public:
 		}
 		return s.substr(pstr_head, pstr_len);	//返回计算结果
 	}
+
+	///@brief	中心扩展法
+	///@param	s	字符串
+	///@param	i	扩展中心的左边界
+	///@param	j	扩展中心的右边界
+	///@return	返回扩展出的回文子串
+	///@author	zhaowei
+	///@date	2015.05.25
+	string ExtendAroundCenter(string s, int l, int r)
+	{
+		int left_bound = l;
+		int right_bound = r;
+		while (left_bound >= 0 && right_bound <= s.length()-1 && s[left_bound] == s[right_bound])
+		{
+			left_bound--;
+			right_bound++;
+		}
+		return s.substr(left_bound+1, right_bound-left_bound-1);		//这里要注意，left_bound和right_bound在跳出循环时已经各自又向两侧延伸过，所以要减去这部分。
+	}
+
+	///@brief	中心扩展法找最长回文子串
+	///@param	s	待处理的字符串
+	///@return	返回最长回文子串
+	///@author  zhaowei
+	///@date	2015.05.25
+	string longestPalindrome_extendAroundCenter(string s)
+	{
+		int pstr_head = 0;
+		int pstr_len = 1;
+		string pstr = s.substr(0, 1);
+
+		for (int i = 0; i < s.length()-1; i++)
+		{
+			string p1 = ExtendAroundCenter(s, i, i);	//基本情形1：从一个字符的中心点开始向两侧扩展
+			if (p1.length() > pstr.length())
+			{
+				pstr = p1;
+			}
+
+			string p2 = ExtendAroundCenter(s, i, i+1);	//基本情形2：从有两个相同字符的中心点向两侧扩展
+			if (p2.length() > pstr.length())
+			{
+				pstr = p2;
+			}
+		}
+		return pstr;
+	}
 };
 
 int main()
@@ -122,8 +171,10 @@ int main()
 	string s = "abbaaaaa";
 	string pstr_bf = slt.longestPalindrome_BruteForce(s);
 	string pstr_dp = slt.longestPalindrome_DP(s);
+	string pstr_extend = slt.longestPalindrome_extendAroundCenter(s);
 	cout << pstr_bf << endl;
 	cout << pstr_dp;
 	cout << endl;
+	cout << pstr_extend << endl;
 	return 0;
 };
