f85d238462b999fb8f56bfe296cb3008b1825c8b
Zhao Wei <pkuzw@126.com>
2015-05-27 23:04:16 +0800

    Zigzag Conversion
    1. 通过观察横向读取时的字符间距规律来进行处理，oj上能够ac。

--- ZigZag Conversion.cpp
+++ ZigZag Conversion.cpp
@@ -16,7 +16,8 @@ public:
 	///@author	zhaowei
 	///@date	2015.05.27
 	///@status	在leetcode上报internal error，应该还有别的方法。
-	string convert(string s, int numRows) {
+	///@note	模拟二维矩形来保存字符
+	string convert1(string s, int numRows) {
 
 		if (numRows > 1)	//如果大于一行，则转换为二维数组矩形
 		{
@@ -78,11 +79,56 @@ public:
 			return s;
 		}
 	}
+	///@brief	将原本纵向阅读的之字形字符串转换成横向阅读的字符串
+	///@param	s	待处理的字符串
+	///@param	numRows	之字形矩阵的行数
+	///@return	转换后的字符串
+	///@author	zhaowei
+	///@date	2015.05.27
+	///@note	通过观察发现，这个之字形的字符串之间是有间隔来读取的，如果行号是i，那么第一行和最后一行的字符相隔的间距是2*(numRows-1)；
+	//			中间各行的间距分别是2*(numRows-i-1)和2*i。这样按照两种情况进行读取即可。
+	string convert(string s, int numRows) {
+		string rstr;
+		if (numRows == 1)	//如果行数是1直接返回
+		{
+			return s;
+		}
+
+		for (int i = 0; i < numRows; i++)
+		{
+			int j = 0;
+			if (i == 0 || i == numRows-1)	//如果是首行和末行的情况，每个字符的间距相等，都是2*(numRows-1)
+			{
+				j = i;
+				while (j < s.length())
+				{
+					rstr += s[j];
+					j += 2*(numRows-1);
+				}				
+			}
+			else							//中间各行的情况不一样，每numRows列中间又夹着一个字符，需要分两次处理
+			{
+				j = i;
+				while (j < s.length())
+				{
+					rstr += s[j];
+					j += 2*(numRows-i-1);
+					if (j >= s.length())	//注意判定不要越界
+					{
+						break;
+					}
+					rstr += s[j];
+					j += 2*i;
+				}
+			}
+		}
+		return rstr;
+	}
 };
 int main()
 {
-	string t = "";
-	int nrows = 1;
+	string t = "PAYPALISHIRING";
+	int nrows = 3;
 	
 	Solution slt;
 	string rslt = slt.convert(t, nrows);	
