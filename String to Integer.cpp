40253a2561fbb0b9c9f252b82787f8ecf95d8a51
Zhao Wei <pkuzw@126.com>
2015-05-28 16:44:57 +0800

    String to Integer

---
+++ String to Integer.cpp
@@ -0,0 +1,22 @@
+﻿///@brief	自己实现atoi函数
+///@author	zhaowei
+///@date	2015.05.28
+///@version	1.0
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution
+{
+public:
+	int myAtoi(string s)
+	{
+
+	}
+};
+
+int main()
+{
+	return 0;
+}
