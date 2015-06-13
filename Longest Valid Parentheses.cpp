319f35dd1c09e87f3e26d13c0b779e38a9f6118c
Zhao Wei <pkuzw@126.com>
2015-06-13 16:38:08 +0800

    Longest Valid Parentheses
    题目说明

---
+++ Longest Valid Parentheses.cpp
@@ -0,0 +1,13 @@
+﻿///@file	Longest Valid Parentheses
+/*
+			Given a string containing just the characters '(' and ')', find the length of the longest valid (well-formed)
+			
+			parentheses substring.
+
+			For "(()", the longest valid parentheses substring is "()", which has length = 2.
+
+			Another example is ")()())", where the longest valid parentheses substring is "()()", which has length = 4.
+*/
+///@author	zhaowei
+///@date	2015.06.13
+
