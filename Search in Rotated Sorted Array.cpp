ad4bbf4c9af583276fb00a9dca8653f74972e579
Zhao Wei <pkuzw@126.com>
2015-06-15 22:41:46 +0800

    Search in Rotated Sorted Array - Binary Search
    1.
    采用二分查找法先找到pivot值，然后再分成两部分[0...pivot]和[pivot+1...n-1]二分查找该值。时间复杂度为O(logn)，空间复杂度为O(1)；
    2. 添加注释。

--- Search in Rotated Sorted Array.cpp
+++ Search in Rotated Sorted Array.cpp
@@ -18,15 +18,19 @@ using namespace std;
 
 class Solution {
 public:
-	///@brief	查找数组中是否存在某元素
+	///@brief	查找已排序数组中是否存在某元素（默认为升序）
 	///@param	nums	数组
 	///@param	target	待查找值
 	///@return	如果数组中存在该值，则返回该值在数组中的下标，否则返回-1
+	///@author	zhaowei
+	///@date	2015.06.15
+	///@note	采用二分查找法先找到pivot值，然后再分成两部分[0...pivot]和[pivot+1...n-1]二分查找该值。时间复杂度为O(logn)，空间复杂度为O(1)
 	int search(vector<int>& nums, int target) {
-		int nums_len = nums.size();
+		
+		int nums_len = nums.size();	
 		int pivot = getPivot(nums, 0, nums_len-1);
 
-		if (pivot == nums_len-2 && nums[pivot] < nums[pivot+1])
+		if (pivot == nums_len-2 && nums[pivot] < nums[pivot+1])	//	特殊情况处理：对于没有翻转过的情形，getPivot的返回值是倒数第二个元素下标，需要后移一位
 		{
 			pivot++;
 		}
@@ -35,14 +39,15 @@ public:
 		int max_num = nums[pivot];
 
 		int min_num = 0;
+
 		if (pivot+1 != nums_len)
 		{
-			min_num = nums[pivot+1];
+			min_num = nums[pivot+1];	//	pivot的后一位就是最小值
 		}
 		else
-			min_num = nums[0];
+			min_num = nums[0];	//	循环移位
 
-		if (target > max_num || target < min_num)
+		if (target > max_num || target < min_num)	
 		{
 			return -1;
 		}
@@ -57,6 +62,13 @@ public:
 		return rslt;		
 	}
 
+	///@brief	二分查找Pivot值
+	///@param	nums	已排好序的数组
+	///@param	p	数组起始下标
+	///@param	r	数组终止下标
+	///@return	返回pivot值所在下标
+	///@author	zhaowei
+	///@date	2015.06.15
 	int getPivot(vector<int>& nums, int p, int r)
 	{
 		if (p == r)
@@ -74,6 +86,14 @@ public:
 		}		
 	}
 
+	///@brief	二分查找目标值
+	///@param	a	已排好序的数组
+	///@param	p	数组起始下标
+	///@param	r	数组终止下标
+	///@param	val	目标值
+	///@return	如果目标值val所在下标存在，则返回其下标；否则返回-1
+	///@author	zhaowei
+	///@date	2015.06.15
 	int binarySearch(vector<int>& a, int p, int r, int val)
 	{
 		if (p > r)
@@ -94,8 +114,7 @@ public:
 			return binarySearch(a, p, q-1, val);
 		}
 		return -1;
-	}
-	
+	}	
 };
 
 int main()
