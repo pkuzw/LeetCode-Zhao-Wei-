020d1a3df0bfffd405bb89d0be97d96dea8e9d0c
Zhao Wei <pkuzw@126.com>
2015-06-20 21:31:38 +0800

    Trapping Rain Water - Divide & Conquer
    1. 在高程值中从高往低遍历，先找到最高值和次高值，然后将次高值到最高值之间所夹矩形面积减去已有的高程值所占面积；
    2. 然后将此区间标记为已处理；寻找剩余区间的最高值和次高值，仿照之前的处理，要求矩形不能包含已处理过的区域；
    3. 根据主定理，该算法的时间复杂度为T(n) = 2T(n/2) +
    f(n)。因为是先出里的最大值和次大值区间将问题分成了左右两个子问题；然后处理中间的区间是找最大值和次大值的时间复杂度均为O(n)，计算高程值和的时间复杂度也为O(n)，故每个子问题的中间区域处理时间复杂度为f(n)
    = O(n)。又因为n^(log2_2) = f(n)，所以整个算法的时间复杂度为Theta(nlogn)，空间复杂度为O(1)。

--- Trapping Rain Water.cpp
+++ Trapping Rain Water.cpp
@@ -21,6 +21,9 @@ public:
 	///@return	返回其容积
 	///@note	在高程值中从高往低遍历，先找到最高值和次高值，然后将次高值到最高值之间所夹矩形面积减去已有的高程值所占面积。
 	//			然后将此区间标记为已处理；寻找剩余区间的最高值和次高值，仿照之前的处理，要求矩形不能包含已处理过的区域。
+	//			根据主定理，该算法的时间复杂度为T(n) = 2T(n/2) + f(n)。因为是先出里的最大值和次大值区间将问题分成了左右两个子问题；
+	//			然后处理中间的区间是找最大值和次大值的时间复杂度均为O(n)，计算高程值和的时间复杂度也为O(n)，故每个子问题的中间区域处理
+	//			时间复杂度为f(n) = O(n)。又因为n^(log2_2) = f(n)，所以整个算法的时间复杂度为Theta(nlogn)，空间复杂度为O(1).
 	int trap(vector<int>& height) 
 	{
 		int vol = calVolume(height, 0, height.size());
@@ -39,23 +42,23 @@ private:
 		int max_index = maxHeight(height, first, last);
 		int second_index = secondHeight(height, first, last, max_index);
 
-		if (max_index == -1 || second_index == -1)
+		if (max_index == -1 || second_index == -1)	// 如果区间范围不合法，则直接返回0
 		{
 			return 0;
 		}
 
-		int volume = abs(max_index - second_index - 1) * height[second_index];
-		int sum_height = 0;
-		if (max_index > second_index)
+		int volume = (abs(max_index - second_index) - 1) * height[second_index];		//	先计算矩形的面积
+		int sum_height = 0;	//	再计算高程值所占的面积
+		if (max_index > second_index)	//	区分两种情形：max_index在second_index之前或之后
 		{
 			sum_height = sum(height, second_index+1, max_index);
 			volume -= sum_height;
-			volume += calVolume(height, first, second_index+1);
+			volume += calVolume(height, first, second_index+1);	//	递归求解
 			volume += calVolume(height, max_index, last);
 		}
 		else
 		{
-			sum_height = sum(height, max_index+1, second_index);
+			sum_height = sum(height, max_index+1, second_index);	//	同上
 			volume -= sum_height;
 			volume += calVolume(height, first, max_index+1);
 			volume += calVolume(height, second_index, last);
@@ -101,16 +104,16 @@ private:
 		int second1 = height[first], second2 = height[first];
 		int second_index1 = first, second_index2 = first;
 
-		second_index1 = maxHeight(height, first, max_index);		
-
-		second_index2 = maxHeight(height, max_index+1, last);
+		second_index1 = maxHeight(height, first, max_index);	//	刨掉最大值，先计算左半区间的最大值
+			
+		second_index2 = maxHeight(height, max_index+1, last);	//	后计算右半区间的最大值
 		
 
-		if (second_index1 == -1)
+		if (second_index1 == -1)		//	最大值正好在first下标上
 		{
 			return second_index2;
 		}
-		else if (second_index2 == -1)
+		else if (second_index2 == -1)	//	最大值正好在last-1下标上
 		{
 			return second_index1;
 		}
@@ -125,13 +128,9 @@ private:
 	///@return	如果区间下标合法，则返回区间的高程和；否则返回-1
 	int sum(vector<int>& height, int first, int last)
 	{
-		if (first >= last)
-		{
-			return -1;
-		}
-
 		int count = 0;
-		for (int i = first; i != last; i++)
+		
+		for (int i = first; i < last; i++)
 		{
 			count += height[i];
 		}
@@ -142,18 +141,42 @@ private:
 int main()
 {
 	vector<int> ivec;	//0,1,0,2,1,0,1,3,2,1,2,1
-	ivec.push_back(0);
-	ivec.push_back(1);
-	ivec.push_back(0);
-	ivec.push_back(2);
-	ivec.push_back(1);
-	ivec.push_back(0);
-	ivec.push_back(3);
-	ivec.push_back(2);
-	ivec.push_back(1);
-	ivec.push_back(2);
-	ivec.push_back(1);
+//	sample 1:
+// 	ivec.push_back(0);
+// 	ivec.push_back(1);
+// 	ivec.push_back(0);
+// 	ivec.push_back(2);
+// 	ivec.push_back(1);
+// 	ivec.push_back(0);
+// 	ivec.push_back(1);
+// 	ivec.push_back(3);
+// 	ivec.push_back(2);
+// 	ivec.push_back(1);
+// 	ivec.push_back(2);
+// 	ivec.push_back(1);
 
+//	sample 2:
+// 	ivec.push_back(4);
+// 	ivec.push_back(3);
+// 	ivec.push_back(2);
+// 	ivec.push_back(1);
+// 	ivec.push_back(2);
+// 	ivec.push_back(3);
+// 	ivec.push_back(2);
+// 	ivec.push_back(1);
+// 	ivec.push_back(0);
+// 	ivec.push_back(1);
+// 	ivec.push_back(2);
+// 	ivec.push_back(3);
+// 	ivec.push_back(4);
+// 	ivec.push_back(4);
+
+//	sample 3:
+	ivec.push_back(0);
+	ivec.push_back(4);
+	ivec.push_back(4);
+	ivec.push_back(4);
+	ivec.push_back(0);
 	Solution slt;
 	
 	cout << slt.trap(ivec);
