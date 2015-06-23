f4771512fb70166011ef448ee56f678bedb2c4f8
Zhao Wei <pkuzw@126.com>
2015-06-23 21:50:27 +0800

    Jump Game II
    1. 对于最大值的下一步是0的情况没有处理，会造成死循环。

---
+++ Jump Game II.cpp
@@ -0,0 +1,70 @@
+﻿///@file	Jump Game II
+/*
+			Given an array of non-negative integers, you are initially positioned at the first index of the array.
+
+			Each element in the array represents your maximum jump length at that position.
+
+			Your goal is to reach the last index in the minimum number of jumps.
+
+			For example:
+			Given array A = [2,3,1,1,4]
+
+			The minimum number of jumps to reach the last index is 2. (Jump 1 step from index 0 to 1, then 3 steps to the last index.)
+*/
+///@author	zhaowei
+///@date	2015.06.23
+///@version	1.0
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+	///@brief	计算到达数组最后一个下标的最小跳跃步数
+	///@param	nums	数组，其中每一个元素值非负，表示能从该下标跳跃的最远距离
+	///@return	返回从数组开头到末尾的最少跳跃步数
+	///@note	解题思路：寻找从当前下标到其所能到达最远处这一范围内的最大值，然后将当前下标移动到最大值处继续搜索。直到到达数组末尾。
+	//			利用库文件<algorithm>中的max_element(first, last)函数来查找指定范围的最大值。时间复杂度为O(n^2)，空间复杂度为O(1)。
+	int jump(vector<int>& nums) 
+	{
+		vector<int>::iterator first = nums.begin();
+		int cnt = 1;
+		while (1)
+		{
+			vector<int>::iterator max_iter = max_element(first, first + *first + 1);	//	寻找指定范围内的最大值
+			cnt++;	//	计数器自增1
+			if (*max_iter >= nums.end()-first-1)		//	如果到达最后一个元素，则返回计数器
+			{
+				return cnt;
+			}
+			first += *max_iter;				//	将当前下标移动到最大值处
+		}		
+		return cnt;
+	}
+};
+
+int main()
+{
+	vector<int> ivec;
+	ivec.push_back(2);
+	ivec.push_back(2);
+	ivec.push_back(0);
+	ivec.push_back(1);
+// 	ivec.push_back(4);
+// 	ivec.push_back(1);
+// 	ivec.push_back(1);
+// 	ivec.push_back(1);
+// 	ivec.push_back(1);
+// 	ivec.push_back(1);
+// 	ivec.push_back(1);
+// 	ivec.push_back(1);
+// 	ivec.push_back(1);
+// 	ivec.push_back(1);
+
+	Solution slt;
+	cout << slt.jump(ivec) << endl;
+	return 0;
+}
