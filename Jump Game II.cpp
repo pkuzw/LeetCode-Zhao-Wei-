a48109940efc262caacd6e401e8d759fb60911d2
Zhao Wei <pkuzw@126.com>
2015-06-24 16:43:20 +0800

    Jump Game II - 边界情况
    1. 寻找从当前下标到其所能到达最远处这一范围内的下一跳最远的元素，然后将当前下标移动到该处继续搜索，直到到达数组末尾。
    2. 需要开辟两个长度为n的新数组，其中一个为原始数组的副本，用来划定寻找最远下一跳的范围；另外一个用来标记该元素是否被修改过。
    3. 时间复杂度为O(n^2)，空间复杂度为O(n)。

--- Jump Game II.cpp
+++ Jump Game II.cpp
@@ -25,59 +25,77 @@ class Solution {
 public:
 	///@brief	计算到达数组最后一个下标的最小跳跃步数
 	///@param	nums	数组，其中每一个元素值非负，表示能从该下标跳跃的最远距离
-	///@return	返回从数组开头到末尾的最少跳跃步数
-	///@note	解题思路：寻找从当前下标到其所能到达最远处这一范围内的最远的非零值，然后将当前下标移动到最远的非零值处继续搜索，直到到达数组末尾。
-	//			利用库文件<algorithm>中的max_element(first, last)函数来查找指定范围的最大值。时间复杂度为O(n^2)，空间复杂度为O(1)。
+	///@return	如果能够最后到达终点，则返回从数组开头到末尾的最少跳跃步数；否则返回-1
+	///@note	解题思路：寻找从当前下标到其所能到达最远处这一范围内的下一跳最远的元素，然后将当前下标移动到该处继续搜索，直到到达数组末尾。
+	//			需要开辟两个长度为n的新数组，其中一个为原始数组的副本，用来划定寻找最远下一跳的范围；另外一个用来标记该元素是否被修改过。
+	//			时间复杂度为O(n^2)，空间复杂度为O(n)。
 	int jump(vector<int>& nums) 
 	{
-		vector<int>::iterator first = nums.begin();
+		int first = 0;
 		int cnt = 0;
-		while (1)
+		vector<int> init_nums(nums);	//	复制一个初始数组
+		//	用于标记该数组元素是否修改过。因为修改之前的元素表示以其为中心所能到达的范围（一个相对值），修改之后的则表示该元素所能到达的
+		//	最远下标（一个绝对值），二者不能混淆
+		bool *modified = new bool[nums.size()];	
+		memset(modified, false, nums.size());
+		while (nums[first] < nums.size()-1)
 		{
-			vector<int>::iterator max_iter = max_element(first, first + *first + 1);	//	寻找指定范围内的最大值
-			cnt++;	//	计数器自增1
-			if (*max_iter >= nums.end()-first-1)		//	如果到达最后一个元素，则返回计数器
+			cnt++;
+			for (int i = first; i != first + init_nums[first] + 1; i++)	//	i的范围应该用初始数组来标定
 			{
-				return cnt;
+				if (!modified[i])
+				{
+					nums[i] += i;
+					modified[i] = true;
+				}				
 			}
-			first = nextPos(nums, first, *first);				//	将当前下标移动到最远的非0值处
-			if (first == nums.end())
+
+			first = getMax(nums, first, first+init_nums[first]+1);	//	为了防止寻找最大值范围超过数组本身，需要使用原始数组来计算
+
+			if (cnt > nums.size())
 			{
-				return cnt;
+				return -1;	//	表示陷入了死循环，无法到达终点。比如[1, 0, 1]
 			}
 		}		
-		return cnt;
+		delete []modified;
+		if (init_nums.size() == 1)	//	处理边界情况：只有一个元素的情形，返回0
+		{
+			return cnt;
+		}
+		else
+			return cnt+1;
 	}
 
-	vector<int>::iterator nextPos(vector<int>& nums, vector<int>::iterator first, int range)
+	///@brief	找到指定区间的最大值
+	///@param	nums	数组
+	///@param	p		起始下标
+	///@param	r		终止下标的后一个元素
+	///@return	返回该范围内的第一个最大值下标
+	int getMax(vector<int>& nums, int p, int r)
 	{
-		vector<int>::iterator max = first;
-		for (int i = 0; i <= range; i++)
+		int max_val = nums[p];
+		int max_indx = p;
+		for (int i = p; i != r; i++)
 		{
-			if (first >= nums.end() - i)
+			if (nums[i] > max_val)
 			{
-				return nums.end();
-			}
-			else
-			{
-				if (*(first + i) != 0 && first+i > max)
-				{
-					max = first + i;			
-				}
+				max_val = nums[i];
+				max_indx = i;
 			}
 		}
-		return max;
+		return max_indx;
 	}
+
 };
 
 int main()
 {
 	vector<int> ivec;
-	ivec.push_back(2);
-	ivec.push_back(3);
-	ivec.push_back(1);
-	ivec.push_back(1);
- 	ivec.push_back(4);
+	ivec.push_back(5);
+// 	ivec.push_back(1);
+// 	ivec.push_back(0);
+// 	ivec.push_back(0);
+//  ivec.push_back(4);
 // 	ivec.push_back(1);
 // 	ivec.push_back(1);
 // 	ivec.push_back(1);
