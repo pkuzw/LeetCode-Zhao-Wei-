4b6030c19c95d35bfe4eb2c0462ebd8be89af40e
Zhao Wei <pkuzw@126.com>
2015-06-25 16:22:15 +0800

    Permutations - Recursion
    1.
    利用递归的思想来解题。含有n和候选元素的数组可以分解成1个元素和n-1个元素。基本情况是只有1个元素的情形，只需要输出其本身；这时候返回上一级递归，假设有k个元素，如果选出的那1个元素还不是这k个元素中的最后一个，则向后选择下一个，并继续递归。直至最顶层选到了最后一个元素。
    2. 时间复杂度为O(n!)，空间复杂度为O(n^2)

--- Permutations.cpp
+++ Permutations.cpp
@@ -6,7 +6,7 @@
 			[1,2,3], [1,3,2], [2,1,3], [2,3,1], [3,1,2], and [3,2,1].
 */
 ///@author	zhaowei
-///@date	2015.06.24
+///@date	2015.06.25
 ///@version	1.0
 
 #include <iostream>
@@ -20,89 +20,81 @@ public:
 	///@brief	给定候选元素数组，计算所有可能的组合
 	///@param	nums	候选元素数组
 	///@return	返回所有可能的组合
-	///@note	把长度为n的原始数组简化为长度为1, 2, 3, 4, ... , n-1的，逐级递归。对于只有1个元素的，只需要返回其本身；只有2个的元素的，
-	//			返回两个交换过的情形，相当于选出一个，然后剩下一个元素，与处理一个元素的情形类似；3个元素的，每次选出1个元素，
-	//			剩下两个就像2各元素那样处理。向上依次类推。
+	///@note	利用递归的思想来解题。含有n和候选元素的数组可以分解成1个元素和n-1个元素。基本情况是只有1个元素的情形，只需要输出其本身；
+	//			这时候返回上一级递归，假设有k个元素，如果选出的那1个元素还不是这k个元素中的最后一个，则向后选择下一个，并继续递归。直至
+	//			最顶层选到了最后一个元素。时间复杂度为O(n!)，空间复杂度为O(n^2)。
 	///@author	zhaowei
-	///@date	2015.06.24
+	///@date	2015.06.25
 	vector<vector<int>> permute(vector<int>& nums)
 	{
 		vector<int> ivec;
-		getPermutate(nums, nums, ivec);
+		if (nums.empty())	//	如果原始数组为空，则返回空。其实如果不加这个判断语句，返回的是含有一个空数组的二维数组。
+		{
+			return rslt;
+		}
+		getPermutate(nums, ivec);
 		return rslt;
 	}
 
 	///@brief	计算所有可能的组合
 	///@param	nums	原始数组
-	///@param	pre_nums	递归的上一级数组
 	///@param	residue	可能的一种组合
 	///@return	void
 	///@note	我的想法是每次拿掉一个元素，将剩余的元素组成一个新数组，并且记录下当前拿掉的是第几个元素；当新数组为空时需要得到一个可能的组合；
-	//			此时返回上一级递归，再拿掉下一个元素。依次类推。但是程序还是写的有问题。
-	void getPermutate(vector<int> nums, vector<int> pre_nums, vector<int>& residue)
+	//			此时返回上一级递归，再拿掉下一个元素。依次类推。
+	void getPermutate(vector<int> nums, vector<int>& residue)
 	{
-		if (nums.empty())	//	如果
+		if (nums.empty())	//	如果数组为空，则递归结束
 		{
-			rslt.push_back(residue);
+			rslt.push_back(residue);	//	将一种可能的组合加入结果二维数组
 			return;
 		}
-		int indx = 0;
-		bool flag = false;	//	第一遍写入的标识符
-		while (indx != pre_nums.size())
+		int indx = 0;	//	标记当前选到的元素下标
+		while (indx != nums.size())	//	如果当前元素不是数组中的最后一个元素，则进入循环
 		{
-			vector<int> tmp;
-			if (!flag)
-			{
-				residue.push_back(nums[indx]);
-				for (int i = 0; i != nums.size(); i++)
-				{
-					if (i == indx)
-					{
-						continue;
-					}
-					tmp.push_back(nums[i]);
-				}
-				getPermutate(tmp, nums, residue);
-			}
-			
-			
-			
-			
-			int pre_size = pre_nums.size();
-			while (pre_size != 0)
-			{
-				residue.pop_back();
-				pre_size--;
-			}
-			indx++;	//	???这里有问题
-			
-			flag = true;
-			residue.push_back(pre_nums[indx]);
-			for (int i = 0; i != pre_nums.size(); i++)
+			vector<int> tmp;	//	用来保存除了当前选择的元素，剩余元素组成的数组
+			residue.push_back(nums[indx]);	//	将当前元素加入结果
+			for (int i = 0; i != nums.size(); i++)	//	生成剩余元素组成的数组
 			{
 				if (i == indx)
 				{
 					continue;
 				}
-				tmp.push_back(pre_nums[i]);
+				tmp.push_back(nums[i]);
 			}
-			getPermutate(tmp, pre_nums, residue);
-		}
-		
-		return;	
+			getPermutate(tmp, residue);		//	递归
+			residue.pop_back();				//	将结果中的元素弹出
+			indx++;							//	下标后移一位
+		}	
+		return;
 	}
 
 private:
-	vector<vector<int>> rslt;
+	vector<vector<int>> rslt;	//	结果数组
 };
 
 int main()
 {
 	Solution slt;
 	vector<int> nums;
-	nums.push_back(1);
-	nums.push_back(2);
-	nums.push_back(3);
-	slt.permute(nums);
+	vector<vector<int>> rslt;
+ 	nums.push_back(1);
+ 	nums.push_back(2);
+ 	nums.push_back(3);
+ 	nums.push_back(4);
+ 	nums.push_back(5);
+ 	nums.push_back(6);
+	rslt = slt.permute(nums);
+	
+	for (int i = 0; i != rslt.size(); i++)
+	{
+		for (int j = 0; j != rslt[i].size(); j++)
+		{
+			cout << rslt[i][j] << ' ';
+		}
+		cout << endl;
+	}
+
+	cout << rslt.size() << endl;
 	return 0;
 }
