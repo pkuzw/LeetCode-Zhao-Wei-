38832056f377a89ce22f67d33dbb81927994a392
Zhao Wei <pkuzw@126.com>
2015-07-04 17:31:14 +0800

    N-Queens
    1. 添加头部注释。

---
+++ N-Queens.cpp
@@ -0,0 +1,45 @@
+﻿///@file	N-Queens
+/*
+The n-queens puzzle is the problem of placing n queens on an n×n chessboard such that no two queens attack each other.
+
+
+Given an integer n, return all distinct solutions to the n-queens puzzle.
+
+Each solution contains a distinct board configuration of the n-queens' placement, where 'Q' and '.' both indicate a queen and an empty space respectively.
+
+For example,
+There exist two distinct solutions to the 4-queens puzzle:
+
+[
+[".Q..",  // Solution 1
+"...Q",
+"Q...",
+"..Q."],
+
+["..Q.",  // Solution 2
+"Q...",
+"...Q",
+".Q.."]
+]
+*/
+///@author	zhaowei
+///@date	2015.07.04
+///@version 1.0
+
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution
+{
+public:
+	vector<vector<string>> solveNQueens(int n) 
+	{
+
+	}
+};
+
+int main()
+{
+	return 0;
+}
