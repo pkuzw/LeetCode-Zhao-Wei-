e243f64decae95a5968d59da457516fc18737047
Zhao Wei <pkuzw@126.com>
2015-07-07 17:03:09 +0800

    Permutation Sequence
    1. 利用一个数组记录下k能够被分解成多少个子序列的排列，然后再根据这个数组来进行相应的排列。还没有想的很清楚。

--- Permutation Sequence.cpp
+++ Permutation Sequence.cpp
@@ -27,59 +27,77 @@ using namespace std;
 
 class Solution {
 public:
-	///@brief	计算出从1到n的全排列中第k个排列
-	///@param	n	元素数目，1 <= n <= 9
-	///@param	k	全排列中的第k个排列
-	///@return	返回第k个排列
-	///@note	利用"Permutations"中的算法：
-	//			利用递归的思想来解题。含有n和候选元素的数组可以分解成1个元素和n-1个元素。基本情况是只有1个元素的情形，只需要输出其本身；
-	//			这时候返回上一级递归，假设有k个元素，如果选出的那1个元素还不是这k个元素中的最后一个，则向后选择下一个，并继续递归。直至
-	//			最顶层选到了最后一个元素。因为只需计算出第k个排列，因此不需要计算所有的排列。在到达第k个时就返回。
-	//			计算出所有的排列后再返回其中的第k个。时间复杂度为O(n!)，空间复杂度为O(n^2)。
-	string getPermutation(int n, int k) 
+	///@note	利用一个数组记录下k能够被分解成多少个子序列的排列，然后再根据这个数组来进行相应的排列。还没有想的很清楚。
+	string getPermutation(int n, int k)
 	{
+		string str;
+		int cnt[10];	//	记录k能够被分解成多少个子排列。cnt[i]表示第1个数到第i个数所组成的全排列数目的倍数。
+		memset(cnt, 0, 10*sizeof(int));
+		initFractorial(n);
+
+		if (k > fractorial.back() || k < fractorial.front())
+		{
+			return str;
+		}
+
+		for (int i = fractorial.size()-1; i >= 0; i--)
+		{
+			if (k >= fractorial[i])
+			{
+				cnt[i] = k/fractorial[i];
+				k %= fractorial[i];
+			}
+		}
+
 		vector<int> nums;
 		for (int i = 1; i <= n; i++)
 		{
 			nums.push_back(i);
 		}
-		vector<int> ivec;
-		vector<int> rslt;
-		int cnt = 0;
-		getPermutate(nums, ivec, k, cnt, rslt);	//计算出所有可能的排列
 
-		string str;
-				
-		for (int i = 0; i != rslt.size(); i++)
-		{
-			str += rslt[i]+'0';
-		}
-		return str;
 	}
 
 private:
+	vector<int> fractorial;
+	///@brief	初始化1~9的阶乘数组
+	///@param	n	候选元素的数目
+	void initFractorial(int n)
+	{
+		int tmp = 1;
+		for (int i = 1; i <= n; i++)
+		{
+			tmp *= i;
+			fractorial.push_back(tmp);
+		}
+		return;
+	}
+
+	///@brief	给定候选元素数组，计算所有可能的组合
+	///@param	nums	候选元素数组
+	///@param	k		候选元素的全排列的第k个排列
+	///@return	返回候选元素的全排列的第k个排列
+	///@note	利用递归的思想来解题。含有n和候选元素的数组可以分解成1个元素和n-1个元素。基本情况是只有1个元素的情形，只需要输出其本身；
+	//			这时候返回上一级递归，假设有k个元素，如果选出的那1个元素还不是这k个元素中的最后一个，则向后选择下一个，并继续递归。直至
+	//			最顶层选到了最后一个元素。时间复杂度为O(n!)，空间复杂度为O(n^2)。
+	vector<int> permute(vector<int>& nums, int k)
+	{
+		vector<int> ivec;
+		
+		getPermutate(nums, ivec);
+		return ivec;
+	}
+
 	///@brief	计算所有可能的组合
 	///@param	nums	原始数组
 	///@param	residue	可能的一种组合
-	///@param	k		全排列中的第k个排列
-	///@param	rslt	保存有第k个排列的数组	
 	///@return	void
 	///@note	我的想法是每次拿掉一个元素，将剩余的元素组成一个新数组，并且记录下当前拿掉的是第几个元素；当新数组为空时需要得到一个可能的组合；
-	//			此时返回上一级递归，再拿掉下一个元素。依次类推。只需计算出第k个排列，不需要计算所有的排列，在到达第k个时就返回。
-	void getPermutate(vector<int> nums, vector<int>& residue, const int k, int &cnt, vector<int>& rslt)
+	//			此时返回上一级递归，再拿掉下一个元素。依次类推。
+	void getPermutate(vector<int> nums, vector<int>& residue)
 	{
-		if (!rslt.empty())
-		{
-			return;
-		}
 		if (nums.empty())	//	如果数组为空，则递归结束
 		{
-			cnt++;
-			if (cnt == k)
-			{
-				rslt = residue;
-				return;
-			}
+			rslt.push_back(residue);	//	将一种可能的组合加入结果二维数组
 			return;
 		}
 		int indx = 0;	//	标记当前选到的元素下标
@@ -95,12 +113,13 @@ private:
 				}
 				tmp.push_back(nums[i]);
 			}
-			getPermutate(tmp, residue, k, cnt, rslt);		//	递归
+			getPermutate(tmp, residue);		//	递归
 			residue.pop_back();				//	将结果中的元素弹出
 			indx++;							//	下标后移一位
 		}	
 		return;
-	}	
+	}
+	
 };
 
 int main()
