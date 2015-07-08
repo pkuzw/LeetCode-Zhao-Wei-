63b5e14f54e33166c98b28d5b8073b2fb7e9d4b1
Zhao Wei <pkuzw@126.com>
2015-07-08 16:58:10 +0800

    Minimum Path Sum
    1. 贪心法不可行。

---
+++ Minimum Path Sum.cpp
@@ -0,0 +1,31 @@
+﻿///@file	Minimum Path Sum
+/*
+Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right which minimizes 
+the sum of all numbers along its path.
+
+Note: You can only move either down or right at any point in time.
+*/
+///@author	zhaowei
+///@date	2015.07.08
+///@version 1.0
+
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+	///@brief	给定一个二维矩阵，计算从左上角到右下角和最小的路径
+	///@param	grid	二维矩阵
+	///@return	返回和最小的路径
+	///@note	贪心法不可行。
+	int minPathSum(vector<vector<int>>& grid) {
+
+	}
+};
+
+int main()
+{
+
+}
