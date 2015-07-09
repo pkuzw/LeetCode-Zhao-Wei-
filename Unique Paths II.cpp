c25b2c508a9eef0c1d8effe1810b181f49a67775
Zhao Wei <pkuzw@126.com>
2015-07-09 18:22:58 +0800

    Unique Paths II - DP
    1. 动态规划算法：设到点(i, j)共有dp[i][j]种不同的路径，则dp[i][j] = dp[i][j-1] +
    dp[i-1][j]。在遇到障碍物grid[i][j] == 1时，将dp[i][j] = 0；
    2.
    在初始化时生成一个所有元素为0的m+1*n+1的二维矩阵。对于左上角第一个元素要单独初始化，在双重循环中跳过左上角的dp[1][1]，否则其会由于多出来的一列和一行，被再次置为0；
    2. 时间复杂度为O(m*n)，空间复杂度为O(m*n)。

--- Unique Paths II.cpp
+++ Unique Paths II.cpp
@@ -17,7 +17,7 @@ There is one obstacle in the middle of a 3x3 grid as illustrated below.
 The total number of unique paths is 2.
 */
 ///@author	zhaowei
-///@date	2015.07.08
+///@date	2015.07.09
 ///@version	1.0
 
 #include <iostream>
@@ -29,14 +29,78 @@ public:
 	///@brief	计算有障碍物的路径总数
 	///@param	obstacleGrid	用二维数组表示障碍物分布，其中1为障碍物，0为通路
 	///@return	返回路径总数
-	///@note	根据障碍物将棋盘划分成若干个小的不含有障碍物的矩形。通过这些不含有障碍物的矩形，来计算可能的路径总数。还没有想得很清楚。
+	///@note	动态规划算法：设到点(i, j)共有dp[i][j]种不同的路径，则dp[i][j] = dp[i][j-1] + dp[i-1][j]。在遇到障碍物grid[i][j] == 1
+	//			时，将dp[i][j] = 0。在初始化时生成一个所有元素为0的m+1*n+1的二维矩阵。对于左上角第一个元素要单独初始化，在双重循环中跳过
+	//			左上角的dp[1][1]，否则其会由于多出来的一列和一行，被再次置为0.时间复杂度为O(m*n)，空间复杂度为O(m*n)。
 	int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) 
 	{
+		if (obstacleGrid.empty())	return 0;	//	除去无效棋盘
+		int m = obstacleGrid.size();
 
+		if (obstacleGrid[0].empty())	return 0;
+		int n = obstacleGrid[0].size();
+		
+		vector<vector<int>> dp(m+1, vector<int>(n+1, 0));	//	初始化dp二维数组
+		dp[1][1] = obstacleGrid[0][0] == 1 ? 0 : 1;			//	对于左上角的第一个dp值要根据棋盘上在该处是否有障碍物来单独初始化
+
+		for (int i = 1; i != m+1; i++)
+		{
+			for (int j = 1; j != n+1; j++)
+			{				
+				if (i == 1 && j == 1)	continue;	//	跳过处理dp[1][1]
+				
+				if (obstacleGrid[i-1][j-1] == 1)
+					dp[i][j] = 0;
+				else
+					dp[i][j] = dp[i][j-1] + dp[i-1][j];				
+			}
+		}
+		return dp[m][n];
 	}
 };
 
 int main()
 {
+	vector<vector<int>> obs;
+	vector<int> line;
+	line.push_back(0);
+	line.push_back(0);
+	line.push_back(0);	
+	line.push_back(0);
+	line.push_back(0);
+	line.push_back(0);	
+	obs.push_back(line);
+
+	line.clear();
+	line.push_back(0);
+	line.push_back(0);
+	line.push_back(1);	
+	line.push_back(0);
+	line.push_back(0);
+	line.push_back(0);	
+	obs.push_back(line);
+
+	line.clear();
+	line.push_back(0);
+	line.push_back(0);
+	line.push_back(0);
+	line.push_back(0);
+	line.push_back(1);
+	line.push_back(0);	
+	obs.push_back(line);
+
+
+	line.clear();
+	line.push_back(0);
+	line.push_back(1);
+	line.push_back(0);
+	line.push_back(0);
+	line.push_back(0);
+	line.push_back(0);	
+	obs.push_back(line);
+
+	Solution slt;
+	cout << slt.uniquePathsWithObstacles(obs);
+	cout << endl;
 	return 0;
 }
