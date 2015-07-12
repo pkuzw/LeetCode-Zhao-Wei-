c5a4fee538bf44a792c227d3d86341131a8f060c
Zhao Wei <pkuzw@126.com>
2015-07-12 18:21:17 +0800

    Simplify Path - Stack
    0. 路径的简化规则：
    1. 去掉位于字符串末尾的'/'；
    2. '/.'相当于保持在当前目录；
    3. '/..'是返回上一级目录；
    4. 多个'/'连续出现相当于一个'/'；
    5. '/xxx'就是一个目录，注意这里xxx可以是连续的三个'.'；
    6. 在根目录时无法再向上，则无论出现多少返回上级目录的命令，都应该保持在根目录。
    7.
    首先，将路径中连续出现的'/'简化成一个'/'；其次，如果最后一个字符是'/'，则也需要删去；再次，将由'/'分隔的子串提取出来压栈，如果遇到'/.'，则不压栈也不弹栈，维持现状，如果遇到'/..'，则弹栈，如果此时栈为空，则先压入'/'（根目录），再弹栈；如果遇到其他合法目录名，则压栈。最后在栈中保留的子串从后向前组成了简化后的路径名；
    8. 时间复杂度为O(n)，空间复杂度为O(1)。

--- Simplify Path.cpp
+++ Simplify Path.cpp
@@ -7,11 +7,12 @@ path = "/home/", => "/home"
 path = "/a/./b/../../c/", => "/c"
 */
 ///@author	zhaowei
-///@date	2015.07.11
-///@version	1.0
+///@date	2015.07.12
+///@version	1.1
 
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -26,44 +27,92 @@ public:
 	//			3. '/..'是返回上一级目录；
 	//			4. 多个'/'连续出现相当于一个'/'；
 	//			5. '/xxx'就是一个目录，注意这里xxx可以是连续的三个'.'；
-	//			首先，将路径中连续出现的'/'简化成一个'/'；其次，如果最后一个字符是'/'，则也需要删去；再次，将由'/'分隔的子串提取出来压栈，如果遇到'/.'，则不
-	//			压栈也不弹栈，维持现状，如果遇到'/..'，则弹栈，如果遇到其他合法目录名，则压栈。最后在栈中保留的子串从后向前组成了简化后的路径名；
-	//			时间复杂度为O(n)，空间复杂度为O(1)
-	string simplifyPath(string path) {
+	//			6. 在根目录时无法再向上，则无论出现多少返回上级目录的命令，都应该保持在根目录。
+	//			首先，将路径中连续出现的'/'简化成一个'/'；其次，如果最后一个字符是'/'，则也需要删去；再次，将由'/'分隔的子串提取出来压栈，
+	//			如果遇到'/.'，则不压栈也不弹栈，维持现状，如果遇到'/..'，则弹栈，如果此时栈为空，则先压入'/'（根目录），再弹栈；
+	//			如果遇到其他合法目录名，则压栈。最后在栈中保留的子串从后向前组成了简化后的路径名；
+	//			时间复杂度为O(n)，空间复杂度为O(1)。
+	string simplifyPath(string path) 
+	{		
 		string rslt;
-		int slash_cnt = 0;
-		int last_slash = 0;
-		for (int i = path.length()-1; i >= 0; i--)
-		{
+
+		eraseRedundantSlash(path);	//	删除冗余的'/'
+		eraseLastSlash(path);	//	删除位于字符串末尾的'/'
+
+		vector<string> stck;
+		vector<int> slash_indx;
+
+		for (int i = 0; i != path.length(); i++)	//	记录下'/'的下标
+		{		
 			if (path[i] == '/')
 			{
-				slash_cnt++;
-				if (slash_cnt == 1)
-				{
-					path = path.substr(0, i);
-				}
-				else if (slash_cnt == 2)
-				{
-					last_slash = i;
-					path = path.substr(i, path.length() - i);
-					return path;
-				}
+				slash_indx.push_back(i);
 			}
 		}
+
+		for (int i = 0; i != slash_indx.size(); i++)
+		{
+			string dname;
+			if (i < slash_indx.size()-1)		
+				dname = path.substr(slash_indx[i], slash_indx[i+1]-slash_indx[i]);			
+			else
+				dname = path.substr(slash_indx[i], path.length()-slash_indx[i]+1);
+
+			if (dname == "/..")
+			{
+				if (stck.empty())	//	当容器为空时，不允许弹出操作。又因为到达根目录后无法再向上，故先添加一个根目录，再弹出。
+				{
+					stck.push_back("/");
+				}
+				stck.pop_back();
+			}
+			else if (dname == "/.")
+			{
+				continue;
+			}
+			else
+			{
+				stck.push_back(dname);
+			}						 
+		}
+
+		for (int i = stck.size()-1; i >= 0; i--)
+		{
+			rslt = stck[i] + rslt;
+		}
+		if (rslt.empty())
+		{
+			rslt = "/";
+		}
+		return rslt;
 	}
+
 private:
 	///@brief	删除连续出现的冗余'/'
 	///@param	s	字符串
 	///@return	无
 	void eraseRedundantSlash(string &s)
 	{
-		for (string::iterator i = s.begin(); i != s.end()-1; i++)
+		for (int i = 0; i <= s.length()-1; i++)
 		{
-			while (*i == '/' && *i+1 == '/')
+			while (i+1 < s.length() && s[i] == '/' && s[i+1] == '/')
 			{
-				s.erase(i+1);
+				s.erase(i+1, 1);	//	删除从下标i+1处开始的1个字符。
 			}
 		}
+		return;
+	}
+
+	///@brief	删除位于字符串最末尾的'/'
+	///@param	s	字符串
+	///@return	无
+	void eraseLastSlash(string &s)
+	{
+		if (s.back() == '/')
+		{
+			s = s.substr(0, s.length()-1);
+		}
+		return;
 	}
 };
 
