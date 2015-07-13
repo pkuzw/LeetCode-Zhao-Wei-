8a2e088e74bcc5b05fc46d186230872ad1591637
Zhao Wei <pkuzw@126.com>
2015-07-13 19:27:54 +0800

    Sort Color - Quick Sort
    1. 写快速排序（《算法导论》版），时间复杂度为O(nlogn)，空间复杂度为O(1)。

---
+++ Sort Color.cpp
@@ -0,0 +1,90 @@
+﻿///@file	Sort Color
+/*
+Given an array with n objects colored red, white or blue, sort them so that objects of the same color are adjacent, 
+with the colors in the order red, white and blue.
+
+Here, we will use the integers 0, 1, and 2 to represent the color red, white, and blue respectively.
+*/
+///@author	zhaowei
+///@date	2015.07.13
+///@version 1.0
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	///@brief	对数组nums进行排序
+	///@param	nums	数组
+	///@return  无
+	///@note	手写快速排序（《算法导论》版），时间复杂度为O(nlogn)，空间复杂度为O(1)。
+	void sortColors(vector<int>& nums) {
+		if (nums.empty())
+		{
+			return;
+		}
+		quickSort(nums, 0, nums.size()-1);
+		return;
+	}
+
+private:
+	///@brief	快速排序
+	///@param	nums	待排序数组
+	///@param	p		数组起始下标
+	///@param	r		数组终止下标
+	///@return  无
+	///@note	快速排序的思想是分治，在指定范围内的数组内找到一个轴值，根据其将数组划分成两部分，左半部分小于等于轴值，有半部分大于轴值。
+	void quickSort(vector<int>& nums, int p, int r)
+	{
+		if (p >= r)
+		{
+			return;
+		}
+		int q = partition(nums, p, r);
+		quickSort(nums, p, q-1);
+		quickSort(nums, q+1, r);
+	}
+
+	///@brief	找到轴值，并根据轴值进行划分
+	///@param	nums	待排序数组
+	///@param	p		数组起始下标
+	///@param	r		数组终止下标
+	///@return  返回轴值所在下标
+	///@note	nums[0...i]是小于等于轴值x的部分，nums[i+1...j]是大于轴值的部分，nums[j+1...r]是未判定的部分
+	int partition(vector<int>& nums, int p, int r)
+	{
+		int x = nums[r];
+		int i = p-1;
+		for (int j = p; j < r; j++)
+		{
+			if (nums[j] <= x)
+			{
+				i++;
+				swap(nums[i], nums[j]);
+			}
+		}
+		swap(nums[i+1], nums[r]);
+		return i+1;
+	}
+};
+
+int main()
+{
+	vector<int> test;
+	for (int i = 0; i <= 3; i++)
+	{
+		test.push_back(0);
+		test.push_back(1);
+		test.push_back(2);
+	}
+	Solution slt;
+	slt.sortColors(test);
+	for (int i = 0; i != test.size(); i++)
+	{
+		cout << test[i] << " ";
+	}
+	cout << endl;
+	return 0;
+}
