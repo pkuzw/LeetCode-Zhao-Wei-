50af26a4385483dfca93107318c317d9f590b7a2
Zhao Wei <zhaowei.pku@gmail.com>
2015-07-15 23:57:54 +0800

    Edit Distance
    1. 写文件头部。

---
+++ Edit Distance.cpp
@@ -0,0 +1,35 @@
+﻿///@file	Edit Distance
+/*
+Given two words word1 and word2, find the minimum number of steps required to convert word1 to word2. (each operation is counted as 1 step.)
+
+You have the following 3 operations permitted on a word:
+
+a) Insert a character
+b) Delete a character
+c) Replace a character
+*/
+///@author	zhaowei
+///@date	2015.07.15
+///@version	1.0
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+	///@brief	给定两个单词，将单词1转换成单词2，转换包括三种方法：1. 插入一个字符；2. 删除一个字符；3. 替换一个字符，计算需要的最少步数。
+	///@param	word1	单词1
+	///@param	word2	单词2
+	///@return	返回转换需要的最小步数
+	///@note	
+	int minDistance(string word1, string word2) {
+
+	}
+};
+
+int main()
+{
+	return 0;
+}
