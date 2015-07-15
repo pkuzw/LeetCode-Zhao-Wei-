23903b720ff7b64dd3f1c0db46559a957250e354
Zhao Wei <pkuzw@126.com>
2015-07-15 18:58:18 +0800

    Remove Duplicates from Sorted Array II - Enumeration & Loop invariant Update
    1. 设置一个统计重复元素出现次数的变量，每当重复元素出现的次数超过2，就将其后面的元素向前移动一位，此时重复元素的下标需要随之改变；
    2. 时间复杂度为O(n)，空间复杂度为O(1)。

---
+++ Remove Duplicates from Sorted Array II.cpp
@@ -0,0 +1,88 @@
+﻿///@file	Remove Duplicates from Sorted Array II 
+/*
+Follow up for "Remove Duplicates":
+What if duplicates are allowed at most twice?
+
+For example,
+Given sorted array nums = [1,1,1,2,2,3],
+
+Your function should return length = 5, with the first five elements of nums being 1, 1, 2, 2 and 3. 
+It doesn't matter what you leave beyond the new length.
+*/
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	///@brief	在一个已经排好序的数组中，如果只允许同一个元素出现两次，生成新数组并计算新数组的长度
+	///@param	nums	已经排好序的数组
+	///@return	新数组的长度
+	///@note	设置一个统计重复元素出现次数的变量，每当重复元素出现的次数超过2，就将其后面的元素向前移动一位，此时重复元素的下标需要随之改变。
+	//			时间复杂度为O(n)，空间复杂度为O(1)。
+	int removeDuplicates(vector<int>& nums) {
+		int len = nums.size();	//	数组的长度，随着重复元素被删除动态变化
+		if (len <= 2)	//	当数组的长度小于等于2时，直接返回数组的长度
+		{
+			return len;
+		}
+				
+		int dup_cnt = 1;
+		for (int i = 0; i < len-1; i++)
+		{
+			int j = i+1;
+			dup_cnt = 1;	//	统计元素重复出现的次数
+			int delete_times = 0;	//	重复元素的总次数，即数组向前移动过多少次
+			while (j-delete_times < len && nums[i] == nums[j-delete_times])	//	因为发现超过两次的重复元素后，都将后面的元素向前移动一位，故相应的j下标也需要减去移动的次数
+			{
+				dup_cnt++;
+				j++;
+				if (dup_cnt > 2)
+				{
+					deleteElement(nums, j-delete_times-1);
+					len--;
+					delete_times++;
+				}
+			}
+			i = j-delete_times-1;
+		}	
+		return len;
+	}
+
+private:
+	///@brief	将数组中指定下标后的所有元素向前移动一位
+	///@param	nums	数组
+	///@param	indx	指定下标
+	///@return	无
+	void deleteElement(vector<int> &nums, const int indx)
+	{
+		for (int i = indx+1; i != nums.size(); i++)
+		{
+			nums[i-1] = nums[i];
+		}
+		return;
+	}
+};
+
+int main()
+{
+	Solution slt;
+	vector<int> test;
+ 	test.push_back(1);
+ 	test.push_back(2);
+// 	test.push_back(2);
+// 	test.push_back(3);
+//   	test.push_back(3);
+// 	test.push_back(4);
+// 	test.push_back(4);
+// 	test.push_back(4);
+	int new_len = slt.removeDuplicates(test);
+	cout << new_len << endl;
+	for (int i = 0; i != new_len; i++)
+	{
+		cout << test[i] << ' ';
+	}
+	cout << endl;
+	return 0;
+}
