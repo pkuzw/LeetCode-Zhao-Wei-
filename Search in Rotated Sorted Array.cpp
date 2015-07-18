16ba08f7214ca3413f8dbc6d71c0e37626c9513b
Zhao Wei <pkuzw@126.com>
2015-07-19 00:03:35 +0800

    Search in Rotated Sorted Array - 寻找哪一半有序
    1. 当有序数组被颠倒后，关键是找到哪一部分是有序的。通过比较中间值和数组首元素，来判断左半部分还是右半部分有序。
    2. 如果数组首部元素小于中间值，则左半部分有序；
    3. 如果数组首部元素大于中间值，则右半部分有序；
    4. 如果数组首部元素等于中间值，则修改左边界，自增1，因为mid = (l+r)/2，所以中间值也会改变；
    5. 时间复杂度为O(logn)，空间复杂度为O(1)。

--- Search in Rotated Sorted Array.cpp
+++ Search in Rotated Sorted Array.cpp
@@ -10,7 +10,7 @@
 */
 ///@author	zhaowei
 ///@date	2015.06.15
-///@version 1.0
+///@version 2.0
 
 #include <iostream>
 #include <vector>
@@ -18,6 +18,58 @@ using namespace std;
 
 class Solution {
 public:
+	///@brief	查找已排序数组中是否存在某元素（默认为升序）
+	///@param	nums	数组
+	///@param	target	待查找值
+	///@return	如果数组中存在该值，则返回该值下标，否则返回-1
+	///@author	zhaowei
+	///@date	2015.07.18
+	///@note	当有序数组被颠倒后，关键是找到哪一部分是有序的。通过比较中间值和数组首元素，来判断左半部分还是右半部分有序。
+	//			如果数组首部元素小于中间值，则左半部分有序；
+	//			如果数组首部元素大于中间值，则右半部分有序；
+	//			如果数组首部元素等于中间值，则修改左边界，自增1，因为mid = (l+r)/2，所以中间值也会改变。
+	//			时间复杂度为O(logn)，空间复杂度为O(1)。
+	int search_2(vector<int>& nums, int target)
+	{
+		int l = 0;
+		int r = nums.size()-1;
+		while (l <= r)
+		{
+			int m = (l+r)/2;
+			if (nums[m] == target)	//	找到目标值则返回true
+			{
+				return m;
+			}
+			if (nums[l] < nums[m])	//	左半部分有序
+			{
+				//	target的范围应该是左闭右开
+				if (nums[l] <= target && target < nums[m])	//	如果目标值在左半部分，则修改右边界
+				{
+					r = m-1;
+				}
+				else	//	否则修改左边界
+				{
+					l = m+1;
+				}
+			}
+			else if (nums[m] < nums[l])	//	右半部分有序
+			{
+				//	target的范围应该是左开右闭
+				if (nums[m] < target && target <= nums[r])	//	如果目标值在右半部分，则修改左边界
+				{
+					l = m+1;
+				}
+				else
+				{
+					r = m-1;		//	否则修改右边界
+				}
+			}
+			else
+				l++;	//	如果中间值恰好等于首部元素，则更新左边界，以使中间值下标变化
+		}
+		return -1;
+	}
+
 	///@brief	查找已排序数组中是否存在某元素（默认为升序）
 	///@param	nums	数组
 	///@param	target	待查找值
@@ -126,19 +178,15 @@ int main()
 		vector<int> nums;
 		for (int i = 0; i < j; i++)
 		{
-			nums.push_back(i+10-j);
-			nums.push_back(i+10-j);
-			nums.push_back(i+10-j);			
+			nums.push_back(i+10-j);	
 		}
 
 		for (int i = 0; i < 10-j; i++)
 		{
 			nums.push_back(i);
-			nums.push_back(i);
-			nums.push_back(i);
 		}
 
-		for (int i = 0; i < 30; i++)
+		for (int i = 0; i < 10; i++)
 		{
 			cout << nums[i] << ' ';
 		}
@@ -146,7 +194,7 @@ int main()
 		for (int i = 0; i < 12; i++)
 		{
 			int target = i;
-			cout << target << " " << slt.search(nums, target);
+			cout << target << " " << slt.search(nums, target) << ' ' << slt.search_2(nums, target);
 			cout << endl;
 		}
 
