f8a6acd3618546b37ef9280bf3378e3a36e83097
Zhao Wei <pkuzw@126.com>
2015-07-18 16:11:57 +0800

    Word Serach - TLE
    1.
    遍历矩阵，通过访问它的上下左右邻居来对目标单词进行匹配。需要两个一维数组来存储已经匹配的字符在矩阵中的行列号，便于后续字符不匹配时进行回溯。还需要4个m*n的bool型二维数组来存储每一个点能否向相邻元素移动。设矩阵尺寸为m*n，单词长度为k，则算法时间复杂度为O(mnk)，空间复杂度为O(mn)。OJ报TLE。

--- Word Search.cpp
+++ Word Search.cpp
@@ -18,8 +18,8 @@ word = "SEE", -> returns true,
 word = "ABCB", -> returns false.
 */
 ///@author	zhaowei
-///@date	2015.07.14
-///@version	1.0
+///@date	2015.07.18
+///@version	1.1
 
 #include <vector>
 #include <string>
@@ -27,14 +27,15 @@ word = "ABCB", -> returns false.
 
 using namespace std;
 
-class Solution {
+class Solution1 {
 public:
 	///@brief	判断在二维字符矩阵中是否存在指定单词
 	///@param	board	二维字符数组
 	///@param	word	单词
 	///@return	如果能够通过上下左右相邻的方式找到该单词，则返回true；否则返回false
-	///@note	遍历矩阵，通过访问它的上下左右邻居来对目标单词进行匹配。需要两个一位数组来存储已经匹配的字符在矩阵中的行列号，便于后续字符不匹配
-	//			时进行回溯。还需要4个m*n的bool型二维数组来存储每一个点能否向相邻元素移动。设矩阵尺寸为m*n，单词长度为k，则算法时间复杂度为O(mnk)，空间复杂度为O(mn)。
+	///@note	遍历矩阵，通过访问它的上下左右邻居来对目标单词进行匹配。需要两个一维数组来存储已经匹配的字符在矩阵中的行列号，
+	//			便于后续字符不匹配时进行回溯。还需要4个m*n的bool型二维数组来存储每一个点能否向相邻元素移动。设矩阵尺寸为m*n，
+	//			单词长度为k，则算法时间复杂度为O(mnk)，空间复杂度为O(mn)。
 	bool exist(vector<vector<char>>& board, string word) {
 		if (board.empty())
 		{
@@ -299,6 +300,246 @@ private:
 	vector<vector<bool>> left;
 	vector<vector<bool>> right;
 	
+	int width;	//	矩阵的宽
+	int height;	//	矩阵的高
+};
+
+class Solution
+{
+public:
+	///@brief	判断在二维字符矩阵中是否存在指定单词
+	///@param	board	二维字符数组
+	///@param	word	单词
+	///@return	如果能够通过上下左右相邻的方式找到该单词，则返回true；否则返回false
+	///@note	遍历矩阵，通过访问它的上下左右邻居来对目标单词进行匹配。需要两个一维数组来存储已经匹配的字符在矩阵中的行列号，
+	//			便于后续字符不匹配时进行回溯。还需要4个m*n的bool型二维数组来存储每一个点能否向相邻元素移动。设矩阵尺寸为m*n，
+	//			单词长度为k，则算法时间复杂度为O(mnk)，空间复杂度为O(mn)。
+	bool exist(vector<vector<char>>& board, string word) {
+		if (board.empty())
+		{
+			if (word.empty())	return true;
+			else	return false;
+		}
+		height = board.size();	//	矩阵行数
+
+		if (board[0].empty())
+		{
+			if (word.empty())	return true;
+			else	return false;
+		}
+		width = board[0].size();	//	矩阵列数
+
+		if (word.empty() && (height != 0 && width != 0))
+		{
+			return false;
+		}
+
+		vector<int> x_indx, y_indx;	//	保存已经匹配的字符在矩阵中的行列号，以便进行回溯
+
+		for (int i = 0; i != height; i++)
+		{
+			for (int j = 0; j != width; j++)
+			{
+				resetUp();	//	每次更换匹配起点都要重置方向标志矩阵
+				resetDown();
+				resetLeft();
+				resetRight();
+				x_indx.clear();	//	同时重置行列号栈
+				y_indx.clear();
+				int k = 0;	//	标记单词的下标
+				int u = i, v = j;	
+				while (k < word.length() && k >= 0)	//	以board[i][j]为起点进行匹配
+				{
+					if (word[k] == board[u][v])	//如果匹配则进行四周的探索
+					{
+						x_indx.push_back(u);	//	将匹配字符的行列号入栈
+						y_indx.push_back(v);
+						k++;	
+						if (up[u][v])
+						{						
+							u--;
+							down[u][v] = false;	//	其上一个元素则不可再次到达
+						}
+						else if (right[u][v])
+						{						
+							v++;
+							left[u][v] = false;	//	同上
+						}
+						else if (down[u][v])
+						{						
+							u++;
+							up[u][v] = false;	//	同上
+						}
+						else if (left[u][v])
+						{
+							v--;
+							right[u][v] = false;//	同上
+						}						
+						else
+						{
+							if (!x_indx.empty() && !y_indx.empty())	//	回溯并将这条路堵死，置为false
+							{
+								if (u - x_indx.back() == 1 && v - y_indx.back() == 0)
+									down[u-1][v] = false;
+								else if (u - x_indx.back() == -1 && v - y_indx.back() == 0)
+									up[u+1][v] = false;
+								else if (u - x_indx.back() == 0 && v - y_indx.back() == 1)
+									right[u][v-1] = false;
+								else if (u - x_indx.back() == 0 && v - y_indx.back() == -1)
+									left[u][v+1] = false;
+								else if (u == x_indx.back() && v == y_indx.back())
+								{
+									x_indx.pop_back();
+									y_indx.pop_back();
+									k--;
+									if (x_indx.empty() && y_indx.empty())
+										break;							
+
+									if (u - x_indx.back() == 1 && v - y_indx.back() == 0)	//	将这条路堵死，置为false
+										down[u-1][v] = false;
+									else if (u - x_indx.back() == -1 && v - y_indx.back() == 0)
+										up[u+1][v] = false;
+									else if (u - x_indx.back() == 0 && v - y_indx.back() == 1)
+										right[u][v-1] = false;
+									else if (u - x_indx.back() == 0 && v - y_indx.back() == -1)
+										left[u][v+1] = false;									
+								}
+								u = x_indx.back();
+								v = y_indx.back();
+								x_indx.pop_back();
+								y_indx.pop_back();
+								k--;
+							}	
+							else
+								break;
+						}
+					}
+					else
+					{
+						if (!x_indx.empty() && !y_indx.empty())
+						{
+							if (u - x_indx.back() == 1 && v - y_indx.back() == 0)
+								down[u-1][v] = false;
+							else if (u - x_indx.back() == -1 && v - y_indx.back() == 0)
+								up[u+1][v] = false;
+							else if (u - x_indx.back() == 0 && v - y_indx.back() == 1)
+								right[u][v-1] = false;
+							else if (u - x_indx.back() == 0 && v - y_indx.back() == -1)
+								left[u][v+1] = false;
+							u = x_indx.back();
+							v = y_indx.back();
+							x_indx.pop_back();
+							y_indx.pop_back();
+							k--;
+						}	
+						else
+							break;
+					}									
+				}
+				if (k == word.length())
+				{
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+private:
+	///@brief	重置向上的标志矩阵
+	void resetUp()
+	{
+		up.clear();
+		bool flg = false;
+		vector<bool> line;
+		for (int i = 0; i != width; i++)
+		{
+			line.push_back(flg);
+		}
+		up.push_back(line);	
+		line.clear();
+
+		flg = true;
+		for (int i = 0; i != width; i++)
+		{
+			line.push_back(flg);
+		}
+		for (int i = 0; i != height-1; i++)
+		{
+			up.push_back(line);
+		}
+		return;
+	}
+
+	///@brief	重置向下的标志矩阵
+	void resetDown()
+	{
+		down.clear();
+		bool flg = true;
+		vector<bool> line;
+		for (int i = 0; i != width; i++)
+		{
+			line.push_back(flg);
+		}
+		for (int i = 0; i != height-1; i++)
+		{
+			down.push_back(line);
+		}
+		line.clear();
+
+		flg = false;
+		for (int i = 0; i != width; i++)
+		{
+			line.push_back(flg);
+		}
+		down.push_back(line);	
+		return;
+	}
+
+	///@brief	重置向左的标志矩阵
+	void resetLeft()
+	{
+		left.clear();
+		vector<bool> line;
+		bool flg = false;
+		line.push_back(flg);
+		flg = true;
+		for (int i = 0; i != width-1; i++)
+		{
+			line.push_back(flg);
+		}
+		for (int i= 0; i != height; i++)
+		{
+			left.push_back(line);	//	初始化向左的标志矩阵
+		}
+		return;
+	}
+
+	///@brief	重置向右的标志矩阵
+	void resetRight()
+	{
+		right.clear();
+		vector<bool> line;
+		bool flg = true;
+		for (int i = 0; i != width-1; i++)
+		{
+			line.push_back(flg);
+		}
+		flg = false;
+		line.push_back(flg);
+		for (int i= 0; i != height; i++)
+		{
+			right.push_back(line);	//	初始化向右的标志矩阵
+		}
+		return;
+	}
+
+	//	方位标志矩阵，用来表示在点board[i][j]处能否向各个方向移动
+	vector<vector<bool>> up;
+	vector<vector<bool>> down;
+	vector<vector<bool>> left;
+	vector<vector<bool>> right;
+
 	int width;	//	矩阵的宽
 	int height;	//	矩阵的高
 };
