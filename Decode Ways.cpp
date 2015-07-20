4ba76be1e037409cb1e4ee2fbe8a996fae51ed36
Zhao Wei <pkuzw@126.com>
2015-07-20 21:06:48 +0800

    Decode Ways - DP
    1. 动态归划：用dp[i]表示字符串s[0..i-1]能够解码的方法数。初始条件dp[0] = 1, dp[1] = (s[0] ==
    '0') ? 0 : 1，递推方程为dp[i] = (s[i-1] <= '9' && s[i-1] > '0') ? dp[i-1] : 0)
    +
    ((s[i-2] == '2' && s[i-1] <= '6' && s[i-1] >= '0') || (s[i-1] == '1')) ?
    dp[i-2] : 0；
    2. 时间复杂度为O(n)，空间复杂度为O(n)。

--- Decode Ways.cpp
+++ Decode Ways.cpp
@@ -15,10 +15,11 @@ The number of ways decoding "12" is 2.
 */
 ///@author	zhaowei
 ///@date	2015.07.20
-///@version	1.0
+///@version	2.0
 
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -33,12 +34,52 @@ public:
 				3. 当字符串有2个字符时，如果这两个字符能够合并解译且也能够分开解译，则方法数自增2；如果这两个字符只能单独解译或者只能放在
 				一起解译，方法数自增1；当首字符为'0'时，直接返回；
 				递归时如果字符串首部两个元素能否独立解译且合并解译，则递归求解去掉首部两位的子串，然后再递归求解去掉首部一位的子串即可。
-				时间复杂度为O(?)，空间复杂度为O(n^2)。OJ报TLE。
+				时间复杂度为O(2^n)，空间复杂度为O(n^2)。OJ报TLE。
 	*/
-	int numDecodings(string s) {
+	int numDecodings_Recur(string s) {
 		int cnt = 0;
 		getDecodeMethods(s, cnt);
 		return cnt;
+	}
+
+	///@brief	给定一个数字串，其中数字是按照1->A, 2->B, ..., 26->Z的顺序来进行对应的，计算该数字串有多少种解码方法
+	///@param	s	数字串
+	///@return	返回总的解码方法数
+	/* @note	动态归划：用dp[i]表示字符串s[0..i-1]能够解码的方法数。初始条件dp[0] = 1, dp[1] = (s[0] == '0') ? 0 : 1，递推方程为
+				dp[i] = (s[i-1] <= '9' && s[i-1] > '0') ? dp[i-1] : 0) + 
+				((s[i-2] == '2' && s[i-1] <= '6' && s[i-1] >= '0') || (s[i-1] == '1')) ? dp[i-2] : 0.
+				时间复杂度为O(n)，空间复杂度为O(n)。	
+	*/
+	int numDecodings_DP(string s) {
+		if (s.empty())
+		{
+			return 0;
+		}
+		vector<int> dp;	//	初始化。dp[i]表示s[0..i-1]的解码方法数
+		for (int i = 0; i != s.length()+1; i++)
+		{
+			dp.push_back(0);
+		}	
+
+		dp[0] = 1;	//	用于计算dp[i-2]的值
+		dp[1] = (s[0] == '0') ? 0 : 1;	//	初始条件
+		
+		for (int i = 2; i != s.length()+1; i++)	
+		{
+			if (s[i-1] != '0')	// 先判前一个元素
+			{
+				dp[i] = dp[i-1];
+			}
+			else 
+				dp[i] = 0;	//	出现无法匹配的'0'，直接置为0
+
+
+			if ((s[i-2] == '2' && s[i-1] <= '6') || (s[i-2] == '1'))	//	再判前两个元素
+			{
+				dp[i] += dp[i-2];
+			}			
+		}
+		return dp.back();
 	}
 
 private:
@@ -100,7 +141,8 @@ int main()
 {
 	string s = "9371597631128776948387197132267188677349946742344217846154932859125134924241649584251978418763151253";//3981312
 	Solution slt;
-	cout << slt.numDecodings(s);
+	cout << slt.numDecodings_DP(s);
+	cout << ' ' << slt.numDecodings_Recur(s);
 	cout << endl;
 	return 0;
 }
