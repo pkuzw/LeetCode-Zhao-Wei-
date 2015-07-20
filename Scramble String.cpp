e7fbb75e344dc8cbd244493c306dcc0d2d0a220e
Zhao Wei <pkuzw@126.com>
2015-07-20 09:44:46 +0800

    Scramble String - DP
    1. 维护一个三维数组来dp[k][i][j]保存字符串s1[i..i+k]与字符串s2[j..j+k]是否为scramble
    string。初始条件为dp[1][i][j] = (s1[i] == s2[j]) ? true : false；
    2. dp[k][i][j] = ((dp[l][i][j] && dp[k-l][i+l][j+l]) || (dp[l][i][j+k-l]
    && dp[k-l][i+l][j]))。
    3. 时间复杂度为O(n^4)，空间复杂度为O(n^3)。

--- Scramble String.cpp
+++ Scramble String.cpp
@@ -54,14 +54,10 @@ public:
 	//			写一个函数判定这两个字符串是否是scramble string
 	///@param	s1	字符串1
 	///@param	s2	字符串2
-	///@return  如果这两个字符串是scramble string的话返回true；否则返回false
-	/* @note	递归法：如果字符串s1[0...i]和字符串s2[0...j](i == j)满足下列条件之一时就认为它们是scramble string:
-				a. s1[0..k]与s2[0..k]是scramble string且s1[k+1..i]与s2[k+1..j]是scramble string；
-				b. s1[0..k]与s2[j-k..j]是scramble string且s1[k+1..i]与s2[0..j-k-1]是scramble string；其中k = 0...i-1
-				时间复杂度为O(2^n)
-	*/
+	///@return  如果这两个字符串是scramble string的话返回true；否则返回false	
 	bool isScramble(string s1, string s2) {
-		bool rslt = isScramble_Recur(s1, s2);
+	//	bool rslt = isScramble_Recur(s1, s2);
+		bool rslt = isScramble_DP(s1, s2);
 		return rslt;
 	}
 
@@ -70,6 +66,11 @@ private:
 	///@param	s1	字符串1
 	///@param   s2	字符串2
 	///@return  如果这两个字符串是scramble string的话返回true；否则返回false
+	/* @note	递归法：如果字符串s1[0...i]和字符串s2[0...j](i == j)满足下列条件之一时就认为它们是scramble string:
+				a. s1[0..k]与s2[0..k]是scramble string且s1[k+1..i]与s2[k+1..j]是scramble string；
+				b. s1[0..k]与s2[j-k..j]是scramble string且s1[k+1..i]与s2[0..j-k-1]是scramble string；其中k = 0...i-1
+				时间复杂度为O(2^n)
+	*/
 	bool isScramble_Recur(string& s1, string & s2)
 	{
 		string s1_tmp = s1;
@@ -82,7 +83,7 @@ private:
 		if (s1 == s2)	//	如果两字符串完全相同，直接返回true
 			return true;
 		
-		for (int i = 1; i != s1.length(); i++)
+		for (int i = 1; i != s1.length(); i++)	//	i从1开始，保证不出现空字符串
 		{
 			string s1_left = s1.substr(0, i);
 			string s1_right = s1.substr(i, s1.length()-i);
@@ -105,11 +106,61 @@ private:
 		}
 		return false;
 	}
+
+	///@brief	动态规划法判断两字符串是否为scramble string
+	///@param	s1	字符串1
+	///@param   s2	字符串2
+	///@return  如果这两个字符串是scramble string的话返回true；否则返回false
+	/* @note	维护一个三维数组来dp[k][i][j]保存字符串s1[i..i+k]与字符串s2[j..j+k]是否为scramble string。初始条件为dp[1][i][j] = (s1[i] == s2[j]) ? true : false，
+				dp[k][i][j] = ((dp[l][i][j] && dp[k-l][i+l][j+l]) || (dp[l][i][j+k-l] && dp[k-l][i+l][j]))。时间复杂度为O(n^4)，空间复杂度为O(n^3)。
+	*/
+	bool isScramble_DP(string &s1, string &s2)
+	{
+		if (s1.length() != s2.length())
+		{
+			return false;
+		}
+		const int len = s1.length();
+
+		vector<vector<vector<bool>>> dp;
+		vector<vector<bool>> matrix;
+		vector<bool> line;
+
+		for (int i = 0; i != len; i++)
+			line.push_back(false);
+		for (int i = 0; i != len; i++)
+			matrix.push_back(line);		
+		for (int i = 0; i != len+1; i++)
+			dp.push_back(matrix);
+
+		for (int i = 0; i != len; i++)
+		{
+			for (int j = 0; j != len; j++)
+			{
+				dp[1][i][j] = (s1[i] == s2[j]) ? true : false;
+			}
+		}
+
+		for (int k = 2; k != len+1; k++)
+		{
+			for (int i = 0; i != len-k+1; i++)
+			{
+				for (int j = 0; j != len-k+1; j++)
+				{					
+					for (int l = 1; l != k && !dp[k][i][j]; l++)	//? 这里为什么要判断一下dp[k][i][j]? 这是因为如果已经判定出s1和s2在dp[k][i][j]处为true后，不需要也不能再进入后续的判定，一是浪费时间，二是有可能将原来的true抹成false
+					{
+						dp[k][i][j] = ((dp[l][i][j] && dp[k-l][i+l][j+l]) || (dp[l][i][j+k-l] && dp[k-l][i+l][j]));
+					}
+				}
+			}
+		}
+		return dp[len][0][0];		
+	}
 };
 
 int main()
 {
-	string s1 = "great", s2 = "agtre";
+	string s1 = "abc", s2 = "acb";
 	Solution slt;
 	cout << slt.isScramble(s1, s2) << endl;
 	return 0;
