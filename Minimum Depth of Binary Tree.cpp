254da02c569745d45c1966a87aec76a512d47e8a
Zhao Wei <pkuzw@126.com>
2015-07-23 00:55:24 +0800

    Minimum Depth of Binary Tree
    1. 添加文件头部信息。

---
+++ Minimum Depth of Binary Tree.cpp
@@ -0,0 +1,34 @@
+﻿///@file	Minimum Depth of Binary Tree
+/*
+Given a binary tree, find its minimum depth.
+
+The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+*/
+///@author	zhaowei
+///@date	2015.07.23
+///@version	1.0
+
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+	int minDepth(TreeNode* root) {
+
+	}
+};
+
+int main()
+{
+	return 0;
+}
