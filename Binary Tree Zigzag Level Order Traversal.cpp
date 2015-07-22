3b9b638c21ff3ce7cb7d45f740756aaed4ab230f
Zhao Wei <pkuzw@126.com>
2015-07-22 14:46:13 +0800

    Binary Tree Zigzag Level Order Traversal
    1. 写文件头部信息。

---
+++ Binary Tree Zigzag Level Order Traversal.cpp
@@ -0,0 +1,25 @@
+﻿///@file	Binary Tree Zigzag Level Order Traversal
+/*
+Given a binary tree, return the zigzag level order traversal of its nodes' values. (ie, from left to right, then right to left for the next level and alternate between).
+
+For example:
+Given binary tree {3,9,20,#,#,15,7},
+3
+/ \
+9  20
+/  \
+15   7
+return its zigzag level order traversal as:
+[
+[3],
+[20,9],
+[15,7]
+]
+*/
+///@author	zhaowei
+///@date	2015.07.22
+///@version	1.0
+
+#include <iostream>
+#include <vector>
+#include <deque>
