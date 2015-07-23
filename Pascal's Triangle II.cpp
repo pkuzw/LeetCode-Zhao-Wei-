967786c05b7539bc2467e94ba682291f1b61943d
Zhao Wei <pkuzw@126.com>
2015-07-23 21:27:16 +0800

    Pascal's Triangle - Enumeration
    1. 杨辉三角的第i+1行的第j个元素相当于在第i行的首末各加1个0，然后将第i行的第j和第j+1求和的值。
    2. 时间复杂度为O(n^2)，空间复杂度为O(k)。

--- Pascal's Triangle II.cpp
+++ Pascal's Triangle II.cpp
@@ -18,7 +18,7 @@ Could you optimize your algorithm to use only O(k) extra space?
 */
 ///@author	zhaowei
 ///@date	2015.07.23
-///@versio	1.0
+///@versio	1.1
 
 #include <vector>
 
@@ -26,34 +26,30 @@ using namespace std;
 
 class Solution {
 public:
-	///@brief	生成杨辉三角
-	///@param	n	行号
+	///@brief	生成杨辉三角的第k行
+	///@param	rowIndex	行号
 	///@return	返回杨辉三角的第n行
 	/* @note	杨辉三角的第i+1行的第j个元素相当于在第i行的首末各加1个0，然后将第i行的第j和第j+1求和的值。
-				时间复杂度为O(n^2)，空间复杂度为O(n^2)。*/
+				时间复杂度为O(n^2)，空间复杂度为O(k)。*/
 	vector<int> getRow(int rowIndex) {
 		vector<int> line(1, 1);	//	vector的构造函数：初始化有1个1
-		pascal_tri.push_back(line);
+		
 		for (int i = 1; i <= rowIndex; i++)
-		{
-			line = pascal_tri.back();
+		{			
 			line.insert(line.begin(), 0);
 			line.insert(line.end(), 0);
 			vector<int> next_line;
 			for (int i = 0; i != line.size()-1; i++)
 				next_line.push_back(line[i]+line[i+1]);
-			pascal_tri.push_back(next_line);
+			line = next_line;
 		}
-		return pascal_tri.back();
+		return line;
 	}
-
-private:
-	vector<vector<int>> pascal_tri;
 };
 
 int main()
 {
-	int n = 3;
+	int n = 5;
 	Solution slt;
 	vector<int> rslt = slt.getRow(n);
 	return 0;
