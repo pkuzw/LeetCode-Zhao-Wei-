5ba4c2b83f3d6cb71674df270057602e672aecc1
Zhao Wei <pkuzw@126.com>
2015-07-23 22:31:57 +0800

    Populating Next Right Pointers in Each Node I & II - BFS
    1. 利用BFS的思想，在每一层中将节点横向相连。
    2. 时间复杂度为O(n)，空间复杂度为O(n)。
    3. 想不出来O(1)的空间复杂度的解法。

---
+++ Populating Next Right Pointers in Each Node II.cpp
@@ -0,0 +1,107 @@
+﻿///@file	Populating Next Right Pointers in Each Node
+/*
+Given a binary tree
+
+struct TreeLinkNode {
+TreeLinkNode *left;
+TreeLinkNode *right;
+TreeLinkNode *next;
+}
+Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+
+Initially, all next pointers are set to NULL.
+
+Note:
+
+You may only use constant extra space.
+
+Follow up for problem "Populating Next Right Pointers in Each Node".
+
+What if the given tree could be any binary tree? Would your previous solution still work?
+
+Note:
+
+You may only use constant extra space.
+For example,
+Given the following binary tree,
+
+		 1
+	   /   \
+	  2     3
+	 / \     \
+	4   5     7
+After calling your function, the tree should look like:
+		  1 -> NULL
+	   /    \
+	  2  ->  3 -> NULL
+	 / \      \
+	4-> 5  ->  7 -> NULL
+*/
+///@author	zhaowei
+///@date	2015.07.23
+///@version	1.0
+
+#include <queue>
+using namespace std;
+
+
+struct TreeLinkNode {
+	int val;
+	TreeLinkNode *left, *right, *next;
+	TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+};
+
+class Solution {
+public:
+	///@brief	将二叉树中的各个节点横向连接起来
+	///@root	根节点
+	///@note	利用BFS的思想，在每一层中将节点横向相连。时间复杂度为O(n)，空间复杂度为O(n)。如何才能达到O(1)的空间复杂度呢？
+	void connect(TreeLinkNode *root) {
+		if (root == nullptr)	return;
+
+		queue<TreeLinkNode*> que;
+		que.push(root);
+		que.push(nullptr);
+
+		while (!que.empty())
+		{
+			TreeLinkNode *tlnode = que.front();
+			que.pop();
+
+			if (tlnode != nullptr)
+			{
+				tlnode->next = que.front();
+				if (tlnode->left != nullptr)
+					que.push(tlnode->left);
+				if (tlnode->right != nullptr)
+					que.push(tlnode->right);
+			}
+			else
+			{
+				if (!que.empty())
+					que.push(nullptr);
+			}
+		}
+		return;
+	}
+};
+
+int main()
+{
+	TreeLinkNode* root = new TreeLinkNode(1);
+	TreeLinkNode* n[32];
+	for (int i = 0; i != 32; i++)
+		n[i] = new TreeLinkNode(i);
+
+	root->left = n[2];
+	root->right = n[3];
+	n[2]->left = n[4];
+	n[2]->right = n[5];
+	n[3]->left = n[6];
+	n[3]->right = n[7];
+
+	Solution slt;
+	slt.connect(root);
+
+	return 0;
+}
