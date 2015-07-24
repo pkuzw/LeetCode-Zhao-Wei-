e31ff339d38a0c34244501cdc0afa08a2c1ae6b9
Zhao Wei <pkuzw@126.com>
2015-07-24 23:02:08 +0800

    Invert Binary Tree - DFS
    1. 利用DFS的先序遍历，递归的转换左右孩子。
    2. 时间复杂度为O(n)，空间复杂度为O(n)。

---
+++ Invert Binary Tree.cpp
@@ -0,0 +1,86 @@
+﻿///@file	Invert Binary Tree
+/*
+Invert a binary tree.
+
+4
+/   \
+2     7
+/ \   / \
+1   3 6   9
+to
+4
+/   \
+7     2
+/ \   / \
+9   6 3   1
+Trivia:
+This problem was inspired by this original tweet by Max Howell:
+
+Google: 90% of our engineers use the software you wrote (Homebrew), but you can’t invert a binary tree on a whiteboard so fuck off.
+*/
+///@author	zhaowei
+///@date	2015.07.24
+///@version	1.0
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+class Solution {
+public:
+	///@brief	翻转一棵二叉树
+	///@param	root	二叉树的根节点
+	///@return	返回翻转后的二叉树根节点
+	/* @note	利用DFS的先序遍历，递归的转换左右孩子。时间复杂度为O(n)，空间复杂度为O(n)。*/
+    TreeNode* invertTree(TreeNode* root) {
+        invertTree_Recur(root);
+		return root;
+    }
+
+private:
+	///@brief	递归地翻转一棵二叉树
+	///@param	root	二叉树的根节点
+	///@return	返回翻转后的二叉树根节点
+	void invertTree_Recur(TreeNode *root)
+	{
+		if (root == nullptr)	return;
+
+		TreeNode *tmp = new TreeNode(0);
+		tmp = root->left;
+		root->left = root->right;
+		root->right = tmp;
+
+		if (root->left != nullptr)
+			invertTree_Recur(root->left);
+		if (root->right != nullptr)
+			invertTree_Recur(root->right);
+
+		return;
+	}
+};
+
+int main()
+{
+	TreeNode *n[20];
+	for (int i = 0; i != 20; i++)
+		n[i] = new TreeNode(i);
+
+	n[1]->left = n[2];
+	n[1]->right = n[3];
+	n[2]->left = n[4];
+	n[2]->right = n[5];
+	n[3]->left = n[6];
+	n[3]->right = n[7];
+	n[4]->right = n[8];
+	n[5]->right = n[9];
+	n[7]->right = n[10];
+
+	TreeNode *root = n[1];
+
+	Solution slt;
+	TreeNode *rslt = slt.invertTree(root);
+	return 0;
+}
