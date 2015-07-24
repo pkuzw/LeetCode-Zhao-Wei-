16f9a5081eb53987575da033025eec4acfb0bf5d
Zhao Wei <pkuzw@126.com>
2015-07-24 20:59:27 +0800

    Palindrome Partitioning
    1. 写文件头部信息。

---
+++ Palindrome Partitioning.cpp
@@ -0,0 +1,38 @@
+﻿///@file	Palindrome Partitioning
+/*
+Given a string s, partition s such that every substring of the partition is a palindrome.
+
+Return all possible palindrome partitioning of s.
+
+For example, given s = "aab",
+Return
+
+[
+["aa","b"],
+["a","a","b"]
+]
+*/
+///@author	zhaowei
+///@date	2015.07.24
+///@version	1.0
+
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+	///@brief	给定一个字符串s，对它进行划分，使得划分后的子串是回文的。
+	///@param	s	字符串
+	///@return	返回所有可能的划分
+	/* @note		*/
+	vector<vector<string>> partition(string s) {
+
+	}
+};
+
+int main()
+{
+	return 0;
+}
