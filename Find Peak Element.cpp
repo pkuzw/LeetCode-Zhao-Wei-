f4e1fdcda077f9d20978dd9803befa6a44977b22
Zhao Wei <pkuzw@126.com>
2015-07-28 00:25:28 +0800

    Find Peak Element - Binary Search
    1. 二分查找。
    2. 时间复杂度为O(logn)，空间复杂度为O(1)。

---
+++ Find Peak Element.cpp
@@ -0,0 +1,51 @@
+﻿///@file	Find Peak Element
+/*
+A peak element is an element that is greater than its neighbors.
+
+Given an input array where num[i] ≠ num[i+1], find a peak element and return its index.
+
+The array may contain multiple peaks, in that case return the index to any one of the peaks is fine.
+
+You may imagine that num[-1] = num[n] = -∞.
+
+For example, in array [1, 2, 3, 1], 3 is a peak element and your function should return the index number 2.
+*/
+///@author	zhaowei
+///@date	2015.07.28
+///@version	1.0
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	///@brief	找到数组中的一个局部最大值
+	///@param	nums	数组
+	///@return	返回任意一个局部最大值
+	///@note	二分查找。时间复杂度为O(logn)，空间复杂度为O(1)。
+	int findPeakElement(vector<int>& nums) {
+		if (nums.size() == 1)	return 0;
+
+		int l = 0;
+		int r = nums.size()-1;
+
+		while (l <= r)
+		{
+			if (l == r)	return l;
+			int m = (l + r) / 2;
+			if (nums[m] < nums[m+1])	l = m+1;
+			else						r = m;
+		}
+	}
+};
+
+int main()
+{
+	int n[3] = {1,2,3};
+	vector<int> nums;
+	for (int i = 0; i != 3; i++)
+		nums.push_back(n[i]);
+	Solution slt;
+	int rslt = slt.findPeakElement(nums);
+	return 0;
+}
