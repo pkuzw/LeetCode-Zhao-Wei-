e8719477fbd9bce23bcd1b75dbb6990680113a5d
Zhao Wei <pkuzw@126.com>
2015-07-27 22:52:45 +0800

    Find Minimum in Roatated Sorted Array - Binary Search
    1. 二分查找。建立首尾指针和中间指针，只要左右指针还未相邻，如果首指针比中间指针小，说明前半部分有序，进入后半部分查找；否则进入前半部分查找。
    2. 时间复杂度为O(logn)，空间复杂度为O(1)。

---
+++ Find Minimum in Rotated Sorted Array.cpp
@@ -0,0 +1,54 @@
+﻿///@file	Find Minimum in Rotated Sorted Array
+/*
+Suppose a sorted array is rotated at some pivot unknown to you beforehand.
+
+(i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+
+Find the minimum element.
+
+You may assume no duplicate exists in the array.
+*/
+///@author	zhaowei
+///@date	2015.07.27
+///@version	1.0
+
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	///@brief	在一个已经排好序但部分翻转的数组中查找最小值
+	///@param	nums	数组
+	///@return	返回最小值
+	/* @note	二分查找。建立首尾指针和中间指针，只要左右指针还未相邻，如果首指针比中间指针小，说明前半部分有序，进入后半部分查找；
+				否则进入前半部分查找。时间复杂度为O(logn)，空间复杂度为O(1)。	*/
+	int findMin(vector<int>& nums) {
+		if (nums.size() == 1)	return nums[0];
+
+		int l = 0;
+		int r = nums.size()-1;
+		if (nums[l] < nums[r])	return nums[l];
+
+		while (l != r-1)
+		{
+			int m = (l + r) / 2;
+			if (nums[l] < nums[m])	l = m;
+			else					r = m;
+		}
+
+		return (nums[l] < nums[r]) ? nums[l] : nums[r];
+	}
+};
+
+int main()
+{
+	int n[10] = {5,6,7,8,9,10,1,2,3,4};
+	vector<int> nums;
+	for (int i = 0; i != 10; i++)	
+		nums.push_back(n[i]);
+
+	Solution slt;
+	int rslt = slt.findMin(nums);
+	return 0;
+}
