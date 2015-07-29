279227cacf07d5bc90bd63e36b92775c281a26b1
Zhao Wei <pkuzw@126.com>
2015-07-30 06:41:38 +0800

    LRU Cache - Hash Table & List
    1. 用双向链表和哈希表来设计LRU缓存。其中链表存储数据，哈希表存储链表的节点的地址。

---
+++ LRU Cache.cpp
@@ -0,0 +1,86 @@
+﻿///@file	LRU Cache
+/*
+Design and implement a data structure for Least Recently Used (LRU) cache. It should support the following operations: get and set.
+
+get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
+set(key, value) - Set or insert the value if the key is not already present. When the cache reached its capacity, it should invalidate the least recently used item before inserting a new item. 
+*/
+///@author	zhaowei
+///@date	2015.07.30
+///@versio	1.0
+
+#include <unordered_map>
+#include <list>
+
+using namespace std;
+
+/*
+为了保持cache的性能，使查找，插入，删除都有较高的性能，我们使用双向链表（std::list）和哈希表（std::unordered_map）作为cache的数据结构，
+
+因为：
+双向链表插入删除效率高（单向链表插入和删除时，还要查找节点的前节点）
+哈希表保存每个节点的地址，可以基本保证在O(1)时间内查找节点
+
+具体实现细节：
+越靠近链表头部，表示节点上次访问距离现在时间最短，尾部的节点表示最近访问最少
+查询或者访问节点时，如果节点存在，把该节点交换到链表头部，同时更新hash表中该节点的地址
+插入节点时，如果cache的size达到了上限，则删除尾部节点，同时要在hash表中删除对应的项。新节点都插入链表头部。    
+*/
+
+class LRUCache{
+private:
+	struct CacheNode
+	{
+		int key;
+		int value;
+		CacheNode(int k, int v):key(k), value(v){}
+	};
+
+	list<CacheNode> cacheList;
+	unordered_map<int, list<CacheNode>::iterator>cacheMap;
+	int size;
+
+public:
+	///@brief	设置缓存大小
+	LRUCache(int capacity) {
+		size = capacity;
+	}
+
+	///@brief	访问缓存中元素
+	int get(int key) {
+		if(cacheMap.find(key) == cacheMap.end())
+			return -1;
+		else
+		{
+			//把当前访问的节点移到链表头部,并且更新map中该节点的地址
+			cacheList.splice(cacheList.begin(), cacheList, cacheMap[key]);	//list::splice实现list拼接的功能。将源list的内容部分或全部元素删除，拼插入到目的list。目的list的大小会增加，增加的大小为插入元素的大小。x的大小相应的会减少同样的大小。
+			cacheMap[key] = cacheList.begin();
+			return cacheMap[key]->value;
+		}
+	}
+
+	void set(int key, int value) {
+		if(cacheMap.find(key) == cacheMap.end())
+		{
+			if(cacheList.size() == size)
+			{//删除链表尾部节点（最少访问的节点）
+				cacheMap.erase(cacheList.back().key);
+				cacheList.pop_back();
+			}
+			//插入新节点到链表头部,并且更新map中增加该节点
+			cacheList.push_front(CacheNode(key, value));
+			cacheMap[key] = cacheList.begin();
+		}
+		else
+		{//更新节点的值，把当前访问的节点移到链表头部,并且更新map中该节点的地址
+			cacheMap[key]->value = value;
+			cacheList.splice(cacheList.begin(), cacheList, cacheMap[key]);
+			cacheMap[key] = cacheList.begin();
+		}
+	}
+};
+
+int main()
+{
+	return 0;
+}
