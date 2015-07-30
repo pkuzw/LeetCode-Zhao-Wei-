08473e4445d59ab36ca4ee8739484e763c936073
Zhao Wei <pkuzw@126.com>
2015-07-30 22:32:43 +0800

    Maximum Gap
    1. 写文件头部信息。

---
+++ Maximum Gap.cpp
@@ -0,0 +1,30 @@
+﻿///@file	Maximum Gap
+/*
+Given an unsorted array, find the maximum difference between the successive elements in its sorted form.
+
+Try to solve it in linear time/space.
+
+Return 0 if the array contains less than 2 elements.
+
+You may assume all elements in the array are non-negative integers and fit in the 32-bit signed integer range.
+*/
+///@author	zhaowei
+///@date	2015.07.30
+///@version	1.0
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+	///@brief	给定一组乱序的数组，找出排好序后两个相邻元素的最大差值。要求时间复杂度为O(n)，空间复杂度为O(n)
+	int maximumGap(vector<int>& nums) {
+
+	}
+};
+
+int main()
+{
+	return 0;
+}
