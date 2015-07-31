d4f5bfb023f6de26ba654acfafdbfa8798c4a0d5
Zhao Wei <pkuzw@126.com>
2015-08-01 01:48:19 +0800

    Dungeon Game - DP
    1. 修改dp二维数组的尺寸

--- Dungeon Game.cpp
+++ Dungeon Game.cpp
@@ -34,7 +34,7 @@ public:
 		if (row == 0)	return 0;
 		int col = dungeon[0].size();
 
-		vector<vector<int>> dp(col, vector<int>(row, 1));
+		vector<vector<int>> dp(row, vector<int>(col, 1));
 		dp[row-1][col-1] = max(1, 1-dungeon[row-1][col-1]);
 		for (int i = row-2; i >= 0; i--)
 			dp[i][col-1] = max(1, dp[i+1][col-1] - dungeon[i][col-1]);
