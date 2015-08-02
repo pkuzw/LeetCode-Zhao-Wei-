3ae65b2f87269ec14a61393637f1d1cab78df1b4
Zhao Wei <pkuzw@126.com>
2015-08-02 23:52:41 +0800

    Fraction to Recurring Decimal - Hash Table
    1. 利用哈希表存储以前出现过的商，如果再次出现，则用括号包括起来返回；
    2. 时间复杂度为O(n)，空间复杂度为O(n)。

---
+++ Fraction to Recurring Decimal.cpp
@@ -0,0 +1,42 @@
+﻿///@file	Fraction to Recurring Decimal
+/*
+Given two integers representing the numerator and denominator of a fraction, return the fraction in string format.
+
+If the fractional part is repeating, enclose the repeating part in parentheses.
+
+For example,
+
+Given numerator = 1, denominator = 2, return "0.5".
+Given numerator = 2, denominator = 1, return "2".
+Given numerator = 2, denominator = 3, return "0.(6)".
+*/
+///@author	zhaowei
+///@date	2015.08.02
+///@version	1.0
+
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+	///@brief	将分数转换成小数
+	///@param	numerator	分子
+	///@param	denominator	分母
+	///@return	返回字符串表示的小数，如果是循环小数，则将循环部分用括号括起来
+	/* @note	*/
+	string fractionToDecimal(int numerator, int denominator) {
+
+	}
+};
+
+int main()
+{
+	double a, b;
+	a = 4;
+	b = 17;
+	cout << a/b << endl;
+	return 0;
+}
