7d03e2f1eed6fa06ccdb6ff70bcfb7b6e49f90cc
Zhao Wei <pkuzw@126.com>
2015-08-03 13:15:04 +0800

    Fraction to Recurring Decimal - Hash Table
    1. 利用哈希表存储以前出现过的商，如果再次出现，则用括号包括起来返回；
    2. 时间复杂度为O(n)，空间复杂度为O(n)。

--- Fraction to Recurring Decimal.cpp
+++ Fraction to Recurring Decimal.cpp
@@ -16,6 +16,7 @@ Given numerator = 2, denominator = 3, return "0.(6)".
 
 #include <string>
 #include <vector>
+#include <unordered_map>
 #include <iostream>
 
 using namespace std;
@@ -26,9 +27,37 @@ public:
 	///@param	numerator	分子
 	///@param	denominator	分母
 	///@return	返回字符串表示的小数，如果是循环小数，则将循环部分用括号括起来
-	/* @note	*/
+	/* @note	1. 利用哈希表存储以前出现过的商，如果再次出现，则用括号包括起来返回；
+				2. 时间复杂度为O(n)，空间复杂度为O(n)。*/
 	string fractionToDecimal(int numerator, int denominator) {
+		int s1 = numerator >= 0 ? 1 : -1;	//	分子的符号
+		int s2 = denominator >= 0 ? 1 : -1;	//	分母的符号
+		long long num = abs( (long long)numerator );	//	为了保证精度，将分子和分母转换成long long型
+		long long den = abs( (long long)denominator );
+		long long out = num / den;	//	计算其商
+		long long rem = num % den;	//	计算其余数
 
+		unordered_map<long long, int> m;
+		string res = to_string(out);	//	将long long型变量转换成字符串
+		if (s1 * s2 == -1 && (out > 0 || rem > 0)) res = "-" + res;	//	添加符号
+		if (rem == 0) return res;	//	如果能被整除，则直接返回
+
+		res += ".";	//	否则计算小数部分
+		string s = "";
+		int pos = 0;
+		while (rem != 0) {
+			if (m.find(rem) != m.end())
+			{
+				s.insert(m[rem], "(");
+				s += ")";
+				return res + s;
+			}
+			m[rem] = pos;
+			s += to_string((rem * 10) / den);
+			rem = (rem * 10) % den;
+			++pos;
+		}
+		return res + s;
 	}
 };
 
