c156188f1361c4a96f99166fd6abb73dbf27322a
Zhao Wei <pkuzw@126.com>
2015-08-06 17:16:33 +0800

    v2 - 正算 & 反算
    1. 计算出0..i-1的阶乘；
    2. 再计算出i+1...n的阶乘；
    3. 两者相乘，结果就是除了i的乘积。
    4. 时间复杂度为O(n)，空间复杂度为O(1)。

--- Product of Array Except Self.cpp
+++ Product of Array Except Self.cpp
@@ -17,13 +17,13 @@ Could you solve it with constant space complexity? (Note: The output array does 
 
 using namespace std;
 
-class Solution {
+class Solution_v1 {
 public:
 	///@brief	计算数组中除了自身元素外，其余元素的乘积。
 	///@param	nums为O(1)	数组
 	///@return	返回求完乘积后的数组
 	/* @note	先顺序生成一个中间数组，其中初始元素为1，tmp[i] = tmp[i-1]*nums[i-1]。然后再逆序生成结果数组，引入一个临时变量t = 1，
-				tmp[i] = t*tmp[i]; t = t*nums[i];时间复杂度为O(n)，空间复杂度。*/
+				tmp[i] = t*tmp[i]; t = t*nums[i];时间复杂度为O(n)，空间复杂度为O(1)。*/
 	vector<int> productExceptSelf(vector<int>& nums) {
 		if (nums.empty())	return nums;
 
@@ -44,11 +44,34 @@ public:
 	}
 };
 
+class Solution {
+public:
+	vector<int> productExceptSelf(vector<int>& nums) {		
+		vector<int> rslt(nums.size(), 0);
+		if (nums.empty())	return rslt;
+
+		rslt[0] = 1;
+		for (int i = 1; i != nums.size(); i++)	//	rslt[i]相当于计算0..i-1的阶乘
+		{
+			rslt[i] = rslt[i-1] * nums[i-1];
+		}
+
+		int t = 1;
+		for (int i = nums.size()-1; i >= 0; i--)
+		{
+			rslt[i] = rslt[i] * t;
+			t = t * nums[i];	//	t是i+1...n的阶乘
+		}
+
+		return rslt;
+	}
+};
+
 int main()
 {
 	int n[4] = {4,3,2,1};
 	vector<int> nums;
-	for (int i = 0; i != 1; i++)
+	for (int i = 0; i != 4; i++)
 	{
 		nums.push_back(n[i]);
 	}
