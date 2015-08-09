6f4ac3ac4938c27978cce964734a9e1d18520c41
Zhao Wei <pkuzw@126.com>
2015-08-09 20:47:13 +0800

    v2 - two queues

--- Implement Stack using Queues.cpp
+++ Implement Stack using Queues.cpp
@@ -15,12 +15,15 @@ You may assume that all operations are valid (for example, no pop or top operati
 ///@date	2015.07.26
 ///@version	1.0
 
+///@date	2015.08.09
+///@version	2.0
+
 #include <queue>
 using namespace std;
 
 ///@brief	用队列来实现栈
 /* @note	用两个队列来模拟一个栈。一个用来保存原始数据，另一个存放临时数据。对于top和pop操作，就先将原始数据的队列弹出到只剩一个元素来进行操作，然后再将临时队列中的元素压回原始队列。*/
-class Stack {
+class Stack_v1 {
 public:
 	// Push element x onto stack.
 	void push(int x) {
@@ -75,6 +78,55 @@ private:
 
 };
 
+class Stack {
+public:
+	// Push element x onto stack.
+	void push(int x) {
+		q1.push(x);
+	}
+
+	// Removes the element on top of the stack.
+	void pop() {
+		while (q1.size() > 1)
+		{
+			q2.push(q1.front());
+			q1.pop();
+		}
+		q1.pop();
+		while (!q2.empty())
+		{
+			q1.push(q2.front());
+			q2.pop();
+		}
+	}
+
+	// Get the top element.
+	int top() {
+		while (q1.size() > 1)
+		{
+			q2.push(q1.front());
+			q1.pop();
+		}
+		int tp = q1.front();
+		q2.push(tp);
+		q1.pop();
+		while (!q2.empty())
+		{
+			q1.push(q2.front());
+			q2.pop();
+		}
+		return tp;
+	}
+
+	// Return whether the stack is empty.
+	bool empty() {
+		return q1.empty();
+	}
+
+private:
+	queue<int> q1, q2;
+};
+
 int main()
 {
 	int n[5] = {1,2,3,4,5};
@@ -94,4 +146,5 @@ int main()
 		s.pop();
 
 	emty = s.empty();
+	return 0;
 }
