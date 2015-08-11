15cf0aedc9a6b00f4d9ac8671a338d794ec8ccb3
Zhao Wei <zhaowei.pku@gmail.com>
2015-08-11 13:39:28 +0800

    v2 - directed graph
    1.
    将边表先转换成邻接表，是先修课程指向后续课程。用一个二维的vector来保存。另外还需要一个一维的vector来保存某课程的先修课数目。此后维护一个队列，该队列中的课程都是已经完成了先修要求或者没有先修要求的课。之后每次将队首弹出，压入结果数组，然后将与该队首课程有关的后续课程的先修数目自减1，如果该课程的先修数目也为0时则入队。最后判定课程是否能修完，就看结果数组和课程总数是否相同，如果不相同，则说明图中有环，将结果数组清空返回即可。

--- Course Schedule II.cpp
+++ Course Schedule II.cpp
@@ -19,19 +19,25 @@ There are a total of 4 courses to take. To take course 3 you should have finishe
 Note:
 The input prerequisites is a graph represented by a list of edges, not adjacency matrices. Read more about how a graph is represented.
 */
+///@author	zhaowei
+////@date	2015.08.03
+///@version	1.0
+
+///@date	2015.08.11
+///@version	2.0
 #include <vector>
 #include <queue>
 
 using namespace std;
 
-class Solution {
+class Solution_v1 {
 public:
 	///@brief	给出课程的学习序列
 	///@param	numCourses		课程数目
 	///@param	prerequisites	按照边表给出的有向图
 	///@return	返回一种可行的选课序列
 	/* @note	将课程的先后序列看作是有向图，后续课程依赖于先修课程，在有向图中就是先修课程指向后续课程的节点。此时问题就转换成给出这个
-				有向图的	拓扑排序。先将边表转换成邻接表，然后再利用队列，来宽度优先搜索（BFS）整个有向图，当图中存在环时，最后的结果数组
+				有向图的拓扑排序。先将边表转换成邻接表，然后再利用队列，来宽度优先搜索（BFS）整个有向图，当图中存在环时，最后的结果数组
 				肯定会小于课程数，将课程数清空即可。时间复杂度为O(m+n)，空间复杂度为O(m+n)。m是节点数，n是边数。*/
 	vector<int> findOrder(int numCourses, vector<pair<int, int>>& prerequisites) {
 		vector<int> rslt;	//	结果数组
@@ -68,15 +74,61 @@ public:
 	}
 };
 
+/*
+将边表先转换成邻接表，是先修课程指向后续课程。用一个二维的vector来保存。另外还需要一个一维的vector来保存某课程的先修课数目。此后维护
+一个队列，该队列中的课程都是已经完成了先修要求或者没有先修要求的课。之后每次将队首弹出，压入结果数组，然后将与该队首课程有关的后续课
+程的先修数目自减1，如果该课程的先修数目也为0时则入队。最后判定课程是否能修完，就看结果数组和课程总数是否相同，如果不相同，则说明图中
+有环，将结果数组清空返回即可。
+*/
+class Solution {
+public:
+	vector<int> findOrder(int numCourses, vector<pair<int, int>>& prerequisites) {
+		vector<int> rslt;
+		vector<vector<int>> graph(numCourses, vector<int>(0));
+		vector<int> pre_cnt(numCourses, 0);
+
+		for (int i = 0; i != prerequisites.size(); i++)
+		{
+			graph[prerequisites[i].second].push_back(prerequisites[i].first);
+			pre_cnt[prerequisites[i].first]++;
+		}
+
+		queue<int> que;
+		for (int i = 0; i != numCourses; i++)
+		{
+			if (pre_cnt[i] == 0)	que.push(i);
+		}
+
+		while (!que.empty())
+		{
+			int frt = que.front();
+			que.pop();
+			rslt.push_back(frt);
+
+			for (int i = 0; i != graph[frt].size(); i++)
+			{
+				pre_cnt[graph[frt][i]]--;
+				if (pre_cnt[graph[frt][i]] == 0)	que.push(graph[frt][i]);
+			}
+		}
+
+		if (rslt.size() != numCourses)	rslt.clear();
+		return rslt;
+	}
+};
+
 int main()
 {
 	//4, [[1,0],[2,0],[3,1],[3,2]]
 	vector<pair<int, int>> prerequisite;
-	prerequisite.push_back(make_pair(1, 3));
-	prerequisite.push_back(make_pair(2, 1));
+	prerequisite.push_back(make_pair(1, 2));
+	prerequisite.push_back(make_pair(2, 0));
 	prerequisite.push_back(make_pair(3, 1));
 	prerequisite.push_back(make_pair(3, 2));
 	Solution slt;
 	vector<int> rslt = slt.findOrder(4, prerequisite);
+
+	Solution_v1 slt_v1;
+	rslt = slt_v1.findOrder(4, prerequisite);
 	return 0;
 }
