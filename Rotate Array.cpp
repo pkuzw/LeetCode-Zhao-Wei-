4f4e8eff9a5def0d4e5862fa8bb204665085facb
Zhao Wei <zhaowei.pku@gmail.com>
2015-08-12 15:49:06 +0800

    v2 - iterative

--- Rotate Array.cpp
+++ Rotate Array.cpp
@@ -6,12 +6,20 @@ For example, with n = 7 and k = 3, the array [1,2,3,4,5,6,7] is rotated to [5,6,
 Note:
 Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem. 
 */
+///@author	zhaowie
+///@date	2015.07.26
+///@version	1.0
+
+///@date	2015.08.12
+///@version	2.1
+
 #include <deque>
+#include <stack>
 #include <vector>
 #include <algorithm>
 using namespace std;
 
-class Solution {
+class Solution_v1 {
 public:
 	///@brief	翻转一个数组中的指定长度的元素。
 	///@param	nums	数组
@@ -35,6 +43,26 @@ public:
 		nums = ivec2;
 	}
 };
+
+class Solution_v2 {
+public:
+	void rotate(vector<int>& nums, int k) {
+		k %= nums.size();
+		if (!k)	return;
+
+		vector<int> stk;
+		for (int i = 0; i != k; i++)
+		{
+			stk.push_back(nums.back());
+			nums.pop_back();
+		}
+		vector<int> ivec(stk.rbegin(), stk.rend());
+		ivec.insert(ivec.end(), nums.begin(), nums.end());
+
+		nums = ivec;
+	}
+};
+
 
 int main()
 {
