dc57fd17b53da84d57adc3f761f64277b3d9fc48
Zhao Wei <zhaowei.pku@gmail.com>
2015-08-13 15:15:21 +0800

    v2 - iterative inorder traversal
    1. 利用栈来进行二叉树的非递归中序遍历。

--- Binary Search Tree Iterator.cpp
+++ Binary Search Tree Iterator.cpp
@@ -10,6 +10,9 @@ Note: next() and hasNext() should run in average O(1) time and uses O(h) memory,
 ///@date	2015.08.04
 ///@version	1.0
 
+///@date	2015.08.13
+///@version	2.0
+
 #include <iostream>
 #include <stack>
 
@@ -23,9 +26,9 @@ struct TreeNode {
 };
 
 ///@brief	二叉树的非递归中序遍历，利用一个栈作为辅助数据结构实现。尽可能的将节点的左孩子入栈。
-class BSTIterator {
+class BSTIterator_v1 {
 public:
-    BSTIterator(TreeNode *root) {
+    BSTIterator_v1(TreeNode *root) {
 		while (root)
 		{
 			stk.push(root);
@@ -58,6 +61,42 @@ public:
 private:
 	stack<TreeNode*> stk;
 };
+
+class BSTIterator {
+public:
+	BSTIterator(TreeNode *root) {
+		while (root)
+		{
+			stk.push(root);
+			root = root->left;
+		}
+	}
+
+	/** @return whether we have a next smallest number */
+	bool hasNext() {
+		return !stk.empty();
+	}
+
+	/** @return the next smallest number */
+	int next() {
+		TreeNode* n = stk.top();
+		stk.pop();
+		int rslt = n->val;
+		if (n->right)
+		{
+			n = n->right;
+			while (n)
+			{
+				stk.push(n);
+				n = n->left;
+			}
+		}
+		return rslt;
+	}
+private:
+	stack<TreeNode*> stk;
+};
+
 
 /**
  * Your BSTIterator will be called like this:
