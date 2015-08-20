9ba749177f4eca1d54070778492ed87e9eadbe13
Zhao Wei <zhaowei.pku@gmail.com>
2015-08-20 21:48:04 +0800

    v2 - dp & dfs

--- Word Break II.cpp
+++ Word Break II.cpp
@@ -14,18 +14,22 @@ A solution is ["cats and dog", "cat sand dog"].
 ///@date	2015.07.30
 ///@version	1.0
 
+///@date	2015.08.20
+///@version	2.0
+
 #include <unordered_set>
 #include <string>
 
 using namespace std;
 
-class Solution {
+class Solution_v1 {
 public:
 	///@brief	给出字符串所有可能的满足词典的划分
 	///@param	s	字符串
 	///@param	wordDict	词典
 	///@return	返回所有可能的划分
-	/* @note	用一个一维数组记录下能够被划分的区间。dp[i]表示字符串s[i..end-1]是可以被划分的。这样在DFS递归查找所有可能时，如果不能够划分，就不用继续递归了，省去了很多重复计算。*/
+	/* @note	用一个一维数组记录下能够被划分的区间。dp[i]表示字符串s[i..end-1]是可以被划分的。
+				这样在DFS递归查找所有可能时，如果不能够划分，就不用继续递归了，省去了很多重复计算。*/
 	vector<string> wordBreak(string s, unordered_set<string>& wordDict) {
 		vector<bool> dp(s.size()+1, true);
 		string sentence;
@@ -69,7 +73,58 @@ private:
 	vector<string> rslt;
 };
 
+class Solution {
+public:
+	vector<string> wordBreak(string s, unordered_set<string>& wordDict) {
+		vector<bool> dp(s.size()+1, true);
+		string sentence;
+		wordBreak_DFS(s, wordDict, 0, dp, sentence);
+		return rslt;
+	}
+
+private:
+	vector<string> rslt;
+
+	void wordBreak_DFS(string s, unordered_set<string>& wordDict, int start, vector<bool>& dp, string& sentence)
+	{
+		if (start == s.size())
+		{
+			rslt.push_back(sentence.substr(0, sentence.size()-1));
+			return;
+		}
+
+		for (int i = start; i != s.size(); i++)
+		{
+			string word = s.substr(start, i-start+1);
+			if (dp[i+1] && wordDict.find(s.substr(start, i-start+1)) != wordDict.end())
+			{
+				sentence += word + " ";
+				int old_size = rslt.size();
+				wordBreak_DFS(s, wordDict, i+1, dp, sentence);
+				if (old_size == rslt.size())	dp[i+1] = false;
+				sentence = sentence.substr(0, sentence.size() - word.size() - 1);
+			}
+		}
+	}
+};
+
+
 int main()
 {
+	/*
+	s = "catsanddog",
+	dict = ["cat", "cats", "and", "sand", "dog"]
+	*/
+	unordered_set<string> wordDict;
+	wordDict.insert("cat");
+	wordDict.insert("cats");
+	wordDict.insert("and");
+	wordDict.insert("sand");
+	wordDict.insert("dog");
+
+	string s = "catsanddog";
+
+	Solution slt;
+	vector<string> rslt = slt.wordBreak(s, wordDict);
 	return 0;
 }
