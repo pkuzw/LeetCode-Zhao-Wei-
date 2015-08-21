ebd341f15abd1477dd56eb7d10230e60cc31581b
Zhao Wei <zhaowei.pku@gmail.com>
2015-08-21 14:47:16 +0800

    v2 - dynamic programming
    1. 设dp[i]表示s[0..i-1]可以划分与否。
    2. 初始条件为dp[0] = true;
    3. 递推关系式为dp[i] = dp[j] && s.substr(j, i-j)是否属于词典。

--- Word Break.cpp
+++ Word Break.cpp
@@ -12,13 +12,16 @@ Return true because "leetcode" can be segmented as "leet code".
 ///@date	2015.07.30
 ///@version	1.0
 
+///@date	2015.08.21
+///@version	2.0
+
 #include <vector>
 #include <string>
 #include <unordered_set>
 
 using namespace std;
 
-class Solution {
+class Solution_v1 {
 public:
 	///@brief	判断一个字符串能否被切分成字典中的单词
 	///@param	s	字符串
@@ -45,7 +48,35 @@ public:
 	}
 };
 
+class Solution {
+public:
+	bool wordBreak(string s, unordered_set<string>& wordDict) {
+		if (s.empty())	return false;
+		vector<bool> dp(s.size()+1, false);
+		dp[0] = true;
+
+		for (int i = 0; i != s.size()+1; i++)
+		{
+			for (int j = 0; j < i; j++)
+			{
+				if (dp[j] && wordDict.find(s.substr(j, i-j)) != wordDict.end())
+				{
+					dp[i] = true;
+					break;
+				}
+			}
+		}
+		return dp[s.size()];
+	}
+};
+
 int main()
 {
+	unordered_set<string> wordDict;
+	wordDict.insert("leet");
+	wordDict.insert("code");
+	string s = "leetcode";
+	Solution slt;
+	bool rslt = slt.wordBreak(s, wordDict);
 	return 0;
 }
