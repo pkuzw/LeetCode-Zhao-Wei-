33d4edc7d692460379db11ae64efd302032d920c
Zhao Wei <zhaowei.pku@gmail.com>
2015-08-21 16:16:05 +0800

    v2 - greedy
    1. 从左边算一遍，然后再从右边算一遍。
    2. 从左边算的时候，判断的依据是如果右边邻居的权值比较大，就将它等于左边邻居的糖果数加1；
    3. 从右边算的时候，判断的依据是如果它左边邻居的权值比较大，且左邻居的糖果数不大于右邻居，那么将左邻居的糖果数等于右邻居的糖果数加1.

--- Candy.cpp
+++ Candy.cpp
@@ -12,15 +12,20 @@ What is the minimum candies you must give?
 ///@date	2015.07.28
 ///@version	1.0
 
+///@date	2015.08.21
+///@version	2.0
+
 #include <vector>
+#include <numeric>
 using namespace std;
 
-class Solution {
+class Solution_v1 {
 public:
 	///@brief	给每个孩子发糖，使得权值较邻居更高的孩子拿糖更多，计算最少要准备多少糖。每个孩子至少一颗。
 	///@param	ratings	权值
 	///@return	返回需要准备的最少糖果数
-	/* @note	贪心法：从左向右先发一遍，给每个比他的左邻居权值高的孩子多发一颗糖；然后再从右向左遍历，给每个比他右邻居权值大且糖果数少的孩子多发一颗。
+	/* @note	贪心法：从左向右先发一遍，给每个比他的左邻居权值高的孩子多发一颗糖；然后再从右向左遍历，给每个比他右邻居权值大
+				且糖果数少的孩子多发一颗。
 				时间复杂度为O(n)，空间复杂度为O(1)。*/
 	int candy(vector<int>& ratings) {
 		if (ratings.empty())		return 0;
@@ -50,6 +55,25 @@ public:
 	}
 };
 
+class Solution {
+public:
+	int candy(vector<int>& ratings) {
+		if (ratings.empty())	return 0;
+		vector<int> child(ratings.size(), 1);
+		int rslt = 0;
+		for (int i = 0; i != ratings.size()-1; i++)
+		{
+			if (ratings[i+1] > ratings[i])	child[i+1] = child[i] + 1;
+		}
+		for (int i = ratings.size()-1; i >= 1; i--)
+		{
+			if (ratings[i-1] > ratings[i] && child[i-1] <= child[i])	child[i-1] = child[i] + 1;
+		}
+		rslt = accumulate(child.begin(), child.end(), rslt);
+		return rslt;
+	}
+};
+
 int main()
 {
 	int n[10] = {3,2,1,2,3,2,1,2,3,2};
@@ -59,5 +83,8 @@ int main()
 
 	Solution slt;
 	int rslt = slt.candy(ratings);
+
+	Solution_v1 slt_v1;
+	rslt = slt_v1.candy(ratings);
 	return 0;
 }
