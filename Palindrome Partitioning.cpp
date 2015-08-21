378d46aecac51698732fb4658e4eaf9a7ca5fe62
Zhao Wei <zhaowei.pku@gmail.com>
2015-08-21 18:06:18 +0800

    v2 - dfs
    1. dfs. 判断一个子串是否回文，如果是就将其压入一个结果数组，然后递归调用dfs函数，否则继续向后遍历，直到字符串末尾。

--- Palindrome Partitioning.cpp
+++ Palindrome Partitioning.cpp
@@ -16,12 +16,15 @@ Return
 ///@date	2015.07.24
 ///@version	1.0
 
+///@date	2015.08.21
+///@version	2.0
+
 #include <vector>
 #include <string>
 
 using namespace std;
 
-class Solution {
+class Solution_v1 {
 public:
 	///@brief	给定一个字符串s，对它进行划分，使得划分后的子串是回文的。
 	///@param	s	字符串
@@ -83,9 +86,57 @@ private:
 	}
 };
 
+class Solution {
+public:
+	///@brief	dfs. 判断一个子串是否回文，如果是就将其压入一个结果数组，然后递归调用dfs函数，否则继续向后遍历，直到字符串末尾。
+	vector<vector<string>> partition(string s) {
+		vector<string> part;
+		dfs(s, 0, part);
+		return rslt;
+	}
+
+private:
+	//	结果数组
+	vector<vector<string>> rslt;
+
+	///@brief	判断字符串是否是回文的
+	bool isPalindrome(const string& s)
+	{
+		int start = 0;
+		int end = s.size()-1;
+		while (s[start] == s[end])
+		{
+			start++;
+			end--;
+			if (start > end)	return true;
+		}
+		return false;
+	}
+
+	///@brief	深搜的递归版实现
+	void dfs(string s, int start, vector<string>& part)
+	{
+		if (start == s.size())
+		{
+			rslt.push_back(part);
+			return;
+		}
+
+		for (int i = start; i != s.size(); i++)
+		{
+			if (isPalindrome(s.substr(start, i - start + 1)))
+			{
+				part.push_back(s.substr(start, i - start + 1));
+				dfs(s, i + 1, part);
+				part.pop_back();
+			}
+		}
+	}
+};
+
 int main()
 {
-	string s = "a";
+	string s = "aab";
 	Solution slt;
 	vector<vector<string>> rslt = slt.partition(s);
 	return 0;
