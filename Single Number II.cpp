919a648b051a30864f49a4f4a6fdf03447f4b05a
Zhao Wei <zhaowei.pku@gmail.com>
2015-08-21 15:55:31 +0800

    v2 - bit operation
    1.
    因为数组中每个元素都出现三次，只有一个例外，所以对元素的每一位进行1的计数，在遍历所有元素后，如果该位的1累加之和模3不为0，则说明该位出现不是3次的元素的位，然后再逐一对各位进行累加即可。

--- Single Number II.cpp
+++ Single Number II.cpp
@@ -9,13 +9,15 @@ Your algorithm should have a linear runtime complexity. Could you implement it w
 ///@date	2015.07.27
 ///@version	1.0
 
+///@date	2015.08.21
+///@version	2.0
+
 #include <iostream>
 #include <unordered_map>
 #include <vector>
-
 using namespace std;
 
-class Solution {
+class Solution_v1 {
 public:
 	///@brief	找到数组中出现次数不是3次的数
 	///@param	nums	数组
@@ -29,6 +31,30 @@ public:
 		for (unordered_map<int, int>::iterator i = ht.begin(); i != ht.end(); i++)
 			if (i->second != 3)
 				return i->first;
+	}
+};
+
+///@note	因为数组中每个元素都出现三次，只有一个例外，所以对元素的每一位进行1的计数，在遍历所有元素后，如果该位的1累加之和模3不为0，
+//			则说明该位出现不是3次的元素的位，然后再逐一对各位进行累加即可。时间复杂度为O(n)，空间复杂度为O(1)。
+class Solution {
+public:
+	int singleNumber(vector<int>& nums) {
+		vector<int> cnt(32, 0);
+		int rslt = 0;
+		for (int i = 0; i != nums.size(); i++)
+		{
+			for (int j = 0; j != 32; j++)
+			{
+				if ((nums[i] >> j) & 1)
+					cnt[j] = (cnt[j] + 1) % 3;
+			}
+		}
+		for (int i = 0; i != 32; i++)
+		{
+			if (cnt[i])
+				rslt += (1 << i);
+		}
+		return rslt;
 	}
 };
 
@@ -46,7 +72,9 @@ int main()
 	nums.push_back(3);
 	nums.push_back(3);
 
+	Solution_v1 slt_v1;
+	int rslt = slt_v1.singleNumber(nums);
 	Solution slt;
-	int rslt = slt.singleNumber(nums);
+	rslt = slt.singleNumber(nums);
 	return 0;
 }
