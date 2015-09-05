f91067364fc4652266ebf669352896f5042742b7
Zhao Wei <pkuzw@126.com>
2015-09-05 23:39:18 +0800

    v2 - hash table

--- Substring with Concatenation of All Words.cpp
+++ Substring with Concatenation of All Words.cpp
@@ -188,23 +188,24 @@ private:
 class Solution {
 public:
     vector<int> findSubstring(string s, vector<string>& words) {
-        vector<int> res;
-        if (s.empty() || words.empty()) return res;
+        vector<int> rslt;
+        if (s.empty() || words.empty()) return rslt;
         int n = words.size(), m = words[0].size();
-        unordered_map<string, int> m1;
-        for (auto &a : words) ++m1[a];
-        for (int i = 0; i <= (int)s.size() - n * m; ++i) {
-            unordered_map<string, int> m2;
+        unordered_map<string, int> ht1;
+        for (int i = 0; i != words.size(); i++)
+            ht1[words[i]]++;
+        for (int i = 0; i <= s.size() - n * m; i++){
+            unordered_map<string, int> ht2;
             int j = 0;
-            for (j = 0; j < n; ++j) {
+            for (j = 0; j < n; j++){
                 string t = s.substr(i + j * m, m);
-                if (m1.find(t) == m1.end()) break;
-                ++m2[t];
-                if (m2[t] > m1[t]) break;
+                if (ht1.find(t) == ht1.end())   break;
+                ht2[t]++;
+                if (ht2[t] > ht1[t])    break;
             }
-            if (j == n) res.push_back(i);
+            if (j == n) rslt.push_back(i);
         }
-        return res;
+        return rslt;
     }
 };
 
