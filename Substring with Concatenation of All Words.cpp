b7f3c49e515e55e0b25bc65a1c4ca4ba85dbf2f0
Zhao Wei <pkuzw@126.com>
2015-09-05 16:51:16 +0800

    v2 - hash table

--- Substring with Concatenation of All Words.cpp
+++ Substring with Concatenation of All Words.cpp
@@ -15,14 +15,18 @@
 ///@date	2015.06.12
 ///@version	1.0
 
+///@date    2015.09.05
+///@version 2.0
+
 #include <iostream>
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <unordered_map>
 
 using namespace std;
 
-class Solution
+class Solution_v1
 {
 public:
 	///@brief	找子串组合
@@ -179,6 +183,29 @@ private:
 		}
 		return rslt;
 	}
+};
+
+class Solution {
+public:
+    vector<int> findSubstring(string s, vector<string>& words) {
+        vector<int> res;
+        if (s.empty() || words.empty()) return res;
+        int n = words.size(), m = words[0].size();
+        unordered_map<string, int> m1;
+        for (auto &a : words) ++m1[a];
+        for (int i = 0; i <= (int)s.size() - n * m; ++i) {
+            unordered_map<string, int> m2;
+            int j = 0;
+            for (j = 0; j < n; ++j) {
+                string t = s.substr(i + j * m, m);
+                if (m1.find(t) == m1.end()) break;
+                ++m2[t];
+                if (m2[t] > m1[t]) break;
+            }
+            if (j == n) res.push_back(i);
+        }
+        return res;
+    }
 };
 
 int main()
