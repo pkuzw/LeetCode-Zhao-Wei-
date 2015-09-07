e20eeb59479e369a7fb158511916f23b1986a3e5
Zhao Wei <pkuzw@126.com>
2015-09-07 14:53:35 +0800

    v2

--- Integer to Roman.cpp
+++ Integer to Roman.cpp
@@ -3,11 +3,14 @@
 ///@date	2015.06.03
 ///@version 1.0
 
+///@date    2015.09.07
+///@version 2.0
+
 #include <string>
 #include <iostream>
 using namespace std;
 
-class Solution
+class Solution_v1
 {
 public:
 	///@brief	阿拉伯数字转换成罗马数字
@@ -70,9 +73,30 @@ public:
 		}
 	
 		return rslt;		
-	}
+    }
+};
 
-	
+class Solution {
+public:
+    string intToRoman(int num) {
+        string res = "";
+        char roman[] = {'M', 'D', 'C', 'L', 'X', 'V', 'I'};
+        int value[] = {1000, 500, 100, 50, 10, 5, 1};
+        
+        for (int n = 0; n < 7; n += 2) {
+            int x = num / value[n];
+            if (x < 4) {
+                for (int i = 1; i <= x; ++i) res += roman[n];
+            } else if (x == 4) res = res + roman[n] + roman[n - 1];
+            else if (x > 4 && x < 9) {
+                res += roman[n - 1];
+                for (int i = 6; i <= x; ++i) res += roman[n];
+            }
+            else if (x == 9) res = res + roman[n] + roman[n - 2];
+            num %= value[n];
+        }
+        return res;
+    }
 };
 
 int main()
