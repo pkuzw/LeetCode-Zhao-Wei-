25ce859b9ae3f9a3183f9455e8990952bfbc0296
Zhao Wei <pkuzw@126.com>
2015-09-07 13:02:07 +0800

    v2 - bit operation

--- Divide Two Integers.cpp
+++ Divide Two Integers.cpp
@@ -103,24 +103,26 @@ public:
 	}
 };
 
+
 class Solution {
 public:
     int divide(int dividend, int divisor) {
-        if (divisor == 0 || (dividend == INT_MIN && divisor == -1)) return INT_MAX;
-        long m = labs(dividend), n = labs(divisor), res = 0;
+        if (divisor == 0 || (dividend == INT_MIN) && divisor == -1) return INT_MAX;
+        long m = labs(dividend), n = labs(divisor);
+        long rslt = 0;
         long sign = ((dividend < 0) ^ (divisor < 0)) ? -1 : 1;
         if (n == 1) return m * sign;
-        while (m >= n) {
+        while (m >= n){
             long k = n;
             int i = 0;
             while (k <= m) {
                 m -= k;
                 k <<= 1;
-                res += 1 << i;
-                ++i;
+                rslt += 1 << i;
+                i++;
             }
         }
-        return (res * sign);
+        return rslt;
     }
 };
 
