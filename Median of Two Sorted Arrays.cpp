ce6b33380d8022c1e57803c514fa675499b73b9d
Zhao Wei <pkuzw@126.com>
2015-09-07 14:59:12 +0800

    v2

--- Median of Two Sorted Arrays.cpp
+++ Median of Two Sorted Arrays.cpp
@@ -27,12 +27,16 @@ The overall run time complexity should be O(log (m+n)).
 ///@author zhaowei
 ///@date 2014.09.24
 ///@version 1.2
+
+///@date    2015.09.07
+///@version 2.0
+
 #include <iostream>
 #include <vector>
 #include <assert.h>
 using namespace std;
 
-class Solution 
+class Solution_v1
 {
 public:
 	/*
@@ -187,6 +191,30 @@ public:
 	}
 };
 
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int total = nums1.size() + nums2.size();
+        if (total % 2 == 1) {
+            return findKth(nums1, 0, nums2, 0, total / 2 + 1);
+        } else {
+            return (findKth(nums1, 0, nums2, 0, total / 2) + findKth(nums1, 0, nums2, 0, total / 2 + 1)) / 2;
+        }
+    }
+    double findKth(vector<int> &nums1, int i, vector<int> &nums2, int j, int k) {
+        if (nums1.size() - i > nums2.size() - j) return findKth(nums2, j, nums1, i, k);
+        if (nums1.size() == i) return nums2[j + k - 1];
+        if (k == 1) return min(nums1[i], nums2[j]);
+        int pa = min(i + k / 2, int(nums1.size())), pb = j + k - pa + i;
+        if (nums1[pa - 1] < nums2[pb - 1])
+            return findKth(nums1, pa, nums2, j, k - pa + i);
+        else if (nums1[pa - 1] > nums2[pb - 1])
+            return findKth(nums1, i, nums2, pb, k - pb + j);
+        else
+            return nums1[pa - 1];
+    }
+};
+
 int main()
 {
 	//样例测试
@@ -208,7 +236,7 @@ int main()
 		cin >> arr_b[i];
 
 	double median = 0;
-	Solution slt;
+	Solution_v1 slt;
 	median = slt.findMedianSortedArrays(arr_a, len_a, arr_b, len_b);
 	cout << "program's result: " << median << endl;
 
