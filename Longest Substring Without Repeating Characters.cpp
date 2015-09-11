0541d555d4c5936a49957465a5f49a095fa22157
Zhao Wei <zhaowei.pku@gmail.com>
2015-09-11 14:05:43 +0800

    v1.1 dynamic programming
    1.
    设last_occur[i]表示字符i上次出现的位置。那么如果在遍历字符串时，如果当前下标减去上次出现该字符的下标的长度小于等于最长长度时，最长长度应该为当前下标减去上次出现该字符的下标；否则最长长度自增1.
    在每遍历一个字符时，就要更新每个字符的上次出现位置，并同时更新最长子串长。
    2. 时间复杂度为O(n)，空间复杂度为O(1)。

--- Longest Substring Without Repeating Characters.cpp
+++ Longest Substring Without Repeating Characters.cpp
@@ -10,7 +10,7 @@ For "bbbbb" the longest substring is "b", with the length of 1.
 ///@date	2015.09.10
 ///@version	1.0
 
-#include <unordered_map>
+#include <vector>
 #include <string>
 using namespace std;
 
@@ -19,22 +19,29 @@ public:
 	///@brief	计算最长无重复字符的子串长度
 	///@param	s	字符串
 	///@return	返回最长无重复字符的子串长度
-	///@note	
+	///@note	设last_occur[i]表示字符i上次出现的位置。那么如果在遍历字符串时，如果当前下标减去上次出现该字符的下标的长度小于等于
+	//			最长长度时，最长长度应该为当前下标减去上次出现该字符的下标；否则最长长度自增1. 在每遍历一个字符时，就要更新每个字符的上次
+	//			出现位置，并同时更新最长子串长。
+	//			时间复杂度为O(n)，空间复杂度为O(1)。
 	int lengthOfLongestSubstring(string s) {
-		int m[256] = {0}, res = 0, left = 0;
-		for (int i = 0; i < s.size(); ++i) {
-			if (m[s[i]] == 0 || m[s[i]] < left) {
-				res = max(res, i - left + 1);
-			} else {
-				left = m[s[i]];
-			}
-			m[s[i]] = i + 1;
+		vector<int> last_occur(128, -1);
+		int rslt = 0;
+		int longest_len = 0;
+		for (int i = 0; i != s.size(); i++) {
+			if (i - last_occur[s[i]] <= longest_len)	longest_len = i - last_occur[s[i]];
+			else	longest_len++;
+			last_occur[s[i]] = i;
+			rslt = max(rslt, longest_len);
 		}
-		return res;
+		return rslt;
 	}
 };
 
 int main()
 {
+	string s = "abcdcfg";
+	Solution slt;
+	int rslt = slt.lengthOfLongestSubstring(s);
+
 	return 0;
 }
