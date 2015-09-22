b9013b5d15f62beba2d0284fc808fd084d2b1f98
Zhao Wei <zhaowei.pku@gmail.com>
2015-09-22 22:24:24 +0800

    v2.1 - two loops
    1. 用两重循环遍历字符串，外层循环用来作为字符串的起始位置遍历，内层循环用来匹配子串。
    2. 时间复杂度为O(nk)，其中字符串长度为n，子串长度为k，空间复杂度为O(1)。

--- Implement strStr.cpp
+++ Implement strStr.cpp
@@ -11,6 +11,9 @@
 ///@date    2015.09.07
 ///@version 2.0
 
+///@date	2015.09.22
+///@version	2.1
+
 #include <iostream>
 #include <string>
 using namespace std;
@@ -69,23 +72,29 @@ private:
 
 class Solution {
 public:
+	///@brief	计算字符串中指定子串第一次出现的位置
+	///@param	haystack	字符串
+	///@param	needle		子串
+	///@return	返回子串第一次出现的首字符下标
+	///@note	用两重循环遍历字符串，外层循环用来作为字符串的起始位置遍历，内层循环用来匹配子串。时间复杂度为O(nk)，其中字符串长度为n，子串长度为k，空间复杂度为O(1)。
     int strStr(string haystack, string needle) {
-        if (needle.empty()) return 0;
-        if (needle.size() > haystack.size())    return -1;
-        int i, j;
-        for (i = 0; i != haystack.size() - needle.size() + 1; i++){
-            for (j = 0; j != needle.size(); j++)
-                if (needle[j] != haystack[i+j]) break;
-            if (j == needle.size()) return i;
-        }
-        return -1;
+		if (needle.size() > haystack.size())	return -1;
+		if (needle.empty())	return 0;
+		int i = 0, j = 0;
+		for (i = 0; i <= haystack.size() - needle.size(); i++) {
+			for (j = 0; j != needle.size(); j++) {
+				if (needle[j] != haystack[i + j])	break;
+			}
+			if (j == needle.size())	return i;
+		}
+		return -1;
     }
 };
 
 int main()
 {
 	string s = "hello";
-	string n = "eh";
+	string n = "ll";
 	Solution slt;
 	cout <<slt.strStr(s, n) << endl;
 	return 0;
