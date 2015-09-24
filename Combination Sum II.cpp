bfee46c7a8dab7ac255aff0dd2590f7a82e28e2f
Zhao Wei <zhaowei.pku@gmail.com>
2015-09-24 17:04:13 +0800

    v2.1 - backtracking & recursive
    1. 与"Combination Sum I"类似，也是回溯法解题。只是在递归遍历时，候选元素的下标要向后移一位。
    2. 另外去重也是要注意的部分，在第一题中候选数组中没有重复元素，此题则有重复元素。在递归函数里面遍历候选数组时，遇到重复元素则跳过即可。

--- Combination Sum II.cpp
+++ Combination Sum II.cpp
@@ -23,6 +23,8 @@
 ///@date    2015.09.04
 ///@version 2.0
 
+///@date	2015.09.24
+///@version	2.1
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -70,7 +72,7 @@ private:
 	vector<vector<int>> rslt;
 };
 
-class Solution {
+class Solution_v2 {
 public:
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         vector<vector<int>> rslt;
@@ -93,12 +95,45 @@ public:
     }
 };
 
+class Solution {
+public:
+	///@brief	给定一个数组和一个指定值，找出所有可能的组合，使之和为指定值。其中每个元素不能重复出现。
+	///@param	candidates	数组
+	///@param	target		目标值
+	///@note	与"Combination Sum I"类似，也是回溯法解题。只是在递归遍历时，候选元素的下标要向后移一位。另外去重也是要注意的部分，在第一题中候选数组中没有重复元素，此题则有重复元素。
+	//			在递归函数里面遍历候选数组时，遇到重复元素则跳过即可。
+	vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+		vector<vector<int>> rslt;
+		if (candidates.empty())	return rslt;
+		vector<int> comb;
+		sort(candidates.begin(), candidates.end());
+		dfs(rslt, comb, candidates, 0, target);
+		return rslt;
+	}
+
+	///@brief	回溯递归求解组合
+	///@param	rslt	结果集合
+	///@param	comb	一个结果
+	///@param	candidates	候选数组
+	///@param	indx	候选数组的元素下标
+	///@param	target	目标值
+	void dfs(vector<vector<int>>& rslt, vector<int>& comb, vector<int>& candidates, int indx, int target) {
+		if (!target)	rslt.push_back(comb);
+		else {
+			for (int i = indx; i < candidates.size() && candidates[i] <= target; i++) {
+				if (i > indx && candidates[i] == candidates[i-1])	continue;	//	跳过候选数组中的重复元素
+				comb.push_back(candidates[i]);
+				dfs(rslt, comb, candidates, i + 1, target - candidates[i]);
+				comb.pop_back();
+			}
+		}
+	}
+};
+
 int main()
 {
 	vector<int> test;
 	test.push_back(10);
-	test.push_back(1);
-	test.push_back(1);
 	test.push_back(1);
 	test.push_back(2);
 	test.push_back(7);
