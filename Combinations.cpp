1a72a7fb75d61249d3729ef76f713e820b74ce0c
Zhao Wei <zhaowei.pku@gmail.com>
2015-12-07 21:05:21 +0800

    v2.1 - recursive
    1. 递归：从n个元素中选取k个元素可以先从n个元素中选取一个元素，然后再从剩余n-1个元素中选取k-1个元素。
    2. 时间复杂度为O(n!)，空间复杂度为O(n^2)。

--- Combinations.cpp
+++ Combinations.cpp
@@ -21,6 +21,9 @@ If n = 4 and k = 2, a solution is:
 ///@date	2015.08.26
 ///@version	2.0
 
+///@date	2015.12.07
+///@version	2.1
+
 #include <iostream>
 #include <vector>
 
@@ -73,28 +76,37 @@ private:
 
 class Solution {
 public:
+	///@brief	计算C(n, k)
+	///@param	n	候选元素数目
+	///@param	k	选出的元素数目
+	///@return	返回所有可能的组合
+	///@note	1. 递归：从n个元素中选取k个元素可以先从n个元素中选取一个元素，然后再从剩余n-1个元素中选取k-1个元素。
+	//			2. 时间复杂度为O(n!)，空间复杂度为O(n^2)。
 	vector<vector<int>> combine(int n, int k) {
 		vector<vector<int>> rslt;
-		vector<int> combination;
 		vector<int> nums;
-		for (int i = 0; i != n; i++)
-			nums.push_back(i+1);		
-		getCombination(nums, rslt, combination, n, k);
+		for (int i = 1; i <= n; i++)	nums.push_back(i);
+		vector<int> comb;
+		getCombination(nums, rslt, comb, n, k);
 		return rslt;
 	}
+
 private:
-	void getCombination(vector<int> nums, vector<vector<int>>& rslt, vector<int>& combination, int n, int k)
-	{
-		if (!k)
-		{
+	///@brief	递归计算C(n, k)
+	///@param	nums	候选元素数组
+	///@param	 rslt	结果数组
+	///@param	 combination	一个组合
+	///@param	n		候选元素个数
+	///@param	k		选出的元素个数
+	void getCombination(vector<int> nums, vector<vector<int>>& rslt, vector<int>& combination, int n, int k) {
+		if (!k) {
 			rslt.push_back(combination);
 			return;
 		}
-		for (int i = 0; i != nums.size(); i++)
-		{
+		for (int i = 0; i < nums.size(); i++) {
 			combination.push_back(nums[i]);
-			vector<int> new_nums(nums.begin()+i+1, nums.end());
-			getCombination(new_nums, rslt, combination, n-1, k-1);
+			vector<int> new_nums(nums.begin() + i + 1, nums.end());
+			getCombination(new_nums, rslt, combination, n - 1, k - 1);
 			combination.pop_back();
 		}
 	}
