4ca5fc58eb8b15ee8437588853d190f0a4a9617f
Zhao Wei <zhaowei.pku@gmail.com>
2016-01-11 12:05:02 +0800

    v1.0
    1. 写文件注释。

---
+++ Strobogrammatic Number II.cpp
@@ -0,0 +1,25 @@
+﻿///@file	Strobogrammatic Number II
+///@author	zhaowei
+///@date	2016.01.11
+///@version	1.0
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	///@brief	找到所有长度为n的旋转回文数字串
+	///@param	n	长度
+	///@return	返回所有长度为n的旋转回文数字串
+	///@note	
+	vector<string> findStrobogrammatic(int n) {
+
+	}
+};
+
+int main() {
+
+	return 0;
+}
