d4954047942c876274b6f260bc9a81a468468fc5
Zhao Wei <zhaowei.pku@gmail.com>
2016-01-11 11:27:47 +0800

    v1.2 - dynamic programming
    1. 动态规划；
    2. 设dp[i]表示s[0..i-1]是否能被切分成词典中的单词，初始化dp[0] = true；
    3. 递推关系式dp[i] = dp[j] && s[j..i-1]是词典中的单词；
    4. 时间复杂度为O(n^2)，空间复杂度O(n)。

--- Word Break.cpp
+++ Word Break.cpp
@@ -13,7 +13,10 @@ Return true because "leetcode" can be segmented as "leet code".
 ///@version	1.0
 
 ///@date	2015.08.21
-///@version	2.0
+///@version	1.1
+
+///@date	2016.01.11
+///@version	1.2
 
 #include <vector>
 #include <string>
@@ -50,23 +53,27 @@ public:
 
 class Solution {
 public:
+	///@brief	判断一个字符串能否被切分成字典中的单词
+	///@param	s	字符串
+	///@param	wordDict	词典
+	///@return	如果可以则返回true；否则返回false
+	///@note	1. 动态规划；2. 设dp[i]表示s[0..i-1]是否能被切分成词典中的单词，初始化dp[0] = true；
+	//			3. 递推关系式dp[i] = dp[j] && s[j..i-1]是词典中的单词；
+	//			4. 时间复杂度为O(n^2)，空间复杂度O(n)。
 	bool wordBreak(string s, unordered_set<string>& wordDict) {
-		if (s.empty())	return false;
-		vector<bool> dp(s.size()+1, false);
+		if (s.empty() || wordDict.empty())	return false;
+		const int len = s.size();
+		vector<bool> dp(len + 1, false);
 		dp[0] = true;
-
-		for (int i = 0; i != s.size()+1; i++)
-		{
-			for (int j = 0; j < i; j++)
-			{
-				if (dp[j] && wordDict.find(s.substr(j, i-j)) != wordDict.end())
-				{
+		for (int i = 0; i <= len; i++) {
+			for (int j = 0; j < i; j++) {
+				if (dp[j] && wordDict.find(s.substr(j, i - j)) != wordDict.end()) {
 					dp[i] = true;
 					break;
 				}
 			}
 		}
-		return dp[s.size()];
+		return dp[len];
 	}
 };
 
