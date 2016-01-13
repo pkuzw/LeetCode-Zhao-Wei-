4a2cc53bf3743549bc3f28934c2bf6ff767bf4cf
Zhao Wei <zhaowei.pku@gmail.com>
2016-01-13 20:13:23 +0800

    v1.2
    1. 利用双向链表来保存节点，用哈希表保存节点的指针；
    2. 越靠近队首的节点，越使用频繁；
    3. 在访问过一次某节点后，要将其更新到队首，同时更新哈希表。

--- LRU Cache.cpp
+++ LRU Cache.cpp
@@ -10,7 +10,10 @@ set(key, value) - Set or insert the value if the key is not already present. Whe
 ///@versio	1.0
 
 ///@date	2015.08.20
-///@version	2.0
+///@version	1.1
+
+///@date	2016.01.13
+///@version	1.2
 
 #include <unordered_map>
 #include <list>
@@ -18,7 +21,7 @@ set(key, value) - Set or insert the value if the key is not already present. Whe
 using namespace std;
 
 /*
-为了保持cache的性能，使查找，插入，删除都有较高的性能，我们使用双向链表（std::list）和哈希表（std::unordered_map）作为cache的数据结构，
+为了保持cache的性能，使查找，插入，删除都有较高的性能，使用双向链表（std::list）和哈希表（std::unordered_map）作为cache的数据结构，
 
 因为：
 双向链表插入删除效率高（单向链表插入和删除时，还要查找节点的前节点）
@@ -56,7 +59,9 @@ public:
 		else
 		{
 			//把当前访问的节点移到链表头部,并且更新map中该节点的地址
-			cacheList.splice(cacheList.begin(), cacheList, cacheMap[key]);	//list::splice实现list拼接的功能。将源list的内容部分或全部元素删除，拼插入到目的list。目的list的大小会增加，增加的大小为插入元素的大小。x的大小相应的会减少同样的大小。
+			//list::splice实现list拼接的功能。将源list的内容部分或全部元素删除，拼插入到目的list。
+			//目的list的大小会增加，增加的大小为插入元素的大小。源list的大小相应的会减少同样的大小。
+			cacheList.splice(cacheList.begin(), cacheList, cacheMap[key]);
 			cacheMap[key] = cacheList.begin();
 			return cacheMap[key]->value;
 		}
@@ -84,6 +89,71 @@ public:
 	}
 };
 
+/*
+class LRUCache{
+ public:
+ 	LRUCache(int capacity) {
+ 		size = capacity;
+ 	}
+ 
+ 	int get(int key) {
+ 		if (cache_map.find(key) == cache_map.end())	return -1;
+ 		cache_list.splice(cache_list.begin(), cache_list, cache_map[key]);
+ 		cache_map[key] = cache_list.begin();
+ 		return cache_map[key]->val;
+ 	}
+ 
+ 	void set(int key, int value) {
+ 		if (cache_map.find(key) == cache_map.end())
+ 		{			
+ 			if (size == cache_list.size())
+ 			{
+ 				cache_map.erase(cache_map.find(cache_list.back().key));
+ 				cache_list.pop_back();				
+ 			}
+ 			CacheNode new_node(key, value);
+ 			cache_list.push_front(new_node);
+ 			cache_map[key] = cache_list.begin();
+ 
+ 		}
+ 		else
+ 		{
+ 			cache_map[key]->val = value;
+ 			cache_list.splice(cache_list.begin(), cache_list, cache_map[key]);
+ 			cache_map[key] = cache_list.begin();
+ 		}
+ 	}
+ 
+ private:
+ 	struct CacheNode
+ 	{
+ 		int key;
+ 		int val;
+ 		CacheNode (int k, int v)
+ 		{
+ 			key = k;
+ 			val = v;
+ 		}
+ 	};
+ 
+ 	int size;
+ 	list<CacheNode> cache_list;
+ 	unordered_map<int, list<CacheNode>::iterator> cache_map;
+ };
+*/
+// 1. 利用双向链表来保存节点，用哈希表保存节点的指针；
+// 2. 越靠近队首的节点，越使用频繁；
+// 3. 在访问过一次某节点后，要将其更新到队首，同时更新哈希表。
+class LRUNode {
+public:
+	int key;
+	int val;
+	LRUNode(int a, int b) {
+		key = a;
+		val = b;
+	}
+};
+
 class LRUCache{
 public:
 	LRUCache(int capacity) {
@@ -91,48 +161,32 @@ public:
 	}
 
 	int get(int key) {
-		if (cache_map.find(key) == cache_map.end())	return -1;
-		cache_list.splice(cache_list.begin(), cache_list, cache_map[key]);
-		cache_map[key] = cache_list.begin();
-		return cache_map[key]->val;
+		if (hash_tbl.find(key) == hash_tbl.end())	return -1;
+		LRUList.splice(LRUList.begin(), LRUList, hash_tbl[key]);
+		hash_tbl[key] = LRUList.begin();
+		return hash_tbl[key]->val;
 	}
 
 	void set(int key, int value) {
-		if (cache_map.find(key) == cache_map.end())
-		{			
-			if (size == cache_list.size())
-			{
-				cache_map.erase(cache_map.find(cache_list.back().key));
-				cache_list.pop_back();				
+		if (hash_tbl.find(key) == hash_tbl.end()) {
+			if (hash_tbl.size() == size) {
+				hash_tbl.erase(hash_tbl.find(LRUList.back().key));
+				LRUList.pop_back();
 			}
-			CacheNode new_node(key, value);
-			cache_list.push_front(new_node);
-			cache_map[key] = cache_list.begin();
-
+			LRUNode new_node(key, value);
+			LRUList.push_front(new_node);
+			hash_tbl[key] = LRUList.begin();		
 		}
-		else
-		{
-			cache_map[key]->val = value;
-			cache_list.splice(cache_list.begin(), cache_list, cache_map[key]);
-			cache_map[key] = cache_list.begin();
+		else {
+			hash_tbl[key]->val = value;
+			LRUList.splice(LRUList.begin(), LRUList, hash_tbl[key]);
+			hash_tbl[key] = LRUList.begin();
 		}
 	}
-
 private:
-	struct CacheNode
-	{
-		int key;
-		int val;
-		CacheNode (int k, int v)
-		{
-			key = k;
-			val = v;
-		}
-	};
-
+	list<LRUNode> LRUList;
 	int size;
-	list<CacheNode> cache_list;
-	unordered_map<int, list<CacheNode>::iterator> cache_map;
+	unordered_map<int, list<LRUNode>::iterator> hash_tbl;
 };
 
 int main()
