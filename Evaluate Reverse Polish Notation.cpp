ee8f2c04090a38f83dc1d4f19bba758276484aa1
Zhao Wei <zhaowei.pku@gmail.com>
2016-01-13 21:31:35 +0800

    v1.2 - stack
    1. 栈；
    2. 注意'-'和'/'是需要区分顺序的，后弹出的者作为第一操作数，先弹出的是第二操作数。

--- Evaluate Reverse Polish Notation.cpp
+++ Evaluate Reverse Polish Notation.cpp
@@ -13,7 +13,10 @@ Some examples:
 ///@version	1.0
 
 ///@date	2015.08.19
-///@version	2.0
+///@version	1.1
+
+///@date	2016.01.13
+///@version	1.2
 
 #include <iostream>
 #include <stack>
@@ -76,48 +79,95 @@ public:
 
 class Solution {
 public:
+	///@brief	计算逆波兰表达式的值
+	///@param	tokens	字符串数组：数字字符，或者+,-,*,/
+	///@return	返回表达式的值
+	///@note	1. 栈；2. 注意'-'和'/'是需要区分顺序的，后弹出的者作为第一操作数，先弹出的是第二操作数。
 	int evalRPN(vector<string>& tokens) {
-		if (tokens.size() == 1)	return stoi(tokens.front());
-		
+		if (tokens.size() == 1)	return stoi(tokens[0]);
 		stack<int> stk;
-		for (int i = 0; i != tokens.size(); i++)
-		{
-			if (tokens[i] != "+" && tokens[i] != "-" && tokens[i] != "*" && tokens[i] != "/")
-				stk.push(stoi(tokens[i]));
-			else if (tokens[i] == "+")
-			{
-				int a = stk.top();
-				stk.pop();
-				int b = stk.top();
-				stk.pop();
-				stk.push(a + b);			
+		for (int i = 0; i != tokens.size(); i++) {
+			if (tokens[i] != "+" && tokens[i] != "-" && tokens[i] != "*" && tokens[i] != "/")	stk.push(stoi(tokens[i]));
+			else if (tokens[i] == "+") {
+				int a = stk.top();stk.pop();
+				int b = stk.top();stk.pop();
+				stk.push(a + b);
 			}
-			else if (tokens[i] == "-")
-			{
-				int a = stk.top();
-				stk.pop();
-				int b = stk.top();
-				stk.pop();
-				stk.push(b - a);			
+			else if (tokens[i] == "-") {
+				int a = stk.top();stk.pop();
+				int b = stk.top();stk.pop();
+				stk.push(b - a);
 			}
-			else if (tokens[i] == "*")
-			{
-				int a = stk.top();
-				stk.pop();
-				int b = stk.top();
-				stk.pop();
-				stk.push(a * b);			
+			else if (tokens[i] == "*") {
+				int a = stk.top();stk.pop();
+				int b = stk.top();stk.pop();
+				stk.push(a * b);
 			}
-			else
-			{
-				int a = stk.top();
-				stk.pop();
-				int b = stk.top();
-				stk.pop();
-				stk.push(b / a);			
+			else if (tokens[i] == "/") {
+				int a = stk.top();stk.pop();
+				int b = stk.top();stk.pop();
+				stk.push(b / a);
 			}
 		}
 		return stk.top();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 		if (tokens.size() == 1)	return stoi(tokens.front());
+// 		
+// 		stack<int> stk;
+// 		for (int i = 0; i != tokens.size(); i++)
+// 		{
+// 			if (tokens[i] != "+" && tokens[i] != "-" && tokens[i] != "*" && tokens[i] != "/")
+// 				stk.push(stoi(tokens[i]));
+// 			else if (tokens[i] == "+")
+// 			{
+// 				int a = stk.top();
+// 				stk.pop();
+// 				int b = stk.top();
+// 				stk.pop();
+// 				stk.push(a + b);			
+// 			}
+// 			else if (tokens[i] == "-")
+// 			{
+// 				int a = stk.top();
+// 				stk.pop();
+// 				int b = stk.top();
+// 				stk.pop();
+// 				stk.push(b - a);			
+// 			}
+// 			else if (tokens[i] == "*")
+// 			{
+// 				int a = stk.top();
+// 				stk.pop();
+// 				int b = stk.top();
+// 				stk.pop();
+// 				stk.push(a * b);			
+// 			}
+// 			else
+// 			{
+// 				int a = stk.top();
+// 				stk.pop();
+// 				int b = stk.top();
+// 				stk.pop();
+// 				stk.push(b / a);			
+// 			}
+// 		}
+// 		return stk.top();
 	}
 };
 
