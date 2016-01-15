2f7b82fab256869dd00f64a7a36d04db36f11b8f
Zhao Wei <zhaowei.pku@gmail.com>
2016-01-15 14:15:29 +0800

    v1.1 - recursive
    1. 递归。

--- Binary Tree Upside Down.cpp
+++ Binary Tree Upside Down.cpp
@@ -1,6 +1,10 @@
 ﻿///@file	Binary Tree Upside Down
 ///@author	zhaowei
 ///@date	2016.01.01
+///@version	1.0
+
+///@date	2016.01.15
+///@version	1.1
 /*
 Given a binary tree where all the right nodes are either leaf nodes with a sibling (a left node that shares the same parent node) or empty, 
 flip it upside down and turn it into a tree where the original right nodes turned into left leaf nodes. Return the new root.
@@ -36,17 +40,34 @@ public:
 	//			颠倒过来，将原来的所有右孩子变为左孩子。
 	///@param	root	二叉树根节点
 	///@return	返回新二叉树的根节点
-	///@note	1. 递归；2. 新建三个临时变量，cur_left表示当前节点root的左孩子，cur_right表示当前节点root的右孩子，new_root表示新根节点，通过调用递归函数初始化；
-	//			3. 函数结束递归的条件是当前节点为空或者当前节点的左孩子为空；4. 在递归函数跳出后，cur_left是新的子树节点，将其左孩子设置为cur_right，将其右孩子设置为
-	//			当前节点；5. 将当前节点的左右孩子置为空；6. 最后返回新的根节点new_root即可。
+	///@note	1. 递归；
+	//			2. 新建三个临时变量，cur_left表示当前节点root的左孩子，cur_right表示当前节点root的右孩子，new_root表示新根节点，
+	//			   通过调用递归函数初始化；
+	//			3. 函数结束递归的条件是当前节点为空或者当前节点的左孩子为空；
+	//			4. 在递归函数跳出后，cur_left是新的子树节点，将其左孩子设置为cur_right，将其右孩子设置为当前节点；
+	//			5. 将当前节点的左右孩子置为空；
+	//			6. 最后返回新的根节点new_root即可。
     TreeNode* upsideDownBinaryTree(TreeNode* root) {
-        if (!root || !root->left)	return root;
+		if (!root || !root->left)	return root;
 		TreeNode* cur_left = root->left, *cur_right = root->right, *new_root = upsideDownBinaryTree(cur_left);
 		cur_left->left = cur_right;
 		cur_left->right = root;
 		root->left = nullptr;
 		root->right = nullptr;
 		return new_root;
+
+
+
+
+
+
+//      if (!root || !root->left)	return root;
+// 		TreeNode* cur_left = root->left, *cur_right = root->right, *new_root = upsideDownBinaryTree(cur_left);
+// 		cur_left->left = cur_right;
+// 		cur_left->right = root;
+// 		root->left = nullptr;
+// 		root->right = nullptr;
+// 		return new_root;
     }
 };
 
