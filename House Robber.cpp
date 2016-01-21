3a42a39ca717562d7f22e38b4deb7b05775fcb0b
Zhao Wei <pkuzw@126.com>
2016-01-21 21:24:10 +0800

    v1.2 - dynamic programming
    1. 动态规划。
    2. 设dp[i]表示抢前i栋房子的最大收益，初始化dp[0] = 0, dp[1] = nums[0]。
    3. 递推关系式为dp[i] = max(dp[i-1], dp[i-2] + nums[i-1])。
    4. 时间复杂度为O(n)，空间复杂度为O(1)。

--- House Robber.cpp
+++ House Robber.cpp
@@ -9,7 +9,10 @@ Given a list of non-negative integers representing the amount of money of each h
 ///@version	1.0
 
 ///@date	2015.08.11
-///@version	2.0
+///@version	1.1
+
+///@date    2016.01.21
+///@version 1.2
 
 #include <vector>
 
@@ -41,17 +44,23 @@ public:
 
 class Solution {
 public:
+    ///@brief   抢劫一排房子，不能抢相邻的两栋房子，求最大金额。
+    ///@param   nums    房子
+    ///@return  返回最大金额
+    ///@note    1. 动态规划。
+    //          2. 设dp[i]表示抢前i栋房子的最大收益，初始化dp[0] = 0, dp[1] = nums[0]。
+    //          3. 递推关系式为dp[i] = max(dp[i-1], dp[i-2] + nums[i-1])。
+    //          4. 时间复杂度为O(n)，空间复杂度为O(1)。
 	int rob(vector<int>& nums) {
-		if (nums.empty())		return 0;
-		if (nums.size() == 1)	return nums[0];
-
-		vector<int> dp(nums.size(), 0);
-		dp[0] = nums[0];
-		dp[1] = max(nums[0], nums[1]);
-
-		for (int i = 2; i != nums.size(); i++)
-			dp[i] = max(dp[i-1], nums[i] + dp[i-2]);
-		return dp.back();
+        if (nums.empty())   return 0;
+        else if (nums.size() == 1)  return nums[0];
+        int dp0 = 0, dp1 = nums[0];
+        for (int i = 1; i < nums.size(); i++) {
+            int tmp = dp1;
+            dp1 = max(dp1, dp0 + nums[i]);
+            dp0 = tmp;
+        }
+        return dp1;
 	}
 };
 
