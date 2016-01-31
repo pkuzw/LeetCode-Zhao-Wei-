fa2715971b9673927d11dad3d44be0e5f170e82d
Zhao Wei <zhaowei.pku@gmail.com>
2016-01-31 11:19:43 +0800

    v1.2 - deque
    1. 用双端队列来维护遍历过元素的所有窗口的最大值下标，并维护其按降序排序；
    2. 如果队列不为空且后边界超过了窗口大小，弹出队首；
    3. 如果队列不为空且待压入元素大于队尾元素，则一直弹出队尾元素，知道不符合该条件为止；
    4. 每次当达到一个窗口大小时，就将队首压入结果数组。

--- Sliding Window Maximum.cpp
+++ Sliding Window Maximum.cpp
@@ -26,7 +26,10 @@ Could you solve it in linear time?
 ///@version	1.0
 
 ///@date	2015.08.06
-///@version	2.0
+///@version	1.1
+
+///@date	2016.01.31
+///@version	1.2
 
 #include <vector>
 #include <deque>
@@ -40,8 +43,9 @@ public:
 	///@param	nums	数组
 	///@param	k		窗口大小
 	///@return	返回窗口最大值的累加值
-	/* @note	用一个双端队列保存当前窗口中的最大元素，队首元素是当前窗口的最大值下标，队列中的元素会按照它所代表的数组元素的大小降序排列。至少保证有一个元素。
-				时间复杂度为O(nlogn)，空间复杂度为O(n)。*/
+	/* @note	用一个双端队列保存当前窗口中的最大元素，队首元素是当前窗口的最大值下标，队列中的元素会按照它所代表的数组元素的大小降序排列。
+				至少保证有一个元素。
+				时间复杂度为O(n)，空间复杂度为O(n)。*/
 	vector<int> maxSlidingWindow(vector<int>& nums, int k) {
 		vector<int> rslt;
 		deque<int> deq;
@@ -63,20 +67,18 @@ public:
 
 class Solution {
 public:
+	///@note	1. 用双端队列来维护遍历过元素的所有窗口的最大值下标，并维护其按降序排序；
+	//			2. 如果队列不为空且后边界超过了窗口大小，弹出队首；
+	//			3. 如果队列不为空且待压入元素大于队尾元素，则一直弹出队尾元素，知道不符合该条件为止；
+	//			4. 每次当达到一个窗口大小时，就将队首压入结果数组。
 	vector<int> maxSlidingWindow(vector<int>& nums, int k) {
 		vector<int> rslt;
 		deque<int> deq;
-		for (int i = 0; i != nums.size(); i++)
-		{
-			if (!deq.empty() && i - deq.front() >= k)
-				deq.pop_front();
-			while (!deq.empty() && nums[i] > nums[deq.back()])
-				deq.pop_back();
-
+		for (int i = 0; i != nums.size(); i++) {
+			if (!deq.empty() && i - deq.front() >= k)		deq.pop_front();
+			while (!deq.empty() && nums[i] > nums[deq.back()]) deq.pop_back();
 			deq.push_back(i);
-
-			if (i >= k-1)
-				rslt.push_back(nums[deq.front()]);
+			if (i >= k - 1)	rslt.push_back(nums[deq.front()]);
 		}
 		return rslt;
 	}
