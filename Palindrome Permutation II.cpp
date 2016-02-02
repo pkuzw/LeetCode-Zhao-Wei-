7b39767aa0bfdcc2ce661ab88ff215527229d1bd
Zhao Wei <zhaowei.pku@gmail.com>
2016-02-03 01:33:36 +0800

    v1.0
    vs2015可以在windows10下添加中文注释。

--- Palindrome Permutation II.cpp
+++ Palindrome Permutation II.cpp
@@ -1,4 +1,4 @@
-///@file	Palindrome Permutation II
+﻿///@file	Palindrome Permutation II
 ///@author	zhaowei
 ///@date	2016.02.02
 ///@version	1.0
@@ -10,7 +10,10 @@ using namespace std;
 
 class Solution {
 public:
-	///@brief	hello world 
+	///@brief	给定一个字符串，通过重新排列，组成回文字符串
+	///@param	s	字符串
+	///@return	返回所有可能的回文字符串 
+	///@note	1. 假设
 	vector<string> generatePalindromes(string s) {
 
 	}
