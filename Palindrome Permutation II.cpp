a8c11ff445f81259c18739a0984897b243d1e8d9
Zhao Wei <zhaowei.pku@gmail.com>
2016-02-02 20:54:55 +0800

    v1.0
    添加头部注释。

---
+++ Palindrome Permutation II.cpp
@@ -0,0 +1,21 @@
+﻿///@file	Palindrome Permutation II
+///@author	zhaowei
+///@date	2016.02.02
+///@version	1.0
+
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+	vector<string> generatePalindromes(string s) {
+
+	}
+};
+
+int main() {
+	
+	return 0;
+}
