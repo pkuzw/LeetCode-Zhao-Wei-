3e1f4b9420b567fbe6deb744838e10b21078f5f2
Zhao Wei <zhaowei.pku@gmail.com>
2016-02-05 19:31:56 +0800

    v1.0

--- Palindrome Permutation II.cpp
+++ Palindrome Permutation II.cpp
@@ -13,12 +13,13 @@ public:
 	///@brief	给定一个字符串，通过重新排列，组成回文字符串
 	///@param	s	字符串
 	///@return	返回所有可能的回文字符串 
-	///@note	1. 假设s
+	///@note	1. 类似与排列组合遍历算法，候选数组来自于原字符串的一半；
+	//			2. 先判断原字符串是否可以组成回文字符串，如果不可以直接返回空；否则逐个递归遍历；
 	vector<string> generatePalindromes(string s) {
 
 	}
 
-	///@brief
+	///@brief	
 	vector<string> rslt;
 	
 };
