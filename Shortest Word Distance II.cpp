c9bde9219f0fbac42a93547603bb66a1651ea006
Zhao Wei <zhaowei.pku@gmail.com>
2016-02-12 20:30:20 +0800

    v1.1 - iterative
    1. 哈希表记录下每个单词在单词数组中的下标，其中单词为键，下标数组为值；
    2. 由于两个单词之间的距离不可能大于已知的最小值，所以可以在原来的算法基础上进行改进，将时间复杂度降低到O(k)，其中k为单词出现的次数。

--- Shortest Word Distance II.cpp
+++ Shortest Word Distance II.cpp
@@ -3,23 +3,24 @@
 ///@date	2016.01.08
 ///@version	1.0
 
-///@date	2016.02.11
+///@date	2016.02.12
 ///@version	1.1
 
 #include <iostream>
 #include <unordered_map>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
-class WordDistance {
+class WordDistance_v1 {
 private:
 	unordered_map<string, vector<int>> hash_tbl;	//	以word为键值，以word出现的下标数组为值
 public:
 	///@brief	构造函数，填充hash_tbl
 	///@param	words	词典
 	///@note	1. 将词典中的所有单词按照单词为键，其出现的下标数组为值保存起来。
-	WordDistance(vector<string>& words) {
+	WordDistance_v1(vector<string>& words) {
 		for (int i = 0; i != words.size(); i++) 
 			hash_tbl[words[i]].push_back(i);
 	}
@@ -40,6 +41,26 @@ public:
 		}
 		return rslt;
 	}
+};
+
+class WordDistance {
+public:
+	WordDistance(vector<string>& words) {
+		for (int i = 0; i != words.size(); i++)	hash_tbl[words[i]].push_back(i);
+	}
+
+	///@note	1. 哈希表记录下每个单词在单词数组中的下标，其中单词为键，下标数组为值；
+	//			2. 由于两个单词之间的距离不可能大于已知的最小值，所以可以在原来的算法基础上进行改进，将时间复杂度降低到O(k)，其中k为单词出现的次数。
+	int shortest(string word1, string word2) {
+		int i = 0, j = 0, rslt = INT_MAX;
+		while (i < hash_tbl[word1].size() && j < hash_tbl[word2].size()) {
+			rslt = min(rslt, abs(hash_tbl[word1][i] - hash_tbl[word2][j]));
+			hash_tbl[word1][i] < hash_tbl[word2][j] ? i++ : j++;
+		}
+		return rslt;
+	}
+
+	unordered_map<string, vector<int>> hash_tbl;
 };
 
 int main() {
