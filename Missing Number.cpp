b120f17ff9eeb5a4296742c09db2170ac07978db
Zhao Wei <zhaowei.pku@gmail.com>
2016-02-13 16:43:42 +0800

    v1.1 - iterative
    1. 遍历；
    2. 先计算出0 ~ n的数列和，然后减去nums中的每个元素，剩下的元素就是缺失元素；
    3. 时间复杂度为O(n)，空间复杂度为O(1)。

--- Missing Number.cpp
+++ Missing Number.cpp
@@ -2,11 +2,12 @@
 ///@author	zhaowei
 ///@date	2016.02.13
 ///@version	1.0
+///@version	1.1
 
 #include <vector>
 using namespace std;
 
-class Solution {
+class Solution_v1 {
 public:
 	///@brief	给定从0至n的无序数组，从中抽掉一个数，找到缺失的那个数。要求时间复杂度为O(n)，空间复杂度为最好为O(1)
 	///@param	nums	数组
@@ -22,10 +23,26 @@ public:
 	}
 };
 
+class Solution {
+public:
+	///@brief	给定从0至n的无序数组，从中抽掉一个数，找到缺失的那个数。要求时间复杂度为O(n)，空间复杂度为最好为O(1)
+	///@param	nums	数组
+	///@return	返回缺失的那个数
+	///@note	1. 遍历；
+	//			2. 先计算出0 ~ n的数列和，然后减去nums中的每个元素，剩下的元素就是缺失元素；
+	//			3. 时间复杂度为O(n)，空间复杂度为O(1)。
+	int missingNumber(vector<int>& nums) {
+		int len = nums.size();
+		int sum = len * (1 + len) / 2;
+		for (int i = 0; i != nums.size(); i++)	sum -= nums[i];
+		return sum;
+	}
+};
+
 int main() {
 	vector<int> nums;
 	for (int i = 0; i != 10; i++) {
-		if (i == 9)	continue;
+		if (i == 6)	continue;
 		nums.push_back(i);
 	}
 	Solution slt;
