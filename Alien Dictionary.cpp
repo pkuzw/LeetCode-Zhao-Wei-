fb323eeca3d0960cccb67250f06788d629fea9fc
Zhao Wei <zhaowei.pku@gmail.com>
2016-02-14 17:04:36 +0800

    v1.0
    写文件头部注释。

---
+++ Alien Dictionary.cpp
@@ -0,0 +1,24 @@
+﻿///@file	Alien Dictionary
+///@author	zhaowei
+///@date	2016.02.14
+///@version	1.0
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	///@brief	通过已知的字典序好序的单词表，给出对应的字典序
+	///@param	words	单词表
+	///@return	返回对应字典序
+	///@note	1. 
+	string alienOrder(vector<string>& words) {
+
+	}
+};
+
+int main() {
+	return 0;
+}
