658e079d15040532ff6785ae49956e05283d0b20
Zhao Wei <zhaowei.pku@gmail.com>
2016-03-01 23:20:37 +0800

    v1.1 - recursive & hash table
    1. 在处理中间字符时，不能使用char型变量，因为无法表示空字符；暂时用string类型来表达；
    2. 与v1.0不同，这里使用一维整型数组来保存字符的出现次数，更加直观简洁；
    3. 在递归前要将哈希表中的字符次数减半，这样能形成半个字符串；否则后半个字符串会出现前面已经用完的字符。

--- Palindrome Permutation II.cpp
+++ Palindrome Permutation II.cpp
@@ -3,13 +3,16 @@
 ///@date	2016.02.02
 ///@version	1.0
 
+///@date	2016.03.01
+///@version	1.1
+
 #include <vector>
 #include <string>
 #include <unordered_map>
 
 using namespace std;
 
-class Solution {
+class Solution_v1 {
 public:
 	///@brief	给定一个字符串，通过重新排列，组成回文字符串
 	///@param	s	字符串
@@ -76,8 +79,65 @@ public:
 	vector<string> rslt;
 };
 
+class Solution {
+public:
+	///@note	1. 在处理中间字符时，不能使用char型变量，因为无法表示空字符；暂时用string类型来表达；
+	//			2. 与v1.0不同，这里使用一维整型数组来保存字符的出现次数，更加直观简洁；
+	//			3. 在递归前要将哈希表中的字符次数减半，这样能形成半个字符串；否则后半个字符串会出现前面已经用完的字符。
+	vector<string> generatePalindromes(string s) {
+		if (s.empty() || !isPalindrome(s))	return rslt;
+		string str;
+		int len = s.size() / 2;
+		string mid = "";
+		for (int i = 0; i != 256; i++) {	//	将哈希表中的字符出现次数减半，并找出中间字符
+			if (hash_tbl[i] % 2) {
+				mid = (char)i;
+				hash_tbl[i]--;				
+			}
+			hash_tbl[i] /= 2;
+		}		
+		dfs(str, len, hash_tbl);
+		for (int i = 0; i != rslt.size(); i++) {
+			string tmp(rslt[i].rbegin(), rslt[i].rend());
+			rslt[i] += mid + tmp;
+		}
+		return rslt;
+	}
+
+	bool isPalindrome(string s) {
+		if (s.empty())	return true;
+		memset(hash_tbl, 0, sizeof(int)*256);
+		for (int i = 0; i != s.size(); i++)	hash_tbl[s[i]]++;	//	初始化hash_tbl
+		int odd_cnt = 0;
+		for (int i = 0; i != 256; i++)
+			if (hash_tbl[i] % 2)	odd_cnt++;
+		return odd_cnt <= 1;
+	}
+
+	void dfs(string& str, int len, int* hash_tbl) {
+		if (str.size() == len) {
+			rslt.push_back(str);
+			return;
+		}
+		for (int i = 0; i != 256; i++) {
+			if (!hash_tbl[i])	continue;
+			str += (char)i;
+			hash_tbl[i]--;
+			dfs(str, len, hash_tbl);
+			hash_tbl[i]++;
+			str.pop_back();
+		}
+	}
+
+	int hash_tbl[256];
+	vector<string> rslt;
+};
+
 int main() {
 	string s = "aacccbb";
+	Solution_v1 s1;
+	vector<string> r1 = s1.generatePalindromes(s);
+
 	Solution slt;
 	vector<string> rslt = slt.generatePalindromes(s);
 	return 0;
