406473c2e5271d560eedf87cd472446f117e557b
Zhao Wei <zhaowei.pku@gmail.com>
2016-03-01 08:25:32 +0800

    v1.1 - graph
    1. 无环；
    2. 连通。

--- Graph Valid Tree.cpp
+++ Graph Valid Tree.cpp
@@ -3,16 +3,19 @@
 ///@date	2016.01.30
 ///@version	1.0
 
+///@date	2016.03.01
+///@version	1.1
+
 #include <vector>
 using namespace std;
 
-class Solution {
+class Solution_v1 {
 public:
 	///@brief	判断给定的一组边能否构成一棵树
 	///@param	n		节点数
 	///@param	edges	边集合
 	///@note	1. 判断一组无向边能否构成树有两点：首先这个无向图没有环；其次该无向图是连通图。
-	//			2. 通过一个数组来保存所有节点的直接父节点，每当插入一个新的边时，同时检查边的两节点的最深父节点是否相同，如果相同则说明存在环；
+	//			2. 通过一个数组来保存所有节点的最深父节点，每当插入一个新的边时，同时检查边的两节点的最深父节点是否相同，如果相同则说明存在环；
 	//			3. 如果无向图是连通图，那么边数 = 节点数 - 1。
 	bool validTree(int n, vector<pair<int, int>>& edges) {		
 		vector<int> parents(n, 0);
@@ -29,13 +32,33 @@ public:
 	}
 };
 
+class Solution {
+public:
+	bool validTree(int n, vector<pair<int, int>>& edges) {
+		vector<int> parents(n, 0);
+		for (int i = 0; i != n; i++)	parents[i] = i;
+		for (int i = 0; i != edges.size(); i++) {
+			int f = edges[i].first;
+			int s = edges[i].second;
+			while (f != parents[f])	f = parents[f];
+			while (s != parents[s])	s = parents[s];
+			if (parents[f] == parents[s])	return false;
+			parents[s] = f;
+		}
+		return n == edges.size() + 1;
+	}
+};
+
 int main() {
-	Solution slt;
+	Solution_v1 s1;
 	vector<pair<int, int>> edges;
 	edges.push_back(make_pair(0, 1));
-	edges.push_back(make_pair(0, 2));
-	edges.push_back(make_pair(0, 3));
 	edges.push_back(make_pair(1, 2));
+	edges.push_back(make_pair(2, 3));
+	edges.push_back(make_pair(1, 3));
+	edges.push_back(make_pair(1, 4));
+	bool r1 = s1.validTree(5, edges);
+	Solution slt;
 	bool rslt = slt.validTree(5, edges);
 	return 0;
 }
