aa3b7bbf6a78070fd8c7a0af1fd2d2bc83ef909c
Zhao Wei <zhaowei.pku@gmail.com>
2016-03-01 08:31:42 +0800

    v1.1 - recursive

--- Ugly Number.cpp
+++ Ugly Number.cpp
@@ -3,10 +3,13 @@
 ///@date	2016.01.30
 ///@version	1.0
 
+///@date	2016.03.01
+///@version	1.1
+
 #include <vector>
 using namespace std;
 
-class Solution {
+class Solution_v1 {
 public:
 	///@brief	判断一个正整数是否为丑数。丑数的定义为一个正整数的因子只能为2，3，5。
 	///@param	num	正整数
@@ -23,8 +26,16 @@ public:
 	}
 };
 
+class Solution {
+public:
+	bool isUgly(int num) {
+		if (num <= 0)	return false;
+		return num == 1 || (!(num % 2) && isUgly(num / 2)) || (!(num % 3) && isUgly(num / 3)) || (!(num % 5) && isUgly(num / 5));
+	}
+};
+
 int main() {
 	Solution slt;
-	bool rslt = slt.isUgly(8);
+	bool rslt = slt.isUgly(0);
 	return 0;
 }
