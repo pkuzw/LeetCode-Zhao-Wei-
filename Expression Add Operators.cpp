20636f430dd41aa4e3d1681d411f6a7718264c8e
Zhao Wei <zhaowei.pku@gmail.com>
2016-03-05 23:10:34 +0800

    v1.1 - dfs

--- Expression Add Operators.cpp
+++ Expression Add Operators.cpp
@@ -2,46 +2,47 @@
 ///@author	zhaowei
 ///@date	2016.03.03
 ///@version	1.0
+
+///@date	2016.03.05
+///@version	1.1
 #include <string>
 #include <vector>
 
 using namespace std;
 
 class Solution {
-private:
+public:
 	// cur: {string} expression generated so far.
 	// pos: {int}    current visiting position of num.
 	// cv:  {long}   cumulative value so far.
 	// pv:  {long}   previous operand value.
 	// op:  {char}   previous operator used.
-	void dfs(std::vector<string>& res, const string& num, const int target, string cur, int pos, const long cv, const long pv, const char op) {
+	void dfs(std::vector<string>& rslt, const string& num, const int target, string cur, int pos, const long cv, const long pv, const char op) {
 		if (pos == num.size() && cv == target) {
-			res.push_back(cur);
-		} else {
-			for (int i=pos+1; i<=num.size(); i++) {
-				string t = num.substr(pos, i-pos);
-				long now = stol(t);
-				if (to_string(static_cast<long long>(now)).size() != t.size()) continue;
-				dfs(res, num, target, cur+'+'+t, i, cv+now, now, '+');
-				dfs(res, num, target, cur+'-'+t, i, cv-now, now, '-');
-				dfs(res, num, target, cur+'*'+t, i, (op == '-') ? cv+pv - pv*now : ((op == '+') ? cv-pv + pv*now : pv*now), pv*now, op);
-			}
+			rslt.push_back(cur);
+			return;
+		}
+		for (int i = pos + 1; i <= num.size(); i++) {
+			string t = num.substr(pos, i - pos);
+			long now = stol(t);
+			if (to_string(static_cast<long long>(now)).size() != t.size())	continue;
+			dfs(rslt, num, target, cur + "+" + t, i, cv + now, now, '+');
+			dfs(rslt, num, target, cur + "-" + t, i, cv - now, now, '-');
+			dfs(rslt, num, target, cur + "*" + t, i, op == '+' ? cv - pv + pv * now : op == '-' ? cv + pv - pv * now : pv * now, pv * now, op);
 		}
 	}
 
-public:
-	vector<string> addOperators(string num, int target) {
-		vector<string> res;
-		if (num.empty()) return res;
-		for (int i=1; i<=num.size(); i++) {
+ 	vector<string> addOperators(string num, int target) {
+		vector<string> rslt;
+		if (num.empty())	return rslt;
+		for (int i = 1; i <= num.size(); i++) {
 			string s = num.substr(0, i);
 			long cur = stol(s);
-			if (to_string(static_cast<long long>(cur)).size() != s.size()) continue;
-			dfs(res, num, target, s, i, cur, cur, '#');         // no operator defined.
+			if (to_string(static_cast<long long>(cur)).size() != s.size())	continue;
+			dfs(rslt, num, target, s, i, cur, cur, '#');
 		}
-
-		return res;
-	}
+		return rslt;
+ 	}
 };
 
 int main() {
