f84e64714b21ef94f341e5d3eedb44a915b636c2
Zhao Wei <zhaowei.pku@gmail.com>
2016-03-07 14:29:07 +0800

    v1.0 - iterative
    1. 在二叉树中，一个节点的中序遍历的后继节点是其右子树中的最左子节点；
    2. 如果该节点存在右子树，那么就是右子树中的最左子节点；
    3. 如果不存在右子树，则寻找最低的祖先节点，且该节点是包含有当前节点为左子树的节点；
    4. 如果还不存在则返回null；
    5. 参考discuss中的算法，简洁明了，厉害啊！

--- Inorder Successor in BST.cpp
+++ Inorder Successor in BST.cpp
@@ -19,13 +19,18 @@ public:
 	///@param	p		指定节点
 	///@return	返回指定节点的后继节点
 	///@note	1. 在二叉树中，一个节点的中序遍历的后继节点是其右子树中的最左子节点；
-	//			2. 如果该节点存在右子树，那么就是右子树中的最左子节点；如果不存在右子树，则看该节点是否为其父节点的左孩子，如果是，则返回父节点；如果不是则返回null。
+	//			2. 如果该节点存在右子树，那么就是右子树中的最左子节点；
+	//			3. 如果不存在右子树，则寻找最低的祖先节点，且该节点是包含有当前节点为左子树的节点；
+	//			4. 如果还不存在则返回null；
+	//			5. 参考discuss中的算法，简洁明了，厉害啊！
 	TreeNode* inorderSuccessor(TreeNode* root, TreeNode* p) {
-		if (!p)	return nullptr;
-
+		TreeNode* parent;
+		while (root) root = (root->val > p->val) ? (parent = root)->left : root->right;
+		return parent;
 	}
-
-
-
-
 };
+
+int main() {
+
+	return 0;
+}
