026c137dd01b6c07d03224d75ac76da84113b3ca
Zhao Wei <zhaowei.pku@gmail.com>
2016-03-09 13:28:42 +0800

    v1.0 - dfs & backtracking
    1. 递归 + 回溯；
    2. 用两个哈希表分别表示模式到字符串和字符串到模式的映射；
    3. 然后遍历所有可能的模式到字符串的匹配，当二者都到最末尾时返回true。

--- Word Pattern II.cpp
+++ Word Pattern II.cpp
@@ -1,6 +1,7 @@
 ﻿///@file	Word Pattern II
 ///@author	zhaowei
 ///@date	2016.03.09
+///@version	0.1
 ///@version	1.0
 
 #include <vector>
@@ -9,7 +10,7 @@
 
 using namespace std;
 
-class Solution {
+class Solution_v0_1 {
 public:
 	///@brief	判断给定字符串是否符合给定的模式
 	///@param	pattern	模式字符串
@@ -40,9 +41,57 @@ public:
 	unordered_map<string, char> hash_tbl_;
 };
 
+class Solution {
+public:
+	///@note	1. 递归 + 回溯；
+	//			2. 用两个哈希表分别表示模式到字符串和字符串到模式的映射；
+	//			3. 然后遍历所有可能的模式到字符串的匹配，当二者都到最末尾时返回true。
+	bool wordPatternMatch(string pattern, string str) {
+		return dfs(pattern, 0, str, 0);
+	}
+
+	///@brief	递归辅助函数
+	///@param	pattern	模式
+	///@param	i		模式串的下标
+	///@param	str		待匹配字符串
+	///@param	j		待匹配字符串下标
+	///@return	如果匹配则返回true，否则返回false
+	bool dfs(string pattern, int i, string str, int j) {
+		int p_len = pattern.length(), s_len = str.length();
+		if (i == p_len || j == s_len) {
+			if (i == p_len && j == s_len)	return true;
+			return false;
+		}
+		bool ins = false;	//	是否有新的映射插入哈希表
+		for (int k = j; k < s_len; k++) {
+			string s = str.substr(j, k-j+1);
+			if (pDict.find(pattern[i]) != pDict.end()) {
+				if (pDict[pattern[i]] != s)	continue;
+			}
+			else if (sDict.find(s) != sDict.end()) {
+				if (sDict[s] != pattern[i])	continue;
+			}
+			else {
+				pDict[pattern[i]] = s;
+				sDict[s] = pattern[i];
+				ins = true;
+			}
+			if (dfs(pattern, i+1, str, k+1))	return true;
+			if (ins) {
+				pDict.erase(pattern[i]);
+				sDict.erase(s);
+			}
+		}
+		return false;
+	}
+
+	unordered_map<char, string> pDict;
+	unordered_map<string, char> sDict;
+};
+
 int main() {
 	Solution slt;
-	string pattern = "abab", str = "redblueredblue";
+	string pattern = "aabb", str = "xyzabcxzyabc";
 	bool rslt = slt.wordPatternMatch(pattern, str);
 	return 0;
 }
