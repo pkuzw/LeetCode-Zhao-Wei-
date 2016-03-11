3e109467e44b9e7469c6968212fd897987a1b6ee
Zhao Wei <zhaowei.pku@gmail.com>
2016-03-11 16:09:23 +0800

    v1.0
    1. 因为STL的set是红黑树的变种，利用红黑树插入和查询都是O(logn)的时间复杂度的优势来解题；
    2. 插入时就调用insert插入，寻找中间值就将iterator累加，直至一半；
    3. TLE。

--- Find Median from Data Stream.cpp
+++ Find Median from Data Stream.cpp
@@ -1,51 +1,47 @@
 ﻿///@file	Find Median from Data Stream
 ///@author	zhaowei
 ///@date	2016.03.11
-///@version	1.0
+///@version	0.3
 
-#include <deque>
+#include <set>
 using namespace std;
 
-struct ListNode {
-	int val;
-	ListNode* next;
-};
-
-///@note	
+///@note	1. 因为STL的set是红黑树的变种，利用红黑树插入和查询都是O(logn)的时间复杂度的优势来解题；
+//			2. 插入时就调用insert插入，寻找中间值就将iterator累加，直至一半;
+//			3. TLE.
 class MedianFinder {
 public:
-
-	MedianFinder() {
-		head = new ListNode;
-	}
-
 	// Adds a number into the data structure.
 	void addNum(int num) {
-		if (!head) {
-			head->val = num;
-			head->next = nullptr;
-		}
-		ListNode* indx = head;
-		ListNode* pre_indx = new ListNode;
-		pre_indx->next = head;
-		pre_indx->val = -1;
-		while (indx->val < num && indx->next) {
-			pre_indx = indx;
-			indx = indx->next;
-		}
-		ListNode* node = new ListNode;
-		node->val = num;
-		pre_indx->next = node;
-		node->next = indx;
+		nums.insert(num);
 	}
 
 	// Returns the median of current data stream
 	double findMedian() {
-
+		int n = nums.size();
+		double rslt = 0.0;
+		if (n % 2) {
+			set<int>::iterator i = nums.begin();
+			int j = 0;
+			while (j < n / 2) {
+				i++;
+				j++;
+			}
+			rslt = double(*i);
+		}
+		else {
+			set<int>::iterator i = nums.begin();
+			int j = 0;
+			while (j < n / 2) {
+				i++;
+				j++;
+			}
+			rslt = (double(*i) + double(*(--i))) / 2;
+		}
+		return rslt;
 	}
 
-	ListNode* head;
-
+	set<int> nums;
 };
 
 // Your MedianFinder object will be instantiated and called as such:
@@ -54,5 +50,23 @@ public:
 // mf.findMedian();
 
 int main() {
+	MedianFinder mf;
+	double r = 0;
+	mf.addNum(1);
+	r = mf.findMedian();
+
+	mf.addNum(2);
+	r = mf.findMedian();
+
+	mf.addNum(3);
+	r = mf.findMedian();
+
+	mf.addNum(10);
+	r = mf.findMedian();
+
+	mf.addNum(5);
+	mf.addNum(11);
+	r = mf.findMedian();
+
 	return 0;
 }
