7ff8cb0ed3a0c9bfd8c24f8c337c862f2055599c
Zhao Wei <zhaowei.pku@gmail.com>
2016-03-14 12:45:09 +0800

    v1.0 - iterative
    1. 写文件头。

---
+++ Longest Increasing Subsequence.cpp
@@ -0,0 +1,22 @@
+﻿///@file	Longest Increasing Subsequence
+///@author	zhaowei
+///@date	2016.03.14
+///@version	1.0
+
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	///@brief	寻找最长的递增子序列
+	///@param	nums	数组
+	///@return	返回最长递增子序列的长度
+	///@note	1. 
+	int lengthOfLIS(vector<int>& nums) {
+
+	}
+};
+
+int main() {
+	return 0;
+}
