7f4f017e09967f35622eab0114d5ab4438e5a85b
Zhao Wei <zhaowei.pku@gmail.com>
2016-03-16 13:55:29 +0800

    v2.0 - partial_sum()
    1. 利用stl中的partial_sum函数，其前两个参数表示数组的起止迭代器，第三个参数表示结果数组的位置下标，结果数组i表示从0到i的和

--- Range Sum Query - Immutable.cpp
+++ Range Sum Query - Immutable.cpp
@@ -3,14 +3,18 @@
 ///@date	2016.03.15
 ///@version	1.0
 
+///@date	2016.03.16
+///@version	2.0
+
 #include <vector>
+#include <numeric>
 
 using namespace std;
 
 ///@note	1. 最naive的方法是tle。
-class NumArray {
+class NumArray_v1 {
 public:
-	NumArray(vector<int> &nums) {
+	NumArray_v1(vector<int> &nums) {
 		for (int i = 0; i != nums.size(); i++)	n.push_back(nums[i]);
 	}
 
@@ -23,6 +27,21 @@ public:
 	vector<int> n;
 };
 
+///@note	1. 利用stl中的partial_sum函数，其前两个参数表示数组的起止迭代器，第三个参数表示结果数组的位置下标，结果数组i表示从0到i的和
+class NumArray {
+public:
+	NumArray(vector<int> &nums) {
+		for (int i = 0; i <= nums.size(); i++)	psum.push_back(0);
+		partial_sum(nums.begin(), nums.end(), psum.begin()+1);
+	}
+
+	int sumRange(int i, int j) {
+		return psum[j+1] - psum[i];
+	}	
+
+	vector<int> psum;
+};
+
 
 // Your NumArray object will be instantiated and called as such:
 // NumArray numArray(nums);
@@ -30,5 +49,11 @@ public:
 // numArray.sumRange(1, 2);
 
 int main() {
+	vector<int> nums;
+	int n[6] = {-2, 0, 3, -5, 2, -1};
+	for (int i = 0; i < 6; i++)	nums.push_back(n[i]);
+	NumArray numArray(nums);
+	int r = numArray.sumRange(0, 1);
+	r = numArray.sumRange(1, 2);
 	return 0;
 }
