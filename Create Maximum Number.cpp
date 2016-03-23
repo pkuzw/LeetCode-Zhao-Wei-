4e6eee484555a00ab908c7dc3827160998043b5d
Zhao Wei <zhaowei.pku@gmail.com>
2016-03-23 22:16:03 +0800

    v1.0

--- Create Maximum Number.cpp
+++ Create Maximum Number.cpp
@@ -8,6 +8,11 @@ using namespace std;
 
 class Solution {
 public:
+	///@brief	给定两个由'0' - '9'构成的数组，从其中两个数组中选择k位数出来使得数字尽可能的大。要求从同一个数组中选出的数要保持相对顺序不变
+	///@param	nums1	数组1
+	///@param	nums2	数组2
+	///@param	k		选出k位数
+	///@return	返回选出来的k个数字组成的数组
 	vector<int> maxNumber(vector<int>& nums1, vector<int>& nums2, int k) {
 
 	}
