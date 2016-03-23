decfc7d5a284e24486a0f70570bb57b711958fd4
Zhao Wei <zhaowei.pku@gmail.com>
2016-03-23 11:17:24 +0800

    v1.0

---
+++ Create Maximum Number.cpp
@@ -0,0 +1,18 @@
+﻿///@file	Create Maximum Number
+///@author	zhaowei
+///@date	2016.03.23
+///@version	1.0
+
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	vector<int> maxNumber(vector<int>& nums1, vector<int>& nums2, int k) {
+
+	}
+};
+
+int main() {
+	return 0;
+}
