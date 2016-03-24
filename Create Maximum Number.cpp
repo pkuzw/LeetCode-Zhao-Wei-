67e4b0f893188ee1732423f4a215f7620a28aab4
Zhao Wei <zhaowei.pku@gmail.com>
2016-03-24 11:55:38 +0800

    v1 - reload

--- Create Maximum Number.cpp
+++ Create Maximum Number.cpp
@@ -1,6 +1,6 @@
 ﻿///@file	Create Maximum Number
 ///@author	zhaowei
-///@date	2016.03.23
+///@date	2016.03.24
 ///@version	1.0
 
 #include <vector>
@@ -14,7 +14,44 @@ public:
 	///@param	k		选出k位数
 	///@return	返回选出来的k个数字组成的数组
 	vector<int> maxNumber(vector<int>& nums1, vector<int>& nums2, int k) {
+		int n1 = nums1.size(), n2 = nums2.size();
+		vector<int> best;
+		for (int k1 = max(k - n2, 0); k1 <= min(k, n1); k1++) {
+			best = max(best, maxNumber(maxNumber(nums1, k1), maxNumber(nums2, k - k1)));
+		}
+		return best;
+	}
 
+	///@brief	计算从一个'0' - '9'构成的数组组成k位数的最大值
+	///@param	nums	数组
+	///@param	k		位数长度
+	///@return	返回选出来的k个数字
+	vector<int> maxNumber(vector<int> nums, int k) {
+		int drop = nums.size() - k;
+		vector<int> rslt;
+		for (int i = 0; i != nums.size(); i++) {
+			while (drop && rslt.size() && rslt.back() < nums[i]) {
+				rslt.pop_back();
+				drop--;
+			}
+			rslt.push_back(nums[i]);
+		}
+		rslt.resize(k);
+		return rslt;
+	}
+
+	///@brief	计算从两个'0' - '9'构成的数组构成的最大数字，里面的元素要全部利用起来
+	///@param	nums1	数组1
+	///@param	nums2	数组2
+	///@return	返回结果数组
+	vector<int> maxNumber(vector<int> nums1, vector<int> nums2) {
+		vector<int> rslt;
+		while (nums1.size() + nums2.size()) {
+			vector<int>& now = nums1 > nums2 ? nums1 : nums2;
+			rslt.push_back(now[0]);
+			now.erase(now.begin());
+		}
+		return rslt;
 	}
 };
 
