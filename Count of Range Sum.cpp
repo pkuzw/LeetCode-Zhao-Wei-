b7ea03191189c7816cf180c3e26f4ccc41b13bfb
Zhao Wei <zhaowei.pku@gmail.com>
2016-03-26 20:58:32 +0800

    v1.0

---
+++ Count of Range Sum.cpp
@@ -0,0 +1,24 @@
+﻿///@file	Count of Range Sum
+///@author	zhaowei
+///@date	2016.03.26
+///@version	1.0
+
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	///@brief	给定一个数组，计算在和为指定区间范围内的子数组的个数
+	///@param	nums	数组
+	///@param	lower	指定区间的下界
+	///@param	upper	指定区间的上界
+	///@return	返回子数组的数目
+	///@note	1. 线段树
+	int countRangeSum(vector<int>& nums, int lower, int upper) {
+
+	}
+};
+
+int main() {
+	return 0;
+}
