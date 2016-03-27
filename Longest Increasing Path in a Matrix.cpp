905bebaa1eb6a7a55c96b738eb285cd7f35e7c2a
Zhao Wei <zhaowei.pku@gmail.com>
2016-03-27 16:07:09 +0800

    v2.0 - dfs & hash table
    1. 利用哈希表存储已经搜索过的合法路径的长度值；这样相当于有所剪枝；
    2. 哈希表在作为形参时一定要声明为引用，否则超时；
    3. 时间复杂度为O(n^3)。

--- Longest Increasing Path in a Matrix.cpp
+++ Longest Increasing Path in a Matrix.cpp
@@ -2,12 +2,13 @@
 ///@author	zhaowei
 ///@date	2016.03.27
 ///@version	1.0
+///@version	2.0
 
 #include <vector>
 
 using namespace std;
 
-class Solution {
+class Solution_v1 {
 public:
 	///@brief	给定一个二维矩阵，找到最长的递增路径
 	///@param	matrix	二维矩阵
@@ -70,6 +71,43 @@ public:
 			visited[i][j] = false;
 			path_len--;
 		}		
+	}
+};
+
+class Solution {
+public:
+	///@note	1. 利用哈希表存储已经搜索过的合法路径的长度值；这样相当于有所剪枝；
+	//			2. 哈希表在作为形参时一定要声明为引用，否则超时；
+	//			3. 时间复杂度为O(n^3)。
+	int longestIncreasingPath(vector<vector<int>>& matrix) {
+		if (matrix.empty() || matrix[0].empty())	return 0;
+		int m = matrix.size(), n = matrix[0].size();
+		vector<vector<int>> hash_tbl(m, vector<int>(n, 0));
+		int rslt = 0;
+		for (int i = 0; i != m; i++)
+			for (int j = 0; j != n; j++)
+				rslt = max(rslt, dfs(matrix, hash_tbl, i, j, INT_MIN));
+		return rslt;
+	}
+
+	///@brief	递归辅助函数
+	///@param	matrix	二维矩阵
+	///@param	hash_tbl	保存从点<i, j>出发的所有合法路径的最大长度
+	///@param	i, j	当前点的坐标
+	///@param	val		前一个点的值
+	///@return	返回从<i, j>出发的合法路径的最大长度
+	int dfs(vector<vector<int>>& matrix, vector<vector<int>>& hash_tbl, int i, int j, int val) {
+		if (i < 0 || i >= matrix.size() || j < 0 || j >= matrix[0].size())	return 0;
+		if (matrix[i][j] > val) {	//	从<i, j>出发如果存在路径
+			if (hash_tbl[i][j])	return hash_tbl[i][j];
+			int a = dfs(matrix, hash_tbl, i + 1, j, matrix[i][j]) + 1;
+			int b = dfs(matrix, hash_tbl, i - 1, j, matrix[i][j]) + 1;
+			int c = dfs(matrix, hash_tbl, i, j + 1, matrix[i][j]) + 1;
+			int d = dfs(matrix, hash_tbl, i, j - 1, matrix[i][j]) + 1;
+			hash_tbl[i][j] = max(max(max(a, b), c), d);
+			return hash_tbl[i][j];
+		}
+		return 0;
 	}
 };
 
