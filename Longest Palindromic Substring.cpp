19dc6ce68ae3d74e308d33b47cfe57bf4156b121
Zhao Wei <zhaowei.pku@gmail.com>
2016-04-01 09:55:23 +0800

    v2.1 - manacher algorithm
    1. Manacher Algorithm. time: O(n)
    2.
    http://www.geeksforgeeks.org/manachers-algorithm-linear-time-longest-palindromic-substring-part-1/
    3.
    http://www.geeksforgeeks.org/manachers-algorithm-linear-time-longest-palindromic-substring-part-2/

--- Longest Palindromic Substring.cpp
+++ Longest Palindromic Substring.cpp
@@ -286,19 +286,107 @@ public:
 
 class Solution {
 public:
+	///@note	Manacher Algorithm. time: O(n)
+	//			http://www.geeksforgeeks.org/manachers-algorithm-linear-time-longest-palindromic-substring-part-1/
+	//			http://www.geeksforgeeks.org/manachers-algorithm-linear-time-longest-palindromic-substring-part-2/
 	string longestPalindrome(string s) {
+		int N = s.length();
+		if (N == 0)	return "";
+		if (N == 1)	return s;
+		N = 2*N + 1; //Position count
+		int* L = new int[N]; //LPS Length Array
+		L[0] = 0;
+		L[1] = 1;
+		int C = 1; //centerPosition 
+		int R = 2; //centerRightPosition
+		int i = 0; //currentRightPosition
+		int iMirror; //currentLeftPosition
+		int expand = -1;
+		int diff = -1;
+		int maxLPSLength = 0;
+		int maxLPSCenterPosition = 0;
+		int start = -1;
+		int end = -1;
 
+		for (i = 2; i < N; i++) 
+		{
+			//get currentLeftPosition iMirror for currentRightPosition i
+			iMirror  = 2*C-i;
+			//Reset expand - means no expansion required
+			expand = 0;
+			diff = R - i;
+			//If currentRightPosition i is within centerRightPosition R
+			if(diff > 0) 
+			{
+				if(L[iMirror] < diff) // Case 1
+					L[i] = L[iMirror];
+				else if(L[iMirror] == diff && i == N-1) // Case 2
+					L[i] = L[iMirror];
+				else if(L[iMirror] == diff && i < N-1)  // Case 3
+				{
+					L[i] = L[iMirror];
+					expand = 1;  // expansion required
+				}
+				else if(L[iMirror] > diff)  // Case 4
+				{
+					L[i] = diff;
+					expand = 1;  // expansion required
+				}
+			}
+			else
+			{
+				L[i] = 0;
+				expand = 1;  // expansion required
+			}
+
+			if (expand == 1)
+			{
+				//Attempt to expand palindrome centered at currentRightPosition i
+				//Here for odd positions, we compare characters and 
+				//if match then increment LPS Length by ONE
+				//If even position, we just increment LPS by ONE without 
+				//any character comparison
+				while (((i + L[i]) < N && (i - L[i]) > 0) && 
+					( ((i + L[i] + 1) % 2 == 0) || 
+					( s[(i + L[i] + 1)/2] == s[(i-L[i]-1)/2] )))
+				{
+					L[i]++;
+				}
+			}
+
+			if(L[i] > maxLPSLength)  // Track maxLPSLength
+			{
+				maxLPSLength = L[i];
+				maxLPSCenterPosition = i;
+			}
+
+			// If palindrome centered at currentRightPosition i 
+			// expand beyond centerRightPosition R,
+			// adjust centerPosition C based on expanded palindrome.
+			if (i + L[i] > R) 
+			{
+				C = i;
+				R = i + L[i];
+			}
+		}
+		start = (maxLPSCenterPosition - maxLPSLength)/2;
+		end = start + maxLPSLength - 1;
+		delete []L;
+		return s.substr(start, end - start + 1);
 	}
 };
 
 int main()
 {
 	Solution_v1 slt;
-	string s = "bb";
+	string s = "a";
 	string pstr_bf = slt.longestPalindrome_BruteForce(s);
 	string pstr_dp = slt.longestPalindrome_DP(s);
 	string pstr_extend = slt.longestPalindrome_extendAroundCenter(s);
 	string pstr_manacher = slt.longestPalindrome_Manacher(s);
+
+	Solution slt_;
+	string rslt = slt_.longestPalindrome(s);
 	cout << pstr_bf << endl;
 	cout << pstr_dp;
 	cout << endl;
