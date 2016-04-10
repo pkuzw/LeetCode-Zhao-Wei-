441bac1429b8f35e160066ad2f1b8b9c5bc94bd3
Zhao Wei <zhaowei.pku@gmail.com>
2016-04-10 23:19:15 +0800

    v2.2 - tricky

--- First Missing Positive.cpp
+++ First Missing Positive.cpp
@@ -18,6 +18,9 @@
 ///@date	2015.09.24
 ///@version	2.1
 
+///@date	2016.04.10
+///@version	2.2
+
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -70,7 +73,8 @@ public:
 	///@brief	给定一个数组，找到第一个不连续出现的正整数
 	///@param	nums	数组
 	///@return	返回第一个为连续出现的正整数
-	///@note	因为题目要求O(n)的时间复杂度和O(1)的空间复杂度。所以不能使用哈希表。我们将现有的输入数组当做临时的存储空间。其中A[i]存放i + 1，i >= 0。因为长度为n的数组最多出现n个正整数。所以空间上一定够用。
+	///@note	因为题目要求O(n)的时间复杂度和O(1)的空间复杂度。所以不能使用哈希表。我们将现有的输入数组当做临时的存储空间。
+	//			其中A[i]存放i + 1，i >= 0。因为长度为n的数组最多出现n个正整数。所以空间上一定够用。
 	//			如果A[i] == i + 1 或者 A[i] <= 0 或者 A[i] > n，则认为不是缺失的最小正整数，跳过；
 	//			如果A[i]没有被安置在它应该存放的临时位置A[A[i] - 1]，则将二者交换；
 	//			否则继续向后遍历。
@@ -88,6 +92,21 @@ public:
 		}
 		return i + 1;
     }
+};
+
+class Solution {
+public:
+	int firstMissingPositive(vector<int>& nums) {
+		int i = 0;
+		while (i < nums.size()) {
+			if (nums[i] == i + 1 || nums[i] <= 0 || nums[i] > nums.size())	i++;
+			else if (nums[i] != nums[nums[i] - 1])	swap(nums[i], nums[nums[i] - 1])	;
+			else i++;
+		}
+		i = 0;
+		while (i < nums.size() && nums[i] == i + 1)	i++;
+		return i + 1;
+	}
 };
 
 int main()
