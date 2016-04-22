7c30db87d765a07abce01ec179a6cf4edda14a50
Zhao Wei <zhaowei.pku@gmail.com>
2016-04-22 21:43:56 +0800

    v1.0 - enumeration

---
+++ Reverse String.cpp
@@ -0,0 +1,26 @@
+﻿///@file	Reverse String
+///@author	zhaowei
+///@date	2016.04.22
+///@version	1.0
+
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+	///@brief	将一个string进行翻转
+	///@param	s	字符串
+	///@return	返回翻转后的字符串
+	string reverseString(string s) {
+		if (s.empty())	return s;
+		for (int i = 0; i < s.length() / 2; i++)	swap(s[i], s[s.size()-1-i]);
+		return s;
+	}
+};
+
+int main() {
+	string s = "hell";
+	Solution slt;
+	string rslt = slt.reverseString(s);
+	return 0;
+}
