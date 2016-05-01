b18faee0b702cbaa438bd648c924dff16a4fdeba
Zhao Wei <zhaowei.pku@gmail.com>
2016-05-01 08:38:45 +0800

    v1.0

---
+++ Moving Average From Data Stream.cpp
@@ -0,0 +1,29 @@
+﻿///@file	Moving Average From Data Stream
+///@author	zhaowei
+///@date	2016.05.01
+///@version	1.0
+
+#include <iostream>
+using namespace std;
+
+class MovingAverage {
+public:
+	/** Initialize your data structure here. */
+	MovingAverage(int size) {
+
+	}
+
+	double next(int val) {
+
+	}
+}
+
+/**
+* Your MovingAverage object will be instantiated and called as such:
+* MovingAverage obj = new MovingAverage(size);
+* double param_1 = obj.next(val);
+*/
+
+int main() {
+	return 0;
+}
