9951f855303b2a4db6a7339ee938b2c30cc088a1
Zhao Wei <zhaowei.pku@gmail.com>
2016-05-19 04:39:11 +0800

    v1.0 - hash table

---
+++ Intersection of Two Arrays.cpp
@@ -0,0 +1,31 @@
+﻿///@file	Intersection of Two Arrays
+///@author	zhaowei
+///@date	2016.05.19
+///@version	1.0
+
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+class Solution {
+public:
+	vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+		vector<int> rslt;
+		if (nums1.empty() && nums2.empty())	return rslt;
+		unordered_set<int> hash_tbl;
+		for (int i = 0; i != nums1.size(); i++) {
+			if (hash_tbl.find(nums1[i]) == hash_tbl.end())	hash_tbl.insert(nums1[i]);
+		}
+		for (int i = 0; i != nums2.size(); i++) {
+			if (hash_tbl.find(nums2[i]) != hash_tbl.end()) {
+				rslt.push_back(nums2[i]);
+				hash_tbl.erase(nums2[i]);
+			}			
+		}
+		return rslt;
+	}
+};
+
+int main() {
+	return 0;
+}
