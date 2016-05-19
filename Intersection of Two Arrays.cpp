6d0d74c2573d56953feb3b98478bf6109bcb5f53
Zhao Wei <zhaowei.pku@gmail.com>
2016-05-19 14:35:37 +0800

    v1.0 - hash table
    1. 利用哈希表来保存数组1中出现过的元素；
    2. 然后再遍历数组2，只要在哈希表中出现过，就将其添加进入结果数组，并从哈希表中删除，以免出现重复结果；
    3. 时间复杂度为O(n)，空间复杂度为O(n)。

--- Intersection of Two Arrays.cpp
+++ Intersection of Two Arrays.cpp
@@ -9,6 +9,13 @@ using namespace std;
 
 class Solution {
 public:
+	///@brief	计算两个数组的重叠元素
+	///@param	nums1	数组1
+	///@param	nums2	数组2
+	///@return	返回数组1和2的重叠元素，其中不包含重复元素
+	///@note	1. 利用哈希表来保存数组1中出现过的元素；
+	//			2. 然后再遍历数组2，只要在哈希表中出现过，就将其添加进入结果数组，并从哈希表中删除，以免出现重复结果；
+	//			3. 时间复杂度为O(n)，空间复杂度为O(n)。
 	vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
 		vector<int> rslt;
 		if (nums1.empty() && nums2.empty())	return rslt;
@@ -27,5 +34,15 @@ public:
 };
 
 int main() {
+	vector<int> nums1;
+	nums1.push_back(1);
+	nums1.push_back(2);	
+	nums1.push_back(2);
+	nums1.push_back(1);
+	vector<int> nums2;
+	nums2.push_back(2);
+	nums2.push_back(2);
+	Solution slt;
+	vector<int> rslt = slt.intersection(nums1, nums2);
 	return 0;
 }
