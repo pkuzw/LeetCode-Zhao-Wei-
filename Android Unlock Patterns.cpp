db49f740c405948b87c28b1653054536f98e8657
Zhao Wei <pkuzw@126.com>
2016-07-22 16:42:47 +0800

    v1.0 - draft
    1. 写文件说明和文件头

---
+++ Android Unlock Patterns.cpp
@@ -0,0 +1,33 @@
+//
+//  Android Unlock Patterns.cpp
+//  LeetCode by zhaowei
+//
+//  Created by Zhao Wei on 16/7/22.
+//  Copyright © 2016年 Zhao Wei. All rights reserved.
+//
+
+///@file    Android Unlock Patterns
+///@author  zhaowei
+///@date    2016.07.22
+///@version 1.0
+
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    ///@brief   计算安卓手机锁屏的种类数目
+    ///@param   m   最少按钮数
+    ///@param   n   最多按钮数
+    ///@return  返回安卓手机9键盘的锁屏方法种类，要求一笔画；穿过的按键之前应该被访问过；数字的顺序有意义
+    int numberOfPatterns(int m, int n) {
+        return 0;
+    }
+};
+
+int main() {
+    Solution slt;
+    int rslt = slt.numberOfPatterns(1, 1);
+    cout << "hello world.\n";
+    return 0;
+}
