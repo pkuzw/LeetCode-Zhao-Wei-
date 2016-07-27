5715cf26ead13ec84c1d76ccb3483019934bcd43
Zhao Wei <pkuzw@126.com>
2016-07-27 22:26:18 +0800

    v1.0 - backtracking
    1. 回溯
    2. 递归遍历合法的解锁图案数目；

--- Android Unlock Patterns.cpp
+++ Android Unlock Patterns.cpp
@@ -21,7 +21,41 @@ public:
     ///@param   n   最多按钮数
     ///@return  返回安卓手机9键盘的锁屏方法种类，要求一笔画；穿过的按键之前应该被访问过；数字的顺序有意义
     int numberOfPatterns(int m, int n) {
-        return 0;
+        int rslt = 0;
+        for (int len = m; len <= n; len++) {
+            rslt += calPatterns(-1, len);
+            for (int i = 0; i < 9; i++) {
+                used[i] = false;
+            }
+        }
+        return rslt;
+    }
+    
+    ///@brief   判断一个模式是否合法
+    bool isValid(int index, int last) {
+        if (used[index])    return false;
+        if (last == -1) return true;
+        if ((index + last) % 2 == 1)    return true;
+        int mid = (index + last) / 2;
+        if (mid == 4)   return used[mid];
+        if ((index % 3 != last % 3) && index / 3 != last / 3)   return true;
+        return used[mid];
+    }
+private:
+    bool used[9];
+    
+    ///@brief   计算合法的解锁图案数目
+    int calPatterns(int last, int len) {
+        if (len == 0)   return 1;
+        int sum = 0;
+        for (int i = 0; i < 9; i++) {
+            if (isValid(i, last)) {
+                used[i] = true;
+                sum += calPatterns(i, len - 1);
+                used[i] = false;
+            }
+        }
+        return sum;
     }
 };
 
