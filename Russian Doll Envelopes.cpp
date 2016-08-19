68bdbd90f80e75e08b57c0a0b65d60790c042685
Zhao Wei <pkuzw@126.com>
2016-08-19 18:46:38 +0800

    v0.1
    1. 写文件头部注释。

---
+++ Russian Doll Envelopes.cpp
@@ -0,0 +1,33 @@
+//
+//  Russian Doll Envelopes.cpp
+//  LeetCode by zhaowei
+//
+//  Created by Zhao Wei on 16/8/19.
+//  Copyright © 2016年 Zhao Wei. All rights reserved.
+//
+
+///@author  zhaowei
+///@date    2016.08.19
+///@version 1.0
+
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <map>
+using namespace std;
+
+class Solution {
+public:
+    ///@brief   计算能够完全包含子信封的最大嵌套数目
+    ///@param   envelopes   信封的尺寸，嵌套时要求较大信封的长宽值应该大于较小信封的长宽值
+    ///@return  返回最大嵌套信封数目
+    ///@note    1. 动态规划
+    //          2. 
+    int maxEnvelopes(vector<pair<int, int>>& envelopes) {
+        return 0;
+    }
+};
+
+int main() {
+    return 0;
+}
