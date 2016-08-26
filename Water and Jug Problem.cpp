acd91d977c578f16673ba414531872ac24a2b8d1
Zhao Wei <pkuzw@126.com>
2016-08-26 23:58:32 +0800

    v0 - 写文件头部信息

---
+++ Water and Jug Problem.cpp
@@ -0,0 +1,31 @@
+//
+//  Water and Jug Problem.cpp
+//  LeetCode by zhaowei
+//
+//  Created by Zhao Wei on 16/8/26.
+//  Copyright © 2016年 Zhao Wei. All rights reserved.
+//
+
+///@file    Water and Jug Problem
+///@author  zhaowei
+///@date    2016.08.26
+///@version 1.0
+
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    ///@brief   判断x和y容量的桶能否称量出z容量多的水
+    ///@param   x, y, z 桶的容量
+    ///@return  如果可以，返回true；否则返回false
+    bool canMeasureWater(int x, int y, int z) {
+        return false;
+    }
+};
+
+int main() {
+    return 0;
+}
