543c1d12e76f846e43168e8c38a8c2a2f5096eea
Zhao Wei <pkuzw@126.com>
2016-08-28 22:56:31 +0800

    v0.2
    1. 将此问题转换为代数问题；
    2. ax + by =? z，但是要满足如下两个条件：
    3. 如果x和y均为偶数，则z一定为偶数；
    4. z <= x + y. 其中a和b是整数。

--- Water and Jug Problem.cpp
+++ Water and Jug Problem.cpp
@@ -8,7 +8,7 @@
 
 ///@file    Water and Jug Problem
 ///@author  zhaowei
-///@date    2016.08.26
+///@date    2016.08.28
 ///@version 1.0
 
 #include <vector>
@@ -21,7 +21,23 @@ public:
     ///@brief   判断x和y容量的桶能否称量出z容量多的水
     ///@param   x, y, z 桶的容量
     ///@return  如果可以，返回true；否则返回false
+    ///@note    1. 将此问题转换为代数问题；
+    //          2. ax + by =? z，但是要满足如下两个条件：
+    //          3. 如果x和y均为偶数，则z一定为偶数；
+    //          4. z <= x + y. 其中a和b是整数。
     bool canMeasureWater(int x, int y, int z) {
+        if (z > x + y)  return false;
+        if (x % 2 == 0 && y % 2 == 0 && z % 2 == 1) return false;
+        int a = 0, b = 0;
+        
+        int i = 0;
+        while (1) {
+            a = i;
+            if ((z - a * x) % y  == 0 || (z + a * x) % y == 0)  return true;
+            i++;
+            if (a * x + b * y)
+        }
+        
         return false;
     }
 };
