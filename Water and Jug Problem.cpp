2e7b040a1636229496693a3da34725f77e73d55d
Zhao Wei <pkuzw@126.com>
2016-08-29 00:10:36 +0800

    v1.0 - logic
    1. 将此问题转换为代数问题；
    2. ax + by =? z，但是要满足如下条件：
    3. 如果x和y均为偶数，则z一定为偶数；
    4. z <= x + y. 其中a和b是整数；
    5. 如果x与y存在公约数，那么z必须要整除二者的最小公约数；
    6. 此外，需要对有0的情况进行额外处理。

--- Water and Jug Problem.cpp
+++ Water and Jug Problem.cpp
@@ -22,26 +22,46 @@ public:
     ///@param   x, y, z 桶的容量
     ///@return  如果可以，返回true；否则返回false
     ///@note    1. 将此问题转换为代数问题；
-    //          2. ax + by =? z，但是要满足如下两个条件：
+    //          2. ax + by =? z，但是要满足如下条件：
     //          3. 如果x和y均为偶数，则z一定为偶数；
-    //          4. z <= x + y. 其中a和b是整数。
+    //          4. z <= x + y. 其中a和b是整数；
+    //          5. 如果x与y存在公约数，那么z必须要整除二者的最小公约数；
+    //          6. 此外，需要对有0的情况进行额外处理。
     bool canMeasureWater(int x, int y, int z) {
+        //  z不能够超过x与y之和
         if (z > x + y)  return false;
+        //  如果x和y是偶数，则z一定是偶数
         if (x % 2 == 0 && y % 2 == 0 && z % 2 == 1) return false;
-        int a = 0, b = 0;
-        
-        int i = 0;
-        while (1) {
-            a = i;
-            if ((z - a * x) % y  == 0 || (z + a * x) % y == 0)  return true;
-            i++;
-            if (a * x + b * y)
+        //  判断x, y, z中有为0的情况
+        if (x == 0) {
+            if (y == 0) {
+                if (z != 0) return false;
+                else    return true;
+            }
+            if (z % y)  return false;
         }
-        
-        return false;
+        if (y == 0)
+            if (z % x)  return false;
+        //  如果x与y有最小公约数
+        int cd = haveSameCommonDivisor(x, y);
+        if (cd != -1 && z % cd) return false;
+        return true;
+    }
+    
+    ///@brief   找出a和b的最小公约数
+    ///@param   a, b    输入参数
+    ///@return  如果a和b存在最小公约数，返回之；否则返回-1
+    int haveSameCommonDivisor(int a, int b) {
+        if (a  > b) swap(a, b);
+        for (int i = 2; i <= a; i++)
+            if (a % i == 0 && b % i == 0)   return i;
+        return -1;
     }
 };
 
 int main() {
+    Solution slt;
+    int x = 23, y = 46, z = 12;
+    bool rslt = slt.canMeasureWater(x, y, z);
     return 0;
 }
