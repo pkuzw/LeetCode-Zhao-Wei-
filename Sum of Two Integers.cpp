5347ec63b404ab3851156337210ad27758e62302
Zhao Wei <pkuzw@126.com>
2016-09-06 23:02:10 +0800

    v1.0 - bit manipulation
    1. 完成了有效数字的和；
    2. 有效数字之间的差不对，还需要继续调试。

--- Sum of Two Integers.cpp
+++ Sum of Two Integers.cpp
@@ -27,34 +27,73 @@ public:
     ///         4. 时间复杂度为O(n)，空间复杂度为O(n)，其中n为整型变量的二进制长度
     int getSum(int a, int b) {
         int rslt = 0;
-        bool isSum = true;
-        int sign = 1;
-        if (a > 0 && b > 0) {
-            sign = 1;
-            isSum = true;
-        }
-        else if (a < 0 && b < 0) {
-            sign = -1;
-            isSum = true;
-        }
+        if (a > 0 && b > 0) rslt = sum(a, b);
+        else if (a < 0 && b < 0)    rslt = -1 * sum(-a, -b);
         else if (a == 0)    return b;
         else if (b == 0)    return a;
         else {
-            isSum = false;
             if (a < 0 && b > 0) {
-                if (-a > b) sign = -1;
-                else    sign = 1;
+                if (-a > b) rslt = -1 * difference(-a, b);
+                else    rslt = difference(b, -a);
             }
             else if (a > 0 && b < 0) {
-                if (a >= -b) sign = 1;
-                else    sign = -1;
+                if (a >= -b) rslt = difference(a, -b);
+                else    rslt = -1 * difference(-b, a);
             }
+        }
+        return rslt;
+    }
+    
+    ///@brief   计算两个正整数的和
+    ///@param   a, b    两个加数
+    ///@return  返回a + b
+    int sum(int a, int b) {
+        int rslt = 0;
+        int carry = 0;
+        for (int i = 0; i < 31; i++) {
+            int x = a & 1;
+            a >>= 1;
+            int y = b & 1;
+            b >>= 1;
+            int z = x ^ y;
+            z ^= carry;
+            z <<= 30;
+            rslt = (rslt >> 1) | z;
+            z >>= 30;
+            carry = z == 0 && x == 1 ? 1 : 0;
+        }
+        return rslt;
+    }
+    
+    ///@brief   计算两个数的差
+    ///@param   a   被减数
+    ///@param   b   减数
+    ///@return  返回a - b的差
+    int difference(int a, int b) {
+        int rslt = 0;
+        int carry = 0;
+        for (int i = 0; i < 31; i++) {
+            int x = a & 1;
+            a >>= 1;
+            x ^= carry;
+            if (x == 1 && carry == 1)   carry = 1;
+            int y = b & 1;
+            b >>= 1;
+            int z = x ^ y;
+            z <<= 30;
+            rslt = (rslt >> 1) | z;
+            z >>= 30;
+            if (z == 1 && x == 0)   carry = 1;
         }
         return rslt;
     }
 };
 
 int main() {
-    
+    int a = 2, b = -1;
+    int rslt = 0;
+    Solution slt;
+//    
+    rslt = slt.getSum(a, b);
     return 0;
 }
