3416423ac31c7d57e184be2d541de4eb0e3295d9
Zhao Wei <pkuzw@126.com>
2016-09-17 21:43:10 -0700

    v1.0 - dynamic programming
    Definition of dp[i][j]: minimum number of money to guarantee win for
    subproblem [i, j].
    
         Target: dp[1][n]
    
         Corner case: dp[i][i] = 0 (because the only element must be
    correct)
    
         Equation: we can choose k (i<=k<=j) as our guess, and pay price k.
    After our guess, the problem is divided into two subproblems. Notice we
    do not need to pay the money for both subproblems. We only need to pay
    the worst case (because the system will tell us which side we should
    go) to guarantee win. So dp[i][j] = min (i<=k<=j) { k + max(dp[i][k-1],
    dp[k+1][j]) }

--- Guess Number Higher or Lower II.cpp
+++ Guess Number Higher or Lower II.cpp
@@ -47,7 +47,7 @@ public:
     ///@return  the money we need to win
     ///@note    1. Wrong Answer.
     //          2. It couldn't be done by naive binary search. For instance, if n = 5, as a naive binary search, we pick {3, 4} in order as the worst case, however, we could actually pick 4 at first, and if higher, 5 is the number. Otherwise it's between 1 to 3. Then we choose 2. So the total expense is 4 + 2 = 6, not $7.
-    int getMoneyAmount(int n) {
+    int getMoneyAmount_wa(int n) {
         int rslt = 0;
         int left = 1, right = n;
         int mid = left + (right - left) / 2;
@@ -57,6 +57,29 @@ public:
             mid = left + (right - left) / 2;
         }
         return rslt;
+    }
+    
+    /*note
+     Definition of dp[i][j]: minimum number of money to guarantee win for subproblem [i, j].
+     
+     Target: dp[1][n]
+     
+     Corner case: dp[i][i] = 0 (because the only element must be correct)
+     
+     Equation: we can choose k (i<=k<=j) as our guess, and pay price k. After our guess, the problem is divided into two subproblems. Notice we do not need to pay the money for both subproblems. We only need to pay the worst case (because the system will tell us which side we should go) to guarantee win. So dp[i][j] = min (i<=k<=j) { k + max(dp[i][k-1], dp[k+1][j]) }
+     
+     */
+    int getMoneyAmount(int n) {
+        vector<vector<int>> dp(n + 2, vector<int>(n+2, 0));
+        for (int len = 1; len < n; len++) {
+            for (int from = 1, to = from + len; to <= n; from++, to++) {
+                dp[from][to] = INT_MAX;
+                for (int k = from; k <= to; k++) {
+                    dp[from][to] = min(dp[from][to], k + max(dp[from][k-1], dp[k+1][to]));
+                }
+            }
+        }
+        return dp[1][n];
     }
 };
 
