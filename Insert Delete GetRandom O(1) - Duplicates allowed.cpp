e482d91a42a8cd21d3043817662b4fe0ca85d20d
Zhao Wei <pkuzw@126.com>
2016-09-22 21:23:38 -0700

    v1.0 - unordered map
    1. based on the Insert Delte GetRandom without duplicates' solution. we
    change the unordered_map<int, int> into unordered_map<int,
    vector<int>>, so it could tolerate the duplicates.
    2.
    https://discuss.leetcode.com/topic/54381/c-128m-solution-real-o-1-soluti
    on

---
+++ Insert Delete GetRandom O(1) - Duplicates allowed.cpp
@@ -0,0 +1,75 @@
+//
+//  Insert Delete GetRandom O(1) - Duplicates allowed.cpp
+//  LeetCode by zhaowei
+//
+//  Created by Zhao Wei on 9/21/16.
+//  Copyright © 2016 Zhao Wei. All rights reserved.
+//
+
+///@file    Insert Delete GetRandom O(1) - Duplicates allowed
+///@author  Wei Zhao
+///@date    2016.09.22
+///@version 1.0
+
+#include <unordered_map>
+#include <vector>
+#include <cstdlib>
+
+using namespace std;
+
+///@note    1. based on the Insert Delte GetRandom without duplicates' solution. we change the unordered_map<int, int> into unordered_map<int, vector<int>>, so it could tolerate the duplicates.
+//          2. https://discuss.leetcode.com/topic/54381/c-128m-solution-real-o-1-solution
+class RandomizedCollection {
+public:
+    /** Initialize your data structure here. */
+    RandomizedCollection() {
+        
+    }
+    
+    /** Inserts a value to the collection. Returns true if the collection did not already contain the specified element. */
+    bool insert(int val) {
+        auto result = m.find(val) == m.end();
+        
+        m[val].push_back((int)nums.size());
+        nums.push_back(pair<int, int>(val, m[val].size() - 1));
+        
+        return result;
+    }
+    
+    /** Removes a value from the collection. Returns true if the collection contained the specified element. */
+    bool remove(int val) {
+        auto result = m.find(val) != m.end();
+        if(result)
+        {
+            auto last = nums.back();
+            m[last.first][last.second] = m[val].back();
+            nums[m[val].back()] = last;
+            m[val].pop_back();
+            if(m[val].empty()) m.erase(val);
+            nums.pop_back();
+        }
+        return result;
+    }
+    
+    /** Get a random element from the collection. */
+    int getRandom() {
+        return nums[rand() % nums.size()].first;
+    }
+private:
+    vector<pair<int, int>> nums;
+    unordered_map<int, vector<int>> m;
+};
+
+/**
+ * Your RandomizedCollection object will be instantiated and called as such:
+ * RandomizedCollection obj = new RandomizedCollection();
+ * bool param_1 = obj.insert(val);
+ * bool param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
+
+
+int main() {
+    
+    return 0;
+}
