ec9c2c73da9a54c47d220b6c275ee43ff2a171f7
Zhao Wei <pkuzw@126.com>
2016-10-01 00:14:33 -0700

    v1.0 - preorder traversal
    1. preorder traversal
    2. find all the left leaves and use a global variable to sum them
    3. if there is only a root node, it doesn't count as left leaf.

--- Sum of Left Leaves.cpp
+++ Sum of Left Leaves.cpp
@@ -16,11 +16,11 @@
  
  Example:
  
- 3
- / \
- 9  20
- /  \
- 15   7
+      3
+     / \
+    9  20
+      /  \
+     15   7
  
  There are two left leaves in the binary tree, with values 9 and 15 respectively. Return 24.
  */
@@ -40,19 +40,44 @@ class Solution {
 public:
     ///@note    1. preorder traversal
     //          2. find all the left leaves and use a global variable to sum them
+    //          3. if there is only a root node, it doesn't count as left leaf.
     int sumOfLeftLeaves(TreeNode* root) {
         int sum = 0;
-        
+        dfs(root, sum, false);
+        return sum;
     }
     
-    void dfs(TreeNode* root, int& sum) {
+    void dfs(TreeNode* root, int& sum, bool fromLeft) {
         if (!root)  return;
-        if (root->left && )
+        if (root->left) dfs(root->left, sum, true);
+        if (root->right)    dfs(root->right, sum, false);
+        if (!root->left && !root->right && fromLeft)
+            sum += root->val;
+        return;
     }
 };
 
 int main() {
+    TreeNode* root = new TreeNode(3);
     
+    TreeNode* n1 = new TreeNode(9);
+    TreeNode* n2 = new TreeNode(20);
+    TreeNode* n3 = new TreeNode(15);
+    TreeNode* n4 = new TreeNode(7);
+    TreeNode* n5 = new TreeNode(5);
+    TreeNode* n6 = new TreeNode(27);
+    
+    root->left = n1;
+    root->right = n2;
+    n2->left = n3;
+    n2->right = n4;
+    n4->left = n5;
+    n1->left = n6;
+    
+    Solution slt;
+    
+    int rslt = slt.sumOfLeftLeaves(root);
+
     return 0;
 }
 
