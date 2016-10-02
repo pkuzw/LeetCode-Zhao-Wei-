7ec6ce4b1ba51f8307595b5162dd2ef50e274253
Zhao Wei <pkuzw@126.com>
2016-10-01 23:57:50 -0700

    v0.1
    hash table

---
+++ Longest Palindrome.cpp
@@ -0,0 +1,46 @@
+//
+//  Longest Palindrome.cpp
+//  LeetCode by zhaowei
+//
+//  Created by Zhao Wei on 10/1/16.
+//  Copyright © 2016 Zhao Wei. All rights reserved.
+//
+
+///@file    Longest Palindrome
+///@author  Wei Zhao
+///@date    10.01.2016
+///@version 1.0
+
+/*
+ Given a string which consists of lowercase or uppercase letters, find the length of the longest palindromes that can be built with those letters.
+ 
+ This is case sensitive, for example "Aa" is not considered a palindrome here.
+ 
+ Note:
+ Assume the length of given string will not exceed 1,010.
+ 
+ Example:
+ 
+ Input:
+ "abccccdd"
+ 
+ Output:
+ 7
+ 
+ Explanation:
+ One longest palindrome that can be built is "dccaccd", whose length is 7.
+ */
+#include <stdio.h>
+
+class Solution {
+public:
+    ///@note    hash table
+    int longestPalindrome(string s) {
+        
+    }
+};
+
+int main() {
+    
+    return 0;
+}
