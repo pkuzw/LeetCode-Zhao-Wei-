c74e1842e4bbf955bd1e1331a75669159bf3ff83
zhaowei <pkuzw@126.com>
2018-10-26 22:18:36 +0800

    Merge Interval - sort and enuermation
    1. You should write your own cmp() function to use sort().

--- Merge Intervals.cpp
+++ Merge Intervals.cpp
@@ -17,6 +17,9 @@ return [1,6],[8,10],[15,18].
 ///@date	2016.04.14
 ///@version	1.2
 
+///@date    October 26, 2018
+///@version 1.3
+
 #include <vector>
 #include <iostream>
 #include <algorithm>
@@ -78,27 +81,41 @@ private:
 
 
 //	v1.2
+///@brief   compare function is used for sort()
+///@param   a   interval a
+///@param   b   interval b
+///@return  if a.start < b.start, return true; else return false
 bool myCmp(Interval& a, Interval& b) {
-	return a.start < b.start;
+    return a.start < b.start;
 }
 
 class Solution {
 public:
+    ///@brief   合并重叠的区间。
+    ///@param   intervals   区间数组。
+    ///@return  返回合并后的区间数组。
+    ///@note    1. 枚举法；
+    //          2. 先对所有区间按照开始端点进行排序；
+    //          3. 然后从前往后遍历区间，如果当前区间的起始端点在上一区间的末尾端点之前或相等，则将上一区间的末尾端点扩充到当前区间的末尾；
+    //          4. 否则就将这个合并的区间压入结果数组，并重新开始新的区间合并。
+    //          5. 算法的时间复杂度为O(nlogn)，空间复杂度为O(n)，其中n是区间的数目。
 	vector<Interval> merge(vector<Interval>& intervals) {
-		vector<Interval> rslt;
-		if (intervals.empty())	return rslt;		
-		sort(intervals.begin(), intervals.end(), myCmp);
-		Interval tmp(intervals[0].start, intervals[0].end);
-		for (int i = 0; i < intervals.size(); i++) {
-			if (intervals[i].start <= tmp.end)	tmp.end = max(tmp.end, intervals[i].end);			
-			else {
-				rslt.push_back(tmp);
-				tmp = intervals[i];
-			}
-		}
-		rslt.push_back(tmp);
-		return rslt;
-	}
+        vector<Interval> rslt;
+        if (intervals.empty())  return rslt;
+        sort(intervals.begin(), intervals.end(), myCmp);
+        Interval tmp(intervals[0].start, intervals[0].end);
+        for (int i = 0; i < intervals.size(); i++) {
+            if (intervals[i].start <= tmp.end) {
+                tmp.end = max(tmp.end, intervals[i].end);
+            }
+            else {
+                rslt.push_back(tmp);
+                tmp = intervals[i];
+            }
+        }
+        rslt.push_back(tmp);
+        return rslt;
+    }
 };
 
 int main()
